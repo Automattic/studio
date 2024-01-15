98630db05f54eb33a8e3276808db4dcd4047c4c4
Philip Jackson <p-jackson@live.com>
2024-01-15 13:58:07 +1300

    eslint configuration for unused variables

--- src/ipc-handlers.ts
+++ src/ipc-handlers.ts
@@ -1,5 +1,8 @@
 import { type IpcMainInvokeEvent } from 'electron';
 
+// IPC functions must accept an `event` as the first argument.
+/* eslint @typescript-eslint/no-unused-vars: ["warn", { "argsIgnorePattern": "event" }] */
+
 export async function ping( event: IpcMainInvokeEvent, message: string ): Promise< string > {
 	return message;
 }

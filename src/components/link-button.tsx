25c8097b50ef6c7749badfa740ccdeb2595b648b
Philip Jackson <p-jackson@live.com>
2024-01-17 11:55:24 +1300

    Button props forward their React refs

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -1,10 +1,10 @@
-import { type ButtonHTMLAttributes } from 'react';
+import { forwardRef, type ButtonHTMLAttributes } from 'react';
 import { cx } from '../cx';
 
-export default function LinkButton( {
-	type = 'button',
-	className,
-	...rest
-}: ButtonHTMLAttributes< HTMLButtonElement > ) {
-	return <button type={ type } className={ cx( 'underline', className ) } { ...rest } />;
-}
+export default forwardRef< HTMLButtonElement, ButtonHTMLAttributes< HTMLButtonElement > >(
+	function LinkButton( { type = 'button', className, ...rest }, ref ) {
+		return (
+			<button ref={ ref } type={ type } className={ cx( 'underline', className ) } { ...rest } />
+		);
+	}
+);

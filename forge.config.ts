e2c8dcdab2a47c618ea8935d451c2ab2b1d83041
Philip Jackson <p-jackson@live.com>
2024-01-19 12:10:41 +1300

    npm run make builds a universal macOS app

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -14,12 +12,7 @@ const config: ForgeConfig = {
 		asar: true,
 	},
 	rebuildConfig: {},
-	makers: [
-		new MakerSquirrel( {} ),
-		new MakerZIP( {}, [ 'darwin' ] ),
-		new MakerRpm( {} ),
-		new MakerDeb( {} ),
-	],
+	makers: [ new MakerZIP( {}, [ 'darwin' ] ), new MakerDMG( {}, [ 'darwin' ] ) ],
 	plugins: [
 		new AutoUnpackNativesPlugin( {} ),
 		new WebpackPlugin( {

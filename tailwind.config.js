bb7f052897a79413330e780922cc81e672ae8888
Antony Agrios <antonyagrios@gmail.com>
2024-02-08 14:14:37 +0200

    Local Environment: Add figma typography to tailwind (#36)
    * Local Environment: Add figma typography to tailwind
    
    * Update: fix text-transform
    
    * Update: label font weight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,116 @@
 /** @type {import('tailwindcss').Config} */
 import palette from '@automattic/color-studio';
+import plugin from 'tailwindcss/plugin';
+
+const BASE_FONT_SIZE = 16; // 1 rem
+const pxToRem = ( px ) => `${ px / BASE_FONT_SIZE }rem`;
+const typographyStyles = {
+	'title-large': {
+		fontSize: pxToRem( 32 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 40 ),
+		textTransform: 'normal',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'title-medium': {
+		fontSize: pxToRem( 24 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 32 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'title-small': {
+		fontSize: pxToRem( 20 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 28 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	subtitle: {
+		fontSize: pxToRem( 16 ),
+		fontWeight: '600',
+		lineHeight: pxToRem( 24 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'subtitle-small': {
+		fontSize: pxToRem( 14 ),
+		fontWeight: '600',
+		lineHeight: pxToRem( 20 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	body: {
+		fontSize: pxToRem( 13 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 20 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	button: {
+		fontSize: pxToRem( 13 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 13 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	label: {
+		fontSize: pxToRem( 11 ),
+		fontWeight: '500',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'uppercase',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'label-semibold': {
+		fontSize: pxToRem( 13 ),
+		fontWeight: '600',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'link-text': {
+		fontSize: pxToRem( 13 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	placeholder: {
+		fontSize: pxToRem( 13 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'body-small': {
+		fontSize: pxToRem( 12 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'helper-text': {
+		fontSize: pxToRem( 12 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'initial',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'small-button-text': {
+		fontSize: pxToRem( 11 ),
+		fontWeight: '400',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'uppercase',
+		letterSpacing: pxToRem( 0 ),
+	},
+	'section-heading': {
+		fontSize: pxToRem( 11 ),
+		fontWeight: '500',
+		lineHeight: pxToRem( 16 ),
+		textTransform: 'uppercase',
+		letterSpacing: pxToRem( 0 ),
+	},
+};
 
 let a8cToTailwindColors = {};
 const PREFIX = 'a8c';
@@ -34,5 +145,21 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin( function ( { addComponents, e } ) {
+			const newComponents = Object.entries( typographyStyles ).reduce( ( acc, [ key, value ] ) => {
+				const name = `.${ e( `${ PREFIX }-${ key }` ) }`; // e.g. .a8c-title-large
+				acc[ name ] = {
+					fontSize: value.fontSize,
+					fontWeight: value.fontWeight,
+					lineHeight: value.lineHeight,
+					textTransform: value.textTransform,
+					letterSpacing: value.letterSpacing,
+				};
+				return acc;
+			}, {} );
+
+			addComponents( newComponents );
+		} ),
+	],
 };

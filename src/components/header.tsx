bfa27e5ec0b1ff314e745967c2ff604ea9397de6
Wojtek Naruniec <wojtek@naruniec.me>
2024-03-12 14:50:30 +0100

    Update 'Open site' link (#136)
    * Rename localhost and port to 'Open site'
    
    * Change 'Open site' and 'WP Admin' links order

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,9 @@
-import { sprintf } from '@wordpress/i18n';
 import { Icon, external } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import { useSiteDetails } from '../hooks/use-site-details';
 import { getIpcApi } from '../lib/get-ipc-api';
 import Button from './button';
 import { SiteManagementActions } from './site-management-actions';
-
-const SITE_URL = 'localhost';
 
 export default function Header() {
 	const { __ } = useI18n();
@@ -20,18 +17,18 @@ export default function Header() {
 						<Button
 							disabled={ ! site.running }
 							className="cursor-pointer text-[13px] text-a8c-gray-50 hover:text-a8c-gray-90 !px-0 h-0 leading-4"
-							onClick={ () => getIpcApi().openSiteURL( site.id ) }
-						>
-							{ sprintf( __( '%s:%s' ), SITE_URL, site.port ) }
-							<Icon className="ml-1" icon={ external } size={ 14 } />
-						</Button>
-						<Button
-							disabled={ ! site.running }
-							className="cursor-pointer text-[13px] text-a8c-gray-50 hover:text-a8c-gray-90 !px-0 h-0 leading-4"
 							onClick={ () => getIpcApi().openSiteURL( site.id, '/wp-admin' ) }
 						>
 							{ __( 'WP Admin' ) }
 							<Icon icon={ external } className="ml-1" size={ 14 } />
+						</Button>
+						<Button
+							disabled={ ! site.running }
+							className="cursor-pointer text-[13px] text-a8c-gray-50 hover:text-a8c-gray-90 !px-0 h-0 leading-4"
+							onClick={ () => getIpcApi().openSiteURL( site.id ) }
+						>
+							{ __( 'Open site' ) }
+							<Icon className="ml-1" icon={ external } size={ 14 } />
 						</Button>
 					</div>
 				</div>

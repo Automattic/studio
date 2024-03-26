77a68a17b62a59ff2bf0dd7dc09514341c3ff060
Wojtek Naruniec <wojtek@naruniec.me>
2024-03-26 08:45:46 +0100

    Reduce number of preview links to 5 (#188)

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const MAIN_MIN_WIDTH = 900;
 export const MAIN_MIN_HEIGHT = 600;
 export const SCREENSHOT_WIDTH = 1040;
 export const SCREENSHOT_HEIGHT = 1248;
-export const LIMIT_OF_ZIP_SITES_PER_USER = 10;
+export const LIMIT_OF_ZIP_SITES_PER_USER = 5;
 export const AUTO_UPDATE_INTERVAL_MS = 60 * 60 * 1000;
 export const WINDOWS_TITLEBAR_HEIGHT = 32;

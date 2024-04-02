bc1384af586f5def7587e7030a28d757fc4b627a
Wojtek Naruniec <wojtek@naruniec.me>
2024-04-02 10:27:58 +0200

    Add mu-plugin that fixes 32it integer warnings (#206)

--- vendor/wp-now/src/download.ts
+++ vendor/wp-now/src/download.ts
@@ -254,4 +254,23 @@ export async function downloadMuPlugins(customMuPluginsPath = '') {
 			require_once __DIR__ . "/${SQLITE_FILENAME}/load.php";
 		}`
 	);
+
+	fs.writeFile(
+		path.join(muPluginsPath, '0-32bit-integer-warnings.php'),
+		`<?php
+/**
+ * This is a temporary workaround to hide the 32bit integer warnings that
+ * appear when using various time related function, such as strtotime and mktime.
+ * Examples of the warnings that are displayed:
+ * Warning: mktime(): Epoch doesn't fit in a PHP integer in <file>
+ * Warning: strtotime(): Epoch doesn't fit in a PHP integer in <file>
+ */
+set_error_handler(function($severity, $message, $file, $line) {
+  if (strpos($message, "fit in a PHP integer") !== false) {
+      return;
+  }
+  return false;
+});
+`);
+
 }

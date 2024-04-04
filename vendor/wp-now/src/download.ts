5ac06e1d85f50c9e0172f23105973d6931748fc5
Antony Agrios <antonyagrios@gmail.com>
2024-04-04 14:06:57 +0300

    Local Environment: Check theme availability (#224)
    * Local Environment: Check theme integrity
    
    * Update: Rename function

--- vendor/wp-now/src/download.ts
+++ vendor/wp-now/src/download.ts
@@ -273,4 +273,31 @@ set_error_handler(function($severity, $message, $file, $line) {
 });
 `);
 
+	fs.writeFile(
+		path.join(muPluginsPath, '0-check-theme-availability.php'),
+		`<?php
+	function check_current_theme_availability() {
+			// Get the current theme's directory
+			$current_theme = wp_get_theme();
+			$theme_dir = get_theme_root() . '/' . $current_theme->stylesheet;
+
+			if (!is_dir($theme_dir)) {
+					$all_themes = wp_get_themes();
+					$available_themes = [];
+
+					foreach ($all_themes as $theme_slug => $theme_obj) {
+							if ($theme_slug != $current_theme->get_stylesheet()) {
+									$available_themes[$theme_slug] = $theme_obj;
+							}
+					}
+
+					if (!empty($available_themes)) {
+							$new_theme_slug = array_keys($available_themes)[0];
+							switch_theme($new_theme_slug);
+					}
+			}
+	}
+	add_action('after_setup_theme', 'check_current_theme_availability');
+`);
+
 }

a1e47fbf43ff9fa7b02877ae2f9ef91c61c46e50
katinthehatsite <katerynakodonenko@gmail.com>
2024-04-09 12:05:44 +0200

    Implement basic logic for Gravatar icon (#249)
    Co-authored-by: Kateryna Kodonenko <kateryna@automattic.com>

--- src/components/gravatar.tsx
+++ src/components/gravatar.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@wordpress/components';
 import { commentAuthorAvatar } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
+import { useState } from 'react';
 import { useAuth } from '../hooks/use-auth';
 import { useGravatarUrl } from '../hooks/use-gravatar-url';
 import { cx } from '../lib/cx';
@@ -17,19 +18,32 @@ export function Gravatar( {
 	const { __ } = useI18n();
 	const { user } = useAuth();
 	const gravatarUrl = useGravatarUrl( user?.email, isBlack );
+	const [ imageError, setImageError ] = useState( false );
+
 	const childClassName = cx(
 		isLarge ? 'w-[32px] h-[32px] rounded-full' : 'w-[18px] h-[18px] rounded-full',
 		className
 	);
 
-	return gravatarUrl ? (
-		<img src={ gravatarUrl } alt={ __( 'User avatar' ) } className={ childClassName } />
-	) : (
+	const renderDefaultGravatarIcon = () => (
 		<Icon
 			icon={ commentAuthorAvatar }
 			viewBox={ '4 4 16 16' }
 			size={ isLarge ? 32 : 18 }
 			className={ childClassName }
+		/>
+	);
+
+	if ( imageError || ! gravatarUrl ) {
+		return renderDefaultGravatarIcon();
+	}
+
+	return (
+		<img
+			src={ gravatarUrl }
+			alt={ __( 'User avatar' ) }
+			className={ childClassName }
+			onError={ () => setImageError( true ) }
 		/>
 	);
 }

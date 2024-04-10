a3d71921a37a774d18fe7a63c24cde6dfd525b44
Antony Agrios <antonyagrios@gmail.com>
2024-04-10 13:04:50 +0300

    Local Environment: Ceil expiration time to show 7 days for the first hour (#255)
    * Local Environment: Ceil expiration time to show 7 days for the first hour
    
    * Update: fix lint errors
    
    * Update: Remove console statement
    
    * Update: change if statement
    
    * Update: simplify code
    
    * Update: remove extra lines
    
    * Update: remove extra lines
    
    * Update: fix tests and make them less flacky

--- src/hooks/tests/use-expiration-date.test.ts
+++ src/hooks/tests/use-expiration-date.test.ts
@@ -1,27 +1,34 @@
-// To run tests, execute `npm run test -- src/hooks/use-expiration-date.test.ts` from the root directory
+// To run tests, execute `npm run test -- src/hooks/tests/use-expiration-date.test.ts` from the root directory
 import { renderHook } from '@testing-library/react';
-import { subDays, subHours } from 'date-fns';
+import { subDays, subHours, subMinutes, subMilliseconds } from 'date-fns';
 import { useExpirationDate } from '../use-expiration-date';
 
 describe( 'useExpirationDate', () => {
 	test( 'should indicate isExpired for dates more than 7 days', () => {
-		const snapshotDate = subDays( new Date(), 7 ).getTime() + 1; // Mor than 7 days
+		const snapshotDate = subDays( new Date(), 8 ).getTime(); // More than 7 days
 		const { result } = renderHook( () => useExpirationDate( snapshotDate ) );
 		expect( result.current.isExpired ).toBeTruthy();
 		expect( result.current.countDown ).toBe( 'Expired' );
 	} );
 
 	test( 'should show correct countdown for dates within 7 days', () => {
-		const snapshotDate = subDays( new Date(), 2 ).getTime() - 1;
+		const snapshotDate = subHours( subDays( new Date(), 2 ), 1 ).getTime();
 		const { result } = renderHook( () => useExpirationDate( snapshotDate ) );
 		expect( result.current.isExpired ).toBeFalsy();
 		expect( result.current.countDown ).toBe( '4 days, 23 hours' );
 	} );
 
-	test( 'should switch to hours and minutes format within 24 hours to expiration', () => {
-		const snapshotDate = subHours( subDays( new Date(), 6 ), 12 ).getTime() - 1;
+	test( 'should extend countdown day for the first hour', () => {
+		const snapshotDate = subMinutes( new Date().getTime(), 5 ).getTime();
 		const { result } = renderHook( () => useExpirationDate( snapshotDate ) );
-		expect( result.current.countDown ).toBe( '11 hours, 59 minutes' );
+		expect( result.current.isExpired ).toBeFalsy();
+		expect( result.current.countDown ).toBe( '7 days' );
+	} );
+
+	test( 'should switch to hours and minutes format within 24 hours to expiration', () => {
+		const snapshotDate = subMilliseconds( subHours( subDays( new Date(), 6 ), 12 ), 100 ).getTime();
+		const { result } = renderHook( () => useExpirationDate( snapshotDate ) );
+		expect( result.current.countDown ).toBe( '12 hours, 59 minutes' );
 	} );
 	test( 'should switch to minutes and seconds format within the last hour before expiration', () => {
 		const snapshotDate = subHours( subDays( new Date(), 6 ), 23 ).getTime() - 1;
--- src/hooks/use-expiration-date.ts
+++ src/hooks/use-expiration-date.ts
@@ -1,5 +1,8 @@
 import { useI18n } from '@wordpress/react-i18n';
-import { intervalToDuration, formatDuration, addDays, Duration } from 'date-fns';
+import { intervalToDuration, formatDuration, addDays, Duration, addHours } from 'date-fns';
+
+const HOUR = 1000 * 60 * 60;
+const DAY = HOUR * 24;
 
 function formatStringDate( ms: number ): string {
 	const { locale = 'en' } = window?.appGlobals || {};
@@ -15,20 +18,23 @@ export function useExpirationDate( snapshotDate: number ) {
 	const { __ } = useI18n();
 	const MAX_DAYS = 7;
 	const now = new Date();
-	const difference = addDays( snapshotDate, MAX_DAYS ).getTime() - now.getTime();
+	const endDate = addDays( snapshotDate, MAX_DAYS );
+	const difference = endDate.getTime() - now.getTime();
 	let isExpired = false;
 	let format: ( keyof Duration )[] = [ 'days', 'hours' ];
 	if ( difference < 0 ) {
 		isExpired = true;
-	} else if ( difference < 1000 * 60 * 60 ) {
+	} else if ( difference < HOUR ) {
 		format = [ 'minutes' ];
-	} else if ( difference < 1000 * 60 * 60 * 24 ) {
+	} else if ( difference < DAY ) {
 		format = [ 'hours', 'minutes' ];
 	}
 	const countDown = formatDuration(
 		intervalToDuration( {
 			start: now,
-			end: addDays( snapshotDate, MAX_DAYS ),
+			// Add 1 hour to the end date serves us here, as for the last hour
+			// we show the minutes left.
+			end: addHours( endDate, 1 ),
 		} ),
 		{ format, delimiter: ', ' }
 	);

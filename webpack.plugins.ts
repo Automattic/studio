b63ca1ca83dfd4c685cee73caf70042b6b7c9807
David Calhoun <github@davidcalhoun.me>
2024-04-25 11:07:18 -0400

    build: Load environment configuration files for Webpack build
    E.g., load necessary Sentry authentication token for the Webpack build
    process to upload source maps.

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,6 +1,7 @@
 import { sentryWebpackPlugin } from '@sentry/webpack-plugin';
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import type { WebpackPluginInstance } from 'webpack';
+import 'dotenv/config';
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' ); // eslint-disable-line @typescript-eslint/no-var-requires
 
 export const plugins: WebpackPluginInstance[] = [

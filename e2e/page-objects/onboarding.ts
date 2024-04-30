40ce149e6386274dac52c0b94258e2041b59cb72
Gio Lodi <gio@mokacoding.com>
2024-04-30 14:32:15 +1000

    Makes onboarding work!

--- e2e/page-objects/onboarding.ts
+++ e2e/page-objects/onboarding.ts
@@ -3,13 +3,33 @@ import { type Page } from '@playwright/test';
 export default class Onboarding {
 	constructor( private page: Page ) {}
 
-	get locator() {
-		return this.page.getByTestId( 'onboarding' );
+	private get locator() {
+		// This fails, not sure why...
+		// return this.page.getByTestId( 'onboarding' );
+		//
+		// Accessing the Page directly works even though it's confusing because Page is not a Locator
+		return this.page;
 	}
 
 	get siteNameInput() {
 		return this.locator.getByLabel( 'Site name' );
 	}
 
-  // See add-site-modal for more bit we'll likely need here
+	get sitePathInput() {
+		return this.locator.getByLabel( 'Local path' );
+	}
+
+	get continueButton() {
+		return this.locator.getByRole('button', { name: 'Continue' });
+	}
+
+	private get localPathButton() {
+		return this.locator.getByTestId( 'select-path-button' );
+	}
+
+	// This usually opens an OS folder dialog, except we can't interact with it in playwrite.
+	// In tests the dialog returns the value of the E2E_OPEN_FOLDER_DIALOG environment variable.
+	async clickLocalPathButtonAndSelectFromEnv() {
+		await this.localPathButton.click();
+	}
 }

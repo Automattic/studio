6c0d86dd6c9a4ed69df1f2ce0744e98720a156c8
katinthehatsite <katerynakodonenko@gmail.com>
2024-05-06 10:37:23 +0200

    Fix: Cannot create BrowserWindow before app is ready (#79)
    * Add check for when app is ready
    
    * Update EJS dependency
    
    * Revert "Update EJS dependency"
    
    This reverts commit 49eb9faba7e9d75397d626e3a282241d6b2a329b.
    
    ---------
    
    Co-authored-by: Kateryna Kodonenko <kateryna@automattic.com>
    Co-authored-by: sejas <antonio@sejas.es>

--- src/index.ts
+++ src/index.ts
@@ -262,7 +262,7 @@ async function appBoot() {
 	app.on( 'activate', () => {
 		// On OS X it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
-		if ( BrowserWindow.getAllWindows().length === 0 ) {
+		if ( BrowserWindow.getAllWindows().length === 0 && app.isReady() ) {
 			mainWindow = createMainWindow();
 		}
 	} );

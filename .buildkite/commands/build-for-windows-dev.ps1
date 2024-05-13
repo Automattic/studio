99d864190c6d143c92834e131c7ceafb03372651
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-05-13 14:22:14 -0600

    Add Windows Release Builds (#114)
    * Isolate common Windows code
    
    * Add Release Steps with Artifacts
    
    * Better error handling
    
    * DRY builds S3 upload logic (#117)
    
    * Extract logic to distribute builds in Fastlane
    
    * DRY builds folder definition in Fastfile
    
    * Remove leftover debug `puts`
    
    * DRY S3 bucket name
    
    * Fix a comment's wording
    
    * DRY further
    
    * DRY even more
    
    * DRY commit, build, etc in Fastfile
    
    * Casing improvements
    
    ---------
    
    Co-authored-by: Gio Lodi <gio.lodi@automattic.com>

---
+++ .buildkite/commands/build-for-windows-dev.ps1
@@ -0,0 +1,12 @@
+# Stop script execution when a non-terminating error occurs
+$ErrorActionPreference = "Stop"
+
+& "$PSScriptRoot\prepare-windows-host.ps1"
+
+Write-Host "--- :node: Building App"
+node ./scripts/prepare-dev-build-version.mjs
+If ($LastExitCode -ne 0) { Exit $LastExitCode }
+
+npm run make
+If ($LastExitCode -ne 0) { Exit $LastExitCode }
+

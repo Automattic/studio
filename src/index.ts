1b1e1ddd5d4c07adb400a364d6e9c7ceabd8f087
David Calhoun <github@davidcalhoun.me>
2024-05-14 12:23:32 -0400

    fix: Activate event window creation awaits app ready state
    Creating a window before the app is "ready" results in an error.

--- src/index.ts
+++ src/index.ts
@@ -250,7 +250,7 @@ async function appBoot() {
 		// On OS X it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if ( BrowserWindow.getAllWindows().length === 0 ) {
-			createMainWindow();
+			app.whenReady().then( createMainWindow );
 		}
 	} );
 }
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,9 +2,11 @@
  * @jest-environment node
  */
 import fs from 'fs';
+import { createMainWindow } from '../main-window';
 
 jest.mock( 'fs' );
 jest.mock( 'file-stream-rotator' );
+jest.mock( '../main-window' );
 
 const mockUserData = {
 	sites: [],
@@ -65,5 +67,35 @@ it( 'should handle authentication deep links', () => {
 		} );
 
 		Object.defineProperty( process, 'platform', { value: originalProcessPlatform } );
+	} );
+} );
+
+it( 'should await the app ready state before creating a window for activate events', () => {
+	jest.isolateModules( async () => {
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		let activate: ( ...args: any[] ) => void = () => {};
+		jest.doMock( 'electron', () => {
+			const electron = jest.genMockFromModule( 'electron' ) as typeof import('electron');
+			return {
+				...electron,
+				app: {
+					...electron.app,
+					whenReady: jest.fn( () => Promise.resolve() ),
+					on: jest.fn( ( event, callback ) => {
+						if ( event === 'activate' ) {
+							activate = callback;
+						}
+					} ),
+				},
+			};
+		} );
+		require( '../index' );
+
+		activate();
+
+		expect( createMainWindow ).not.toHaveBeenCalled();
+		// Await the mocked `whenReady` promise resolution
+		await new Promise( process.nextTick );
+		expect( createMainWindow ).toHaveBeenCalled();
 	} );
 } );

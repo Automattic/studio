7055846e00ea3ceaf316d9801bd01d260ca08116
Siobhan <siobhan@automattic.com>
2024-05-14 10:02:51 +0100

    refactor: Move shared consts to single file

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,8 @@ export const AUTO_UPDATE_INTERVAL_MS = 60 * 60 * 1000;
 export const WINDOWS_TITLEBAR_HEIGHT = 32;
 export const STUDIO_DOCS_URL = `https://developer.wordpress.com/docs/developer-tools/studio/`;
 export const WPCOM_PROFILE_URL = `https://wordpress.com/me`;
+// OAuth constants
+export const CLIENT_ID = '95109';
+export const PROTOCOL_PREFIX = 'wpcom-local-dev';
+export const WP_AUTHORIZE_ENDPOINT = 'https://public-api.wordpress.com/oauth2/authorize';
+export const SCOPES = 'global';

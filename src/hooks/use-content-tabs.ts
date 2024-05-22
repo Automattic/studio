12758d32e38616ab98132fe3d6f6df348b580c55
Derek Blank <derekpblank@gmail.com>
2024-05-22 18:04:13 +1000

    Add STUDIO_AI feature flag

--- src/hooks/use-content-tabs.ts
+++ src/hooks/use-content-tabs.ts
@@ -1,10 +1,14 @@
 import { useI18n } from '@wordpress/react-i18n';
 import { useMemo } from 'react';
+import { getAppGlobals } from '../lib/app-globals';
 
 export function useContentTabs() {
 	const { __ } = useI18n();
-	return useMemo(
-		() => [
+
+	const assistantEnabled = getAppGlobals().assistantEnabled;
+
+	return useMemo( () => {
+		const tabs = [
 			{
 				name: 'overview',
 				title: __( 'Overview' ),
@@ -17,13 +21,17 @@ export function useContentTabs() {
 				name: 'settings',
 				title: __( 'Settings' ),
 			},
-			{
+		];
+
+		if ( assistantEnabled ) {
+			tabs.push( {
 				name: 'assistant',
 				title: __( 'Assistant' ),
 				className:
 					'components-tab-panel__tabs--assistant ltr:pl-8 rtl:pr-8 ltr:ml-auto rtl:mr-auto',
-			},
-		],
-		[ __ ]
-	);
+			} as { name: string; title: string; className: string } );
+		}
+
+		return tabs;
+	}, [ __, assistantEnabled ] );
 }

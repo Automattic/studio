dc121c251728d36f2a5c61536f5f92f560581dae
Kateryna Kodonenko <kateryna@automattic.com>
2024-06-03 16:35:18 +0200

    Add logic for returning the stdout stderr after WP-CLI executes

--- vendor/wp-now/src/execute-wp-cli.ts
+++ vendor/wp-now/src/execute-wp-cli.ts
@@ -5,6 +5,7 @@ import getWpCliPath from './get-wp-cli-path';
 import getWpNowConfig from './config';
 import { DEFAULT_PHP_VERSION, DEFAULT_WORDPRESS_VERSION } from './constants';
 import { dirname } from 'path';
+import fs from 'fs';
 
 /**
  * This is an unstable API. Multiple wp-cli commands may not work due to a current limitation on php-wasm and pthreads.
@@ -24,20 +25,41 @@ export async function executeWPCli(args: string[]) {
 	});
 	const [, php] = phpInstances;
 
+	const stdoutPath = '/tmp/stdout';
+	const stderrPath = '/tmp/stderr';
+	const phpScriptPath = '/tmp/run-cli.php';
+	const vfsWpCliPath = '/wp-cli/wp-cli.phar';
+
+  
+	const phpScriptContent = `<?php
+ 	putenv('SHELL_PIPE=0');
+  	$GLOBALS['argv'] = array_merge(["/tmp/wp-cli.phar", "--path=${wpNowOptions.documentRoot}"], ${JSON.stringify(args)});
+ 	define('STDIN', fopen('php://stdin', 'rb'));
+  	define('STDOUT', fopen('${stdoutPath}', 'wb'));
+  	define('STDERR', fopen('${stderrPath}', 'wb'));
+  	require '${vfsWpCliPath}';
+ 	?>`;
+
+	 fs.writeFileSync(phpScriptPath, phpScriptContent);
+	 fs.writeFileSync(stdoutPath, '');
+	 fs.writeFileSync(stderrPath, '');
+
 	try {
-		const vfsWpCliPath = '/wp-cli/wp-cli.phar';
 		php.mount(dirname(getWpCliPath()), dirname(vfsWpCliPath));
+
 		await php.cli([
 			'php',
-			vfsWpCliPath,
-			`--path=${wpNowOptions.documentRoot}`,
-			...args,
-		]);
-	} catch (resultOrError) {
-		const success =
-			resultOrError.name === 'ExitStatus' && resultOrError.status === 0;
-		if (!success) {
+			phpScriptPath,
+		  ]);
+	  
+		  const stdoutOutput = fs.readFileSync(stdoutPath, 'utf8');
+		  const stderrOutput = fs.readFileSync(stderrPath, 'utf8');
+	  
+		  return { stdout: stdoutOutput, stderr: stderrOutput };
+		} catch (resultOrError) {
+		  const success = resultOrError.name === 'ExitStatus' && resultOrError.status === 0;
+		  if (!success) {
 			throw resultOrError;
+		  }
 		}
-	}
 }

d310ce43b737708fa4356b602ae549643bc99f7d
Antony Agrios <antonyagrios@gmail.com>
2024-06-03 15:00:08 +0300

    Update: Fix lint errors

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,6 +1,6 @@
-import { PromptUsageProvider } from '../hooks/use-prompt-usage';
 import { InstalledAppsProvider } from '../hooks/use-check-installed-apps';
 import { OnboardingProvider } from '../hooks/use-onboarding';
+import { PromptUsageProvider } from '../hooks/use-prompt-usage';
 import { SiteDetailsProvider } from '../hooks/use-site-details';
 import { ThemeDetailsProvider } from '../hooks/use-theme-details';
 import { DemoSiteUpdateProvider } from '../hooks/use-update-demo-site';

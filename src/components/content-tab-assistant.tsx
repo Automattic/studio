6468adb71e7d131224279f65d070f3a867947fd3
Derek Blank <derekpblank@gmail.com>
2024-06-05 11:25:13 +1000

    Add Assistant Markdown parsing using react-markdown (#200)
    * Add remarkable library
    
    * Integrate remarkable parsing to Assistant messages
    
    * Add default Assistant Markdown syntax highlighting styles
    
    * Add highlight.js package
    
    * Fix Markdown code block spacing
    
    * Update code comments
    
    * Fix Assistant chat horizontal padding
    
    * Mock CSS imports in Jest to avoid syntax errors
    
    - Adds  to mock CSS imports during testing
    - Updates  to include  for CSS files
    - Adds exception for  in
    
    * Move Assistant authenticated views to separate subrenders
    
    * Use react-markdown with react-syntax-highlighter to parse Assistant markdown
    
    * Remove syntax highlighting libraries and use only react-markdown
    
    * Clean up Assistant Markdown styles
    
    * Remove identity-obj-proxy and @types/react-syntax-highlighter
    
    * Remove syntax highlighter code references
    
    * Move assistant markdown styles to index.css
    
    * Mock react-markdown for jest
    
    * Restore code comments
    
    * Remove @types/remarkable
    
    * Remove unneeded moduleNameMapper config
    
    * Remove duplicate SiteDetails interface
    
    * Remove Assistant welcome message
    
    * Update Markdown code syntax styles
    
    * Update Assistant test cases

--- src/components/content-tab-assistant.tsx
+++ src/components/content-tab-assistant.tsx
@@ -2,6 +2,7 @@ import { createInterpolateElement } from '@wordpress/element';
 import { Icon, external } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import React, { useState, useEffect, useRef } from 'react';
+import Markdown from 'react-markdown';
 import { useAssistant } from '../hooks/use-assistant';
 import { useAssistantApi } from '../hooks/use-assistant-api';
 import { useAuth } from '../hooks/use-auth';
@@ -24,14 +25,20 @@ interface MessageProps {
 }
 
 export const Message = ( { children, isUser, className }: MessageProps ) => (
-	<div className={ cx( 'flex mb-2 mt-2', isUser ? 'justify-end' : 'justify-start', className ) }>
+	<div className={ cx( 'flex mt-4', isUser ? 'justify-end' : 'justify-start', className ) }>
 		<div
 			className={ cx(
 				'inline-block p-3 rounded-sm border border-gray-300 lg:max-w-[70%] select-text',
 				! isUser && 'bg-white'
 			) }
 		>
-			{ children }
+			{ typeof children === 'string' ? (
+				<div id="assistant-markdown">
+					<Markdown>{ children }</Markdown>
+				</div>
+			) : (
+				children
+			) }
 		</div>
 	</div>
 );
@@ -98,67 +105,63 @@ export function ContentTabAssistant( { selectedSite }: ContentTabAssistantProps 
 	}, [ isAuthenticated ] );
 
 	const disabled = isOffline || ! isAuthenticated;
+
+	const renderAuthenticatedView = () => (
+		<>
+			{ messages.map( ( message, index ) => (
+				<Message key={ index } isUser={ message.role === 'user' }>
+					{ message.content }
+				</Message>
+			) ) }
+			{ isAssistantThinking && (
+				<Message isUser={ false }>
+					<MessageThinking />
+				</Message>
+			) }
+			<div ref={ endOfMessagesRef } />
+		</>
+	);
+
+	const renderUnauthenticatedView = () => (
+		<Message className="w-full" isUser={ false }>
+			<p className="mb-1.5 a8c-label-semibold">{ __( 'Hold up!' ) }</p>
+			<p>{ __( 'You need to log in to your WordPress.com account to use the assistant.' ) }</p>
+			<p className="mb-1.5">
+				{ createInterpolateElement(
+					__( "If you don't have an account yet, <a>create one for free</a>." ),
+					{
+						a: (
+							<Button
+								variant="link"
+								onClick={ () =>
+									getIpcApi().openURL(
+										'https://wordpress.com/?utm_source=studio&utm_medium=referral&utm_campaign=assistant_onboarding'
+									)
+								}
+							/>
+						),
+					}
+				) }
+			</p>
+			<p className="mb-3">
+				{ __( 'Every account gets 200 prompts included for free each month.' ) }
+			</p>
+			<Button variant="primary" onClick={ authenticate }>
+				{ __( 'Log in to WordPress.com' ) }
+				<Icon className="ltr:ml-1 rtl:mr-1 rtl:scale-x-[-1]" icon={ external } size={ 21 } />
+			</Button>
+		</Message>
+	);
+
 	return (
 		<div className="h-full flex flex-col bg-gray-50">
 			<div
 				data-testid="assistant-chat"
-				className={ cx(
-					'flex-1 overflow-y-auto px-8 py-4',
-					! isAuthenticated && 'flex items-end'
-				) }
+				className={ cx( 'flex-1 overflow-y-auto py-4', ! isAuthenticated && 'flex items-end' ) }
 			>
-				{ isAuthenticated ? (
-					<div data-testid="assistant-chat" className="flex-1 overflow-y-auto p-8">
-						<div className="text-gray-500 mb-4">
-							Welcome to the Studio assistant. I can help manage your site, debug issues, and
-							navigate your way around the WordPress ecosystem.
-						</div>
-						<div>
-							{ messages.map( ( message, index ) => (
-								<Message key={ index } isUser={ message.role === 'user' }>
-									{ message.content }
-								</Message>
-							) ) }
-							{ isAssistantThinking && (
-								<Message isUser={ false }>
-									<MessageThinking />
-								</Message>
-							) }
-							<div ref={ endOfMessagesRef } />
-						</div>
-					</div>
-				) : (
-					<Message className="w-full" isUser={ false }>
-						<p className="mb-1.5 a8c-label-semibold">{ __( 'Hold up!' ) }</p>
-						<p>
-							{ __( 'You need to log in to your WordPress.com account to use the assistant.' ) }
-						</p>
-						<p className="mb-1.5">
-							{ createInterpolateElement(
-								__( "If you don't have an account yet, <a>create one for free</a>." ),
-								{
-									a: (
-										<Button
-											variant="link"
-											onClick={ () =>
-												getIpcApi().openURL(
-													'https://wordpress.com/?utm_source=studio&utm_medium=referral&utm_campaign=assistant_onboarding'
-												)
-											}
-										/>
-									),
-								}
-							) }
-						</p>
-						<p className="mb-3">
-							{ __( 'Every account gets 200 prompts included for free each month.' ) }
-						</p>
-						<Button variant="primary" onClick={ authenticate }>
-							{ __( 'Log in to WordPress.com' ) }
-							<Icon className="ltr:ml-1 rtl:mr-1 rtl:scale-x-[-1]" icon={ external } size={ 21 } />
-						</Button>
-					</Message>
-				) }
+				<div data-testid="assistant-chat" className="flex-1 overflow-y-auto px-8 py-4">
+					{ isAuthenticated ? renderAuthenticatedView() : renderUnauthenticatedView() }
+				</div>
 			</div>
 			<div
 				data-testid="assistant-input"

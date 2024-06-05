2c218d2737b5c0a001eff76be02345bd5270bd6e
Gio Lodi <gio.lodi@automattic.com>
2024-06-05 17:27:30 +1000

    Run macOS E2E tests on Buildkite (#207)
    * WIP
    
    * Makes onboarding work!
    
    * Make create site work
    
    * Make delete site work
    
    * All E2E tests work 🎉
    
    * Fix E2E tearDown cleanup condition
    
    * Cleanup
    
    * Add a check for consistency
    
    * Only run onboarding when needed
    
    * Improve debugging
    
    * Remove useless comment
    
    * Address lint issues
    
    * Run lint, unit, and E2E tests on Buildkite (issue with caching)
    
    * Remove E2E step – It's failing and don't want to slow the unit tests
    
    * Add step to run E2E tests in Buildkite
    
    Currently failing, see https://github.com/Automattic/studio/issues/37
    
    * Add build matrix with Windows and Mac queues for E2E tests
    
    * Call E2E with `bash` explicitly
    
    * Add OS name in E2E tests label
    
    * Add traces to E2E runs
    
    * Fix E2E matrix combinations
    
    * Fix lint issues
    
    * Make paths system agnostic
    
    * Remove no longer valid fixme comment
    
    * Clarify comment
    
    * Use latest CI plugin version
    
    * Use new variables in pipeline
    
    * Fix executable path of Windows build in E2E tests
    
    * Use `throw new Error` instead of undefined `fail` to fail E2E teardown
    
    * Run E2E in Buildkite with `DEBUG=pw:browser`
    
    * Remove a trailing whitespace
    
    * Test - Run unit tests on Windows, too
    
    * Remove Windows test steps in Buildkite
    
    Deferring enabling them till we'll have found a solution for
    Windows-specific issues.
    
    In the meantime, I aim to run E2E macOS tests on Buildkite instead of
    GitHub Actions in all PRs.
    
    * Remove macOS as a target for E2E tests in GitHub Actions
    
    * Extract CI toolkit version in dedicated var
    
    This should make it easier to locate and update the version if needed
    
    * Use shared var to define NVM CI plugin
    
    * Remove `agents: queue: mac` from the pipeline root
    
    We have a variety of steps and platforms in the pipeline now. It no
    longer makes sense to default to macOS.
    
    ---------
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    Co-authored-by: Carlos Garcia <fluiddot@gmail.com>

--- e2e/e2e-helpers.ts
+++ e2e/e2e-helpers.ts
@@ -7,9 +7,15 @@ export async function launchApp( testEnv: NodeJS.ProcessEnv = {} ) {
 
 	// parse the packaged Electron app and find paths and other info
 	const appInfo = parseElectronApp( latestBuild );
+	let executablePath = appInfo.executable;
+	if ( appInfo.platform === 'win32' ) {
+		// `parseElectronApp` function obtains the executable path by finding the first executable from the build folder.
+		// We need to ensure that the executable is the Studio app.
+		executablePath = executablePath.replace( 'Squirrel.exe', 'Studio.exe' );
+	}
 	const electronApp = await electron.launch( {
 		args: [ appInfo.main ], // main file from package.json
-		executablePath: appInfo.executable, // path to the Electron executable
+		executablePath, // path to the Electron executable
 		env: {
 			...process.env,
 			...testEnv,

1c3ef8245fa2b0b7eb0cc078cb73f6fb32020b74
Carlos Garcia <fluiddot@gmail.com>
2024-06-10 18:25:39 +0200

    Run WP-CLI commands without starting a site

--- vendor/wp-now/src/execute-wp-cli.ts
+++ vendor/wp-now/src/execute-wp-cli.ts
@@ -4,6 +4,7 @@ import getWpCliPath from './get-wp-cli-path';
 import getWpNowConfig, { WPNowMode } from './config';
 import { DEFAULT_PHP_VERSION, DEFAULT_WORDPRESS_VERSION } from './constants';
 import { phpVar } from '@php-wasm/util';
+import { NodePHP, PHPLoaderOptions } from '@php-wasm/node';
 
 /**
  * This is an unstable API. Multiple wp-cli commands may not work due to a current limitation on php-wasm and pthreads.
@@ -15,18 +16,13 @@ export async function executeWPCli ( projectPath: string, args: string[] ): Prom
 		wp: DEFAULT_WORDPRESS_VERSION,
 		path: projectPath,
 	});
-	let mode = options.mode;
-	if (options.mode !== WPNowMode.WORDPRESS) {
-		// In case of not having the site projectPath,
-		// we use index mode to avoid other logic like downloading WordPress
-		mode = WPNowMode.INDEX;
-	}
-	const { phpInstances, options: wpNowOptions } = await startWPNow({
-		...options,
-		mode,
-		numberOfPhpInstances: 2,
+	const php: NodePHP = await NodePHP.load(options.phpVersion,{
+		requestHandler: {
+			documentRoot: options.documentRoot,
+			absoluteUrl: options.absoluteUrl,
+		}
 	});
-	const [, php] = phpInstances;
+	php.mount(projectPath, options.documentRoot);
 
 	//Set the SAPI name to cli before running the script
 	await php.setSapiName('cli');
@@ -48,7 +44,7 @@ export async function executeWPCli ( projectPath: string, args: string[] ): Prom
 		// the output as ASCII tables.
 		// @see https://github.com/wp-cli/wp-cli/issues/1102
 		putenv( 'SHELL_PIPE=0' );
-		
+
 		// When running PHP on Playground, the value of constant PHP_OS is set to Linux.
 		// This implies that platform-specific logic won't work as expected. To solve this,
 		// we use an environment variable to ensure WP-CLI runs the Windows-specific logic.

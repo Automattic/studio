4320082e814b586a8b57bfde930e706ae79f3d97
David Calhoun <github@davidcalhoun.me>
2024-06-10 16:38:48 -0400

    refactor: Remove unnecessary quotation

--- bin/wp.bat
+++ bin/wp.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF "%APP_PATH%"=="" (
-  ECHO "Error: APP_PATH is undefined; Studio's bundled 'wp-cli' is only usable in shell sessions originating from Studio's \"Open in Terminal\" feature."
+  ECHO Error: APP_PATH is undefined; Studio's bundled 'wp-cli' is only usable in shell sessions originating from Studio's "Open in Terminal" feature.
   EXIT /B 1
 )
 

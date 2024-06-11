c9ba76d835528f48aa2cf16e7b5fe04701acca98
Carlos Garcia <fluiddot@gmail.com>
2024-06-11 16:53:28 +0200

    Mock `matchMedia`

--- jest-setup.ts
+++ jest-setup.ts
@@ -4,6 +4,22 @@ import nock from 'nock';
 if ( typeof window !== 'undefined' ) {
 	// The ipcListener global is usually defined in preload.ts
 	window.ipcListener = { subscribe: jest.fn() };
+
+	// Mock `matchMedia` as it's not implemented in JSDOM
+	// Reference: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+	Object.defineProperty( window, 'matchMedia', {
+		writable: true,
+		value: jest.fn().mockImplementation( ( query ) => ( {
+			matches: false,
+			media: query,
+			onchange: null,
+			addListener: jest.fn(), // deprecated
+			removeListener: jest.fn(), // deprecated
+			addEventListener: jest.fn(),
+			removeEventListener: jest.fn(),
+			dispatchEvent: jest.fn(),
+		} ) ),
+	} );
 }
 
 nock.disableNetConnect();

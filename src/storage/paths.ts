adb1ec9a6f1a2b2be5108238e2d7ed39ab05aa20
Carlos Garcia <fluiddot@gmail.com>
2024-06-11 18:47:14 +0200

    Override app paths in E2E tests

--- src/storage/paths.ts
+++ src/storage/paths.ts
@@ -2,18 +2,37 @@ import { app } from 'electron';
 import path from 'path';
 
 export function getUserDataFilePath(): string {
+	if ( process.env.E2E && process.env.E2E_APP_DATA_PATH ) {
+		return path.join( process.env.E2E_APP_DATA_PATH, app.getName(), 'appdata-v1.json' );
+	}
 	const appDataPath = app.getPath( 'appData' ); // Resolves to ~/Library/Application Support on macOS
 	return path.join( appDataPath, app.getName(), 'appdata-v1.json' );
 }
 
 export function getServerFilesPath(): string {
+	if ( process.env.E2E && process.env.E2E_APP_DATA_PATH ) {
+		return path.join( process.env.E2E_APP_DATA_PATH, app.getName(), 'server-files' );
+	}
 	const appDataPath = app.getPath( 'appData' ); // Resolves to ~/Library/Application Support on macOS
 	return path.join( appDataPath, app.getName(), 'server-files' );
 }
 
-export const DEFAULT_SITE_PATH = path.join( app?.getPath( 'home' ) || '', 'Studio' );
+export const DEFAULT_SITE_PATH = path.join(
+	( process.env.E2E && process.env.E2E_HOME_PATH
+		? process.env.E2E_HOME_PATH
+		: app?.getPath( 'home' ) ) || '',
+	'Studio'
+);
 
 export function getSiteThumbnailPath( siteId: string ): string {
+	if ( process.env.E2E && process.env.E2E_APP_DATA_PATH ) {
+		return path.join(
+			process.env.E2E_APP_DATA_PATH,
+			app.getName(),
+			'thumbnails',
+			`${ siteId }.png`
+		);
+	}
 	const appDataPath = app.getPath( 'appData' );
 	return path.join( appDataPath, app.getName(), 'thumbnails', `${ siteId }.png` );
 }

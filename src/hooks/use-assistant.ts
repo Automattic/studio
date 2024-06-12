263cec7385ac51e2cff4deea0fd1a6fc684dec96
Kateryna Kodonenko <kateryna@automattic.com>
2024-06-12 15:41:31 +0200

    Add logic for updating messages in useAssistant

--- src/hooks/use-assistant.ts
+++ src/hooks/use-assistant.ts
@@ -1,9 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 
-export interface Message {
+export type Message = {
+	id: number;
 	content: string;
 	role: 'user' | 'assistant';
-}
+	blocks?: {
+		cliOutput?: string;
+		cliStatus?: 'success' | 'error';
+		cliTime?: string;
+		codeBlockContent?: string;
+	}[];
+};
 
 export const useAssistant = ( selectedSiteId: string ) => {
 	const [ messages, setMessages ] = useState< Message[] >( [] );
@@ -11,24 +18,69 @@ export const useAssistant = ( selectedSiteId: string ) => {
 	useEffect( () => {
 		const storedChat = localStorage.getItem( selectedSiteId );
 		if ( storedChat ) {
+			console.log( 'Stored chat localStorage:', JSON.parse( storedChat ) );
 			setMessages( JSON.parse( storedChat ) );
 		} else {
 			localStorage.setItem( selectedSiteId, JSON.stringify( [] ) );
 		}
 	}, [ selectedSiteId ] );
 
-	const addMessage = ( content: string, role: 'user' | 'assistant' ) => {
-		setMessages( ( prevMessages ) => {
-			const updatedMessages = [ ...prevMessages, { content, role } ];
-			localStorage.setItem( selectedSiteId, JSON.stringify( updatedMessages ) );
-			return updatedMessages;
-		} );
-	};
+	const addMessage = useCallback(
+		( content: string, role: 'user' | 'assistant' ) => {
+			setMessages( ( prevMessages ) => {
+				const updatedMessages = [ ...prevMessages, { content, role, id: prevMessages.length } ];
+				console.log( 'Saving messages to localStorage:', updatedMessages );
+				localStorage.setItem( selectedSiteId, JSON.stringify( updatedMessages ) );
+				return updatedMessages;
+			} );
+		},
+		[ selectedSiteId ]
+	);
 
-	const clearMessages = () => {
+	const updateMessage = useCallback(
+		(
+			id: number,
+			codeBlockContent: string,
+			cliOutput?: string,
+			cliStatus?: 'success' | 'error',
+			cliTime?: string
+		) => {
+			setMessages( ( prevMessages ) => {
+				const updatedMessages = prevMessages.map( ( message ) => {
+					if ( message.id !== id ) return message;
+					const updatedBlocks = ( message.blocks || [] ).map( ( block ) =>
+						block.codeBlockContent === codeBlockContent
+							? { ...block, cliOutput, cliStatus, cliTime }
+							: block
+					);
+					const isBlockUpdated = updatedBlocks.find(
+						( block ) => block.codeBlockContent === codeBlockContent
+					);
+					if ( ! isBlockUpdated ) {
+						updatedBlocks.push( { codeBlockContent, cliOutput, cliStatus, cliTime } );
+					}
+					return { ...message, blocks: updatedBlocks };
+				} );
+				console.log( 'Saving messages to localStorage:', updatedMessages );
+				localStorage.setItem( selectedSiteId, JSON.stringify( updatedMessages ) );
+				return updatedMessages;
+			} );
+		},
+		[ selectedSiteId ]
+	);
+
+	const clearMessages = useCallback( () => {
 		setMessages( [] );
 		localStorage.setItem( selectedSiteId, JSON.stringify( [] ) );
-	};
+	}, [ selectedSiteId ] );
 
-	return { messages, addMessage, clearMessages };
+	return useMemo(
+		() => ( {
+			messages,
+			addMessage,
+			updateMessage,
+			clearMessages,
+		} ),
+		[ addMessage, clearMessages, messages, updateMessage ]
+	);
 };

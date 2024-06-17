bbb5b334564c67abba12436aaf826342a0194e61
Carlos Garcia <fluiddot@gmail.com>
2024-06-17 17:03:01 +0200

    Add unit test to cover PHP version populate case

--- src/storage/tests/user-data.test.ts
+++ src/storage/tests/user-data.test.ts
@@ -3,11 +3,12 @@
  */
 // To run tests, execute `npm run test -- src/storage/user-data.test.ts` from the root directory
 import fs from 'fs';
+import { UserData } from '../storage-types';
 import { loadUserData } from '../user-data';
 
 jest.mock( 'fs' );
 
-const mockUserData = {
+const mockedUserData = {
 	sites: [
 		{ name: 'Tristan', path: '/to/tristan' },
 		{ name: 'Arthur', path: '/to/arthur' },
@@ -16,14 +17,24 @@ const mockUserData = {
 	snapshots: [],
 };
 
-( fs as MockedFs ).__setFileContents(
-	'/path/to/app/appData/App Name/appdata-v1.json',
-	JSON.stringify( mockUserData )
-);
-// Assume each site path exists
-( fs.existsSync as jest.Mock ).mockReturnValue( true );
+function mockUserData( data: RecursivePartial< UserData > ) {
+	( fs as MockedFs ).__setFileContents(
+		'/path/to/app/appData/App Name/appdata-v1.json',
+		JSON.stringify( data )
+	);
+}
 
 describe( 'loadUserData', () => {
+	beforeEach( () => {
+		mockUserData( mockedUserData );
+		// Assume each site path exists
+		( fs.existsSync as jest.Mock ).mockReturnValue( true );
+	} );
+
+	afterEach( () => {
+		jest.restoreAllMocks();
+	} );
+
 	test( 'loads user data correctly and sorts sites', async () => {
 		const result = await loadUserData();
 
@@ -38,5 +49,18 @@ describe( 'loadUserData', () => {
 		( fs.existsSync as jest.Mock ).mockImplementation( ( path ) => path === '/to/lancelot' );
 		const result = await loadUserData();
 		expect( result.sites.map( ( sites ) => sites.name ) ).toEqual( [ 'Lancelot' ] );
+	} );
+
+	test( 'populates PHP version when unknown', async () => {
+		mockUserData( {
+			sites: [
+				{ name: 'Arthur', path: '/to/arthur', phpVersion: '8.3' },
+				{ name: 'Lancelot', path: '/to/lancelot', phpVersion: '8.1' },
+				{ name: 'Tristan', path: '/to/tristan' },
+			],
+			snapshots: [],
+		} );
+		const result = await loadUserData();
+		expect( result.sites.map( ( site ) => site.phpVersion ) ).toEqual( [ '8.3', '8.1', '8.0' ] );
 	} );
 } );

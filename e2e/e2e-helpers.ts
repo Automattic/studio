3f7758f84a235fc60a82faf87ea50ed96768b647
Carlos Garcia <fluiddot@gmail.com>
2024-06-18 19:54:59 +0200

    Remove deprecated `launchApp` E2E helper function

--- e2e/e2e-helpers.ts
+++ e2e/e2e-helpers.ts
@@ -46,7 +46,7 @@ export class E2ESession {
 			},
 			timeout: 60_000,
 		} );
-		this.mainWindow = await this.electronApp.firstWindow();
+		this.mainWindow = await this.electronApp.firstWindow( { timeout: 60_000 } );
 	}
 
 	async cleanup() {
@@ -54,30 +54,4 @@ export class E2ESession {
 		// Clean up temporal folder to hold application data
 		fs.rmSync( this.sessionPath, { recursive: true, force: true } );
 	}
-}
-
-export async function launchApp( testEnv: NodeJS.ProcessEnv = {} ) {
-	// find the latest build in the out directory
-	const latestBuild = findLatestBuild();
-
-	// parse the packaged Electron app and find paths and other info
-	const appInfo = parseElectronApp( latestBuild );
-	let executablePath = appInfo.executable;
-	if ( appInfo.platform === 'win32' ) {
-		// `parseElectronApp` function obtains the executable path by finding the first executable from the build folder.
-		// We need to ensure that the executable is the Studio app.
-		executablePath = executablePath.replace( 'Squirrel.exe', 'Studio.exe' );
-	}
-	const electronApp = await electron.launch( {
-		args: [ appInfo.main ], // main file from package.json
-		executablePath, // path to the Electron executable
-		env: {
-			...process.env,
-			...testEnv,
-			E2E: 'true', // allow app to determine whether it's running as an end-to-end test
-		},
-	} );
-	const mainWindow = await electronApp.firstWindow( { timeout: 60_000 } );
-
-	return [ electronApp, mainWindow ] as const;
 }

69520a67c2af43b1c4145740dd53ca900ebd5fb6
Carlos Garcia <fluiddot@gmail.com>
2024-06-20 18:34:22 +0200

    Update word `OK` in the clear conversation message box
    We now use `OK` instead of `Ok`.

--- src/components/ai-clear-history-reminder.tsx
+++ src/components/ai-clear-history-reminder.tsx
@@ -58,7 +58,7 @@ function AIClearHistoryReminder( {
 
 		const { response } = await getIpcApi().showMessageBox( {
 			message: __( 'Are you sure you want to clear the conversation?' ),
-			buttons: [ __( 'Ok' ), __( 'Cancel' ) ],
+			buttons: [ __( 'OK' ), __( 'Cancel' ) ],
 			cancelId: CANCEL_BUTTON_INDEX,
 		} );
 

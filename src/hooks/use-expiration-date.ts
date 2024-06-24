3a0c440f3fa03c16024b97460563fb7a01a43a9b
Kateryna Kodonenko <kateryna@automattic.com>
2024-06-24 11:14:36 +0200

    Remove logic to clear floating snapshots

--- src/hooks/use-expiration-date.ts
+++ src/hooks/use-expiration-date.ts
@@ -21,7 +21,7 @@ export function useExpirationDate( snapshotDate: number ) {
 	const now = new Date();
 	const endDate = addDays( snapshotDate, MAX_DAYS );
 	const difference = endDate.getTime() - now.getTime();
-	let isExpired = false;
+	let isExpired = true;
 	let format: ( keyof Duration )[] = [ 'days', 'hours' ];
 	if ( difference < 0 ) {
 		isExpired = true;

1915e05e24f72916fdcd688afd315c062c5b8366
Kateryna Kodonenko <kateryna@automattic.com>
2024-06-24 16:35:33 +0200

    Add another test with click

--- src/components/tests/content-tab-assistant.test.tsx
+++ src/components/tests/content-tab-assistant.test.tsx
@@ -212,4 +212,23 @@ describe( 'ContentTabAssistant', () => {
 		expect( screen.getByText( 'How to clear cache' ) ).toBeInTheDocument();
 		expect( screen.getByText( 'How to install a plugin' ) ).toBeInTheDocument();
 	} );
+
+	test( 'confirm that Welcome messages are present, the selected prompt is present, other prompts are removed', async () => {
+		render( <ContentTabAssistant selectedSite={ runningSite } /> );
+
+		expect( await screen.findByText( 'Welcome to our service!' ) ).toBeInTheDocument();
+		expect( await screen.findByText( 'How to create a WordPress site' ) ).toBeInTheDocument();
+		expect( await screen.findByText( 'How to install a plugin' ) ).toBeInTheDocument();
+
+		const samplePrompt = await screen.findByRole( 'button', {
+			name: 'How to create a WordPress site',
+		} );
+		expect( samplePrompt ).toBeInTheDocument();
+		fireEvent.click( samplePrompt );
+
+		// Check if the selected prompt is still present and other prompts are removed
+		expect( screen.getByText( 'Welcome to our service!' ) ).toBeInTheDocument();
+		expect( screen.getByText( 'How to create a WordPress site' ) ).toBeInTheDocument();
+		expect( screen.queryByText( 'How to clear cache' ) ).not.toBeInTheDocument();
+	} );
 } );

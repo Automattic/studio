850fe2533153cf47f8a4b32957fc138513f0130c
sejas <antonio@sejas.es>
2024-06-26 13:06:36 +0200

    support multiline wp commands

--- src/hooks/use-is-valid-wp-cli-inline.ts
+++ src/hooks/use-is-valid-wp-cli-inline.ts
@@ -1,7 +1,8 @@
 export function useIsValidWpCliInline( command: string ) {
 	const containsWPCommand = /\bwp\s/.test( command );
-	const wpSingleLine = ! /\n/.test( command );
+	const wpCommandCount = ( command.match( /\bwp\s/g ) || [] ).length;
+	const containsSingleWPCommand = wpCommandCount === 1;
 	const wpContainsPlaceHolders = /<.*>/.test( command ) || /\[.*\]/.test( command );
 	const wpHasPath = /path/i.test( command ) || /\//.test( command );
-	return containsWPCommand && wpSingleLine && ! wpContainsPlaceHolders && ! wpHasPath;
+	return containsWPCommand && containsSingleWPCommand && ! wpContainsPlaceHolders && ! wpHasPath;
 }

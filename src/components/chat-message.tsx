0a0a91c81639f1cceb1b816e8fdc59bbd9ce7997
David Calhoun <github@davidcalhoun.me>
2024-06-27 07:56:54 -0400

    test: Assert Assistant anchor (#313)
    * refactor: Relocate Assistant anchor module to separate file
    
    Enable automated tests given `react-markdown` is currently mocked due to
    its ESM-only build.
    
    * test: Assert Assistant anchor
    
    * test: Prefer `toBeVisible` over `toBeInTheDocument`
    
    The former provides a more accurate assertion.
    
    * refactor: Remove redundant mock
    
    This module is already mocked in the top scope.

--- src/components/chat-message.tsx
+++ src/components/chat-message.tsx
@@ -1,11 +1,8 @@
-import * as Sentry from '@sentry/react';
-import { speak } from '@wordpress/a11y';
 import { __ } from '@wordpress/i18n';
-import Markdown, { ExtraProps } from 'react-markdown';
+import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { useSiteDetails } from '../hooks/use-site-details';
 import { cx } from '../lib/cx';
-import { getIpcApi } from '../lib/get-ipc-api';
+import Anchor from './assistant-anchor';
 import createCodeComponent from './assistant-code-block';
 
 export interface ChatMessageProps {
@@ -93,45 +90,3 @@ export const ChatMessage = ( {
 		</div>
 	);
 };
-
-function Anchor( props: JSX.IntrinsicElements[ 'a' ] & ExtraProps ) {
-	const { href } = props;
-	const { node, className, ...filteredProps } = props;
-	const { selectedSite, startServer, loadingServer } = useSiteDetails();
-
-	return (
-		<a
-			{ ...filteredProps }
-			className={ cx(
-				className,
-				selectedSite && loadingServer[ selectedSite.id ] && 'animate-pulse duration-100 cursor-wait'
-			) }
-			onClick={ async ( e ) => {
-				if ( ! href ) {
-					return;
-				}
-
-				e.preventDefault();
-
-				const urlForStoppedSite =
-					/^https?:\/\/localhost/.test( href ) && selectedSite && ! selectedSite.running;
-				if ( urlForStoppedSite ) {
-					speak( __( 'Starting the server before opening the site link' ) );
-					await startServer( selectedSite?.id );
-				}
-
-				try {
-					getIpcApi().openURL( href );
-				} catch ( error ) {
-					getIpcApi().showMessageBox( {
-						type: 'error',
-						message: __( 'Failed to open link' ),
-						detail: __( 'We were unable to open the link. Please try again.' ),
-						buttons: [ __( 'OK' ) ],
-					} );
-					Sentry.captureException( error );
-				}
-			} }
-		/>
-	);
-}

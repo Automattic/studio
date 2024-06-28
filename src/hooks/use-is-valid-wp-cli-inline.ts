205f17c393071f0f483eccd45a493e6bb6d6e850
sejas <antonio@sejas.es>
2024-06-28 17:06:58 +0200

    Exclude run wp db, shell, and server commands

--- src/hooks/use-is-valid-wp-cli-inline.ts
+++ src/hooks/use-is-valid-wp-cli-inline.ts
@@ -1,6 +1,7 @@
 import { parse } from 'shell-quote';
 
 const PLACEHOLDER_CHAR_BEGIN = [ '<', '[', '{', '(' ];
+const UNSUPPORTED_COMMANDS = [ 'db', 'server', 'shell' ];
 
 export function useIsValidWpCliInline( command: string ) {
 	const wpCliArgs = parse( command )
@@ -14,17 +15,20 @@ export function useIsValidWpCliInline( command: string ) {
 			}
 		} )
 		.filter( Boolean ) as string[];
+	console.log( 'wpCliArgs', wpCliArgs );
 	const wpCommandCount = wpCliArgs.filter( ( arg ) => arg === 'wp' ).length;
 	const containsPath = wpCliArgs.some( ( arg ) => /path/i.test( arg ) || arg.startsWith( '/' ) );
 	const containsPlaceholderArgs = wpCliArgs.some( ( arg ) =>
 		PLACEHOLDER_CHAR_BEGIN.includes( arg[ 0 ] )
 	);
+	const containsUnsupportedCommand = UNSUPPORTED_COMMANDS.includes( wpCliArgs[ 1 ] );
 
 	return (
 		wpCliArgs.length > 0 &&
 		wpCliArgs[ 0 ] === 'wp' &&
 		wpCommandCount === 1 &&
 		! containsPath &&
-		! containsPlaceholderArgs
+		! containsPlaceholderArgs &&
+		! containsUnsupportedCommand
 	);
 }

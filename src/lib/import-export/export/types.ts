9e4ddcfe89c03cc2c89e8c68dfa214f155529db0
Antony Agrios <antonyagrios@gmail.com>
2024-07-16 12:07:55 +0300

    initial commit

---
+++ src/lib/import-export/export/types.ts
@@ -0,0 +1,8 @@
+export interface ExportOptions {
+	sitePath: string;
+	backupPath: string;
+	includeDatabse: boolean;
+	includeUploads: boolean;
+	includePlugins: boolean;
+	includeThemes: boolean;
+}

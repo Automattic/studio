b7e8651bf0ee22219d6616ec33d92930796b072f
Antony Agrios <antonyagrios@gmail.com>
2024-07-16 16:34:55 +0300

    Update: Add more types

--- src/lib/import-export/export/types.ts
+++ src/lib/import-export/export/types.ts
@@ -8,3 +8,34 @@ export interface ExportOptions {
 		themes: boolean;
 	};
 }
+
+// Let's use the importer types when merged
+export interface BackupContents {
+	extractionDirectory: string;
+	sqlFiles: string[];
+	wpContent: {
+		uploads: string[];
+		plugins: string[];
+		themes: string[];
+	};
+	wpConfigFile?: string;
+}
+
+export interface ExportValidator {
+	canHandle( files: string[] ): boolean;
+	filterFiles( files: string[], options: ExportOptions ): BackupContents;
+}
+
+export interface Exporter {
+	canHandle( options: ExportOptions ): boolean;
+	export(
+		backupContents: BackupContents,
+		options: ExportOptions,
+		progressCallback: ( progress: number ) => void
+	): Promise< void >;
+}
+
+export interface ExporterOption {
+	validator: ExportValidator;
+	exporter: new () => Exporter;
+}

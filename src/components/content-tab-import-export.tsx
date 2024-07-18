eaf50ea060fa48ef662b02def66f292f1644d3b2
sejas <antonio@sejas.es>
2024-07-18 18:17:15 +0100

    Add progress bar with auto increment

--- src/components/content-tab-import-export.tsx
+++ src/components/content-tab-import-export.tsx
@@ -5,6 +5,7 @@ import { useDragAndDropFile } from '../hooks/use-drag-and-drop-file';
 import { useSiteDetails } from '../hooks/use-site-details';
 import { getIpcApi } from '../lib/get-ipc-api';
 import Button from './button';
+import { ProgressBarWithAutoIncrement } from './progress-bar';
 
 interface ContentTabImportExportProps {
 	selectedSite: SiteDetails;
@@ -15,7 +16,6 @@ export function ContentTabImportExport( props: ContentTabImportExportProps ) {
 	const { importFile } = useSiteDetails();
 	const { dropRef, isDraggingOver } = useDragAndDropFile< HTMLDivElement >( {
 		onFileDrop: ( file: File ) => {
-			console.log( 'importing file', file.path );
 			importFile( file, props.selectedSite );
 		},
 	} );
@@ -40,18 +40,27 @@ export function ContentTabImportExport( props: ContentTabImportExportProps ) {
 						}
 					) }
 				</div>
-				<div ref={ dropRef } className="w-full">
-					<Button variant="icon" className="w-full">
-						<div className="h-48 w-full rounded-sm border border-zinc-300 flex-col justify-center items-center inline-flex ">
-							<Icon className="fill-a8c-gray-70" icon={ download } />
-							<span className="text-a8c-gray-70 a8c-body-small mt-1">
-								{ isDraggingOver
-									? __( 'Drop file' )
-									: __( 'Drag a file here, or click to select a file' ) }
-							</span>
+				{ props.selectedSite.isImporting ? (
+					<div className="h-48 w-full rounded-sm border border-zinc-300 flex-col justify-center items-center inline-flex">
+						<div className="w-[240px]">
+							<ProgressBarWithAutoIncrement initialValue={ 50 } maxValue={ 95 } increment={ 5 } />
 						</div>
-					</Button>
-				</div>
+						<div className="text-a8c-gray-70 a8c-body mt-4">{ __( 'Importing backup…' ) }</div>
+					</div>
+				) : (
+					<div ref={ dropRef } className="w-full">
+						<Button variant="icon" className="w-full">
+							<div className="h-48 w-full rounded-sm border border-zinc-300 flex-col justify-center items-center inline-flex">
+								<Icon className="fill-a8c-gray-70" icon={ download } />
+								<span className="text-a8c-gray-70 a8c-body-small mt-1">
+									{ isDraggingOver
+										? __( 'Drop file' )
+										: __( 'Drag a file here, or click to select a file' ) }
+								</span>
+							</div>
+						</Button>
+					</div>
+				) }
 			</div>
 			<div className="flex flex-col w-full">
 				<div className="a8c-subtitle-small mb-1">{ __( 'Export' ) }</div>

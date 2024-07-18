44ae9c727722db7d0297196231b5ecf8f7ac9164
sejas <antonio@sejas.es>
2024-07-18 15:37:27 +0100

    Wrap drop are with button

--- src/components/content-tab-import-export.tsx
+++ src/components/content-tab-import-export.tsx
@@ -7,7 +7,7 @@ import Button from './button';
 interface ContentTabImportExportProps {
 	selectedSite: SiteDetails;
 }
-export function ContentTabImportExport( _props: ContentTabImportExportProps ) {
+export function ContentTabImportExport( props: ContentTabImportExportProps ) {
 	const { __ } = useI18n();
 	return (
 		<div className="p-8 flex flex-col justify-between gap-8">
@@ -30,12 +30,14 @@ export function ContentTabImportExport( _props: ContentTabImportExportProps ) {
 						}
 					) }
 				</div>
-				<div className="h-48 rounded-sm border border-zinc-300 flex-col justify-center items-center inline-flex ">
-					<Icon className="fill-a8c-gray-70" icon={ download } />
-					<span className="text-a8c-gray-70 a8c-body-small mt-1">
-						{ __( 'Drag a file here, or click to select a file' ) }
-					</span>
-				</div>
+				<Button variant="icon" className="w-full">
+					<div className="h-48 w-full rounded-sm border border-zinc-300 flex-col justify-center items-center inline-flex ">
+						<Icon className="fill-a8c-gray-70" icon={ download } />
+						<span className="text-a8c-gray-70 a8c-body-small mt-1">
+							{ __( 'Drag a file here, or click to select a file' ) }
+						</span>
+					</div>
+				</Button>
 			</div>
 			<div className="flex flex-col w-full">
 				<div className="a8c-subtitle-small mb-1">{ __( 'Export' ) }</div>

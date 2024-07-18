71b49883a6d5f55f87688049861ce2e550f11990
Antony Agrios <antonyagrios@gmail.com>
2024-07-18 14:21:56 +0300

    Update: Fix minor UI issues

--- src/components/content-tab-import-export.tsx
+++ src/components/content-tab-import-export.tsx
@@ -99,34 +99,6 @@ export const ExportSite = ( {
 };
 
 export function ContentTabImportExport( { selectedSite }: ContentTabImportExportProps ) {
-	/* TODO: Remove before merge*/
-	const [ file, setFile ] = useState< File | null >( null );
-
-	/* TODO: Remove before merge*/
-	const handleFileChange = ( e: React.ChangeEvent< HTMLInputElement > ) => {
-		const selectedFile = e.target.files ? e.target.files[ 0 ] : null;
-		if ( selectedFile ) {
-			setFile( selectedFile );
-		}
-	};
-
-	/* TODO: Remove handleImport before merge*/
-	const handleImport = () => {
-		if ( file ) {
-			try {
-				const backupFile: BackupArchiveInfo = {
-					type: file.type,
-					path: file.path,
-				};
-				getIpcApi().importSite( { id: selectedSite.id, backupFile } );
-			} catch ( error ) {
-				console.error( 'Error importing site:', error );
-			}
-		} else {
-			console.warn( 'No file selected for import' );
-		}
-	};
-
 	return (
 		<div className="flex flex-col p-8 gap-8">
 			<ExportSite onExport={ getIpcApi().exportSite } selectedSite={ selectedSite }></ExportSite>

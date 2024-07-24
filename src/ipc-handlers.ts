be84c1f04c11ed06269138e123044ee441f9bf57
Carlos Garcia <fluiddot@gmail.com>
2024-07-24 14:16:26 +0200

    Sort imports in IPC handlers file

--- src/ipc-handlers.ts
+++ src/ipc-handlers.ts
@@ -22,9 +22,9 @@ import { isEmptyDir, pathExists, isWordPressDirectory, sanitizeFolderName } from
 import { getImageData } from './lib/get-image-data';
 import { exportBackup } from './lib/import-export/export/export-manager';
 import { ExportOptions } from './lib/import-export/export/types';
+import { ImportExportEventData } from './lib/import-export/handle-events';
 import { defaultImporterOptions, importBackup } from './lib/import-export/import/import-manager';
 import { BackupArchiveInfo } from './lib/import-export/import/types';
-import { ImportExportEventData } from './lib/import-export/handle-events';
 import { isErrnoException } from './lib/is-errno-exception';
 import { isInstalled } from './lib/is-installed';
 import { getLocaleData, getSupportedLocale } from './lib/locale';

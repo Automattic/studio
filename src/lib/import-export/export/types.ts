f35a3b2b94240cd6032915a0895f40276ca9cdef
Jeroen Pfeil <jeroen.pfeil@automattic.com>
2024-07-26 21:43:51 +0700

    Remove unused types

--- src/lib/import-export/export/types.ts
+++ src/lib/import-export/export/types.ts
@@ -23,17 +23,7 @@ export interface BackupContents {
 	wpConfigFile?: string;
 }
 
-export interface ExportValidatorArguments {
-	files: string[];
-	options: ExportOptions;
-}
-
-export interface ExportValidator extends Partial< EventEmitter > {
-	canHandle( args: ExportValidatorArguments ): boolean;
-	filterFiles( args: ExportValidatorArguments ): BackupContents;
-}
-
-export interface Exporter extends Partial< EventEmitter >{
+export interface Exporter extends Partial< EventEmitter > {
 	canHandle(): Promise< boolean >;
 	export(): Promise< void >;
 }

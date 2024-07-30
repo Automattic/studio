036de787cd05f27e4ff9ef61364e98247b38a673
Jeroen P <jeroenpf@users.noreply.github.com>
2024-07-30 09:20:36 +0700

    Refactor the export manager logic (#391)
    * Refactor the export manager logic
    
    * Remove unused types
    
    * Fix import order
    
    * Fix linting issue
    
    * Fix rebase issues
    
    * rename to camel case
    
    * Remove options parameter from methods
    
    * Revert removal of event emit
    
    * Remove site id from export options
    
    * Remove mock commenet

--- src/lib/import-export/tests/export/export-manager.test.ts
+++ src/lib/import-export/tests/export/export-manager.test.ts
@@ -1,96 +1,63 @@
-import fs from 'fs/promises';
-import path from 'path';
 import { exportBackup } from '../../export/export-manager';
-import { ExportOptions, ExporterOption, BackupContents } from '../../export/types';
-
-jest.mock( 'fs/promises' );
-jest.mock( 'path' );
-jest.mock( '../../export/validators/wordpress-validator' );
-jest.mock( '../../export/exporters' );
+import { ExportOptions, NewExporter } from '../../export/types';
 
 describe( 'exportBackup', () => {
-	const mockSitePath = '/mock/site/path';
-	const mockExportOptions: ExportOptions = {
-		sitePath: mockSitePath,
-		backupFile: '/mock/backup/path/backup.tar.gz',
-		includes: {
-			database: true,
-			uploads: true,
-			plugins: true,
-			themes: true,
-		},
-	};
-
-	const mockFiles = [
-		'wp-config.php',
-		'wp-content/uploads/image.jpg',
-		'wp-content/plugins/plugin.php',
-		'wp-content/themes/theme/style.css',
-	];
-
-	const expectedFiles = mockFiles.map( ( file ) => `${ mockSitePath }/${ file }` );
-
+	let mockExportOptions: ExportOptions;
 	beforeEach( () => {
-		jest.clearAllMocks();
-		( fs.readdir as jest.Mock ).mockResolvedValue(
-			mockFiles.map( ( file ) => ( {
-				isFile: () => true,
-				path: mockSitePath,
-				name: file,
-			} ) )
-		);
-		( path.join as jest.Mock ).mockImplementation( ( ...args ) => args.join( '/' ) );
+		mockExportOptions = {} as ExportOptions;
+		console.log = jest.fn();
 	} );
 
-	it( 'should call the correct exporter when validator can handle files', async () => {
-		const mockValidator = {
-			canHandle: jest.fn().mockReturnValue( true ),
-			filterFiles: jest.fn().mockReturnValue( {} as BackupContents ),
-			on: jest.fn(),
-			emit: jest.fn(),
-		};
-		const mockExportMethod = jest.fn().mockResolvedValue( undefined );
+	it( 'should call export on the first exporter that can handle the options', async () => {
+		const mockExport = jest.fn();
+		const mockCanHandle = jest.fn().mockResolvedValue( true );
 
-		const MockExporter = jest.fn().mockImplementation( () => ( {
-			export: mockExportMethod,
+		const MockExporter1 = jest.fn().mockImplementation( () => ( {
+			canHandle: mockCanHandle,
+			export: mockExport,
 			on: jest.fn(),
 			emit: jest.fn(),
 		} ) );
-		const options: ExporterOption[] = [ { validator: mockValidator, exporter: MockExporter } ];
 
-		await exportBackup( mockExportOptions, jest.fn(), options );
-
-		expect( mockValidator.canHandle ).toHaveBeenCalledWith( expectedFiles );
-		expect( mockValidator.filterFiles ).toHaveBeenCalledWith( expectedFiles, mockExportOptions );
-		expect( MockExporter ).toHaveBeenCalled();
-		expect( mockExportMethod ).toHaveBeenCalledWith( mockExportOptions );
-	} );
-
-	it( 'should not call exporter when no validator can handle files', async () => {
-		const mockValidator = {
-			canHandle: jest.fn().mockReturnValue( false ),
-			filterFiles: jest.fn(),
-			on: jest.fn(),
-			emit: jest.fn(),
-		};
-		const MockExporter = jest.fn().mockImplementation( () => ( {
-			export: jest.fn(),
+		const MockExporter2 = jest.fn().mockImplementation( () => ( {
+			canHandle: mockCanHandle,
+			export: mockExport,
 			on: jest.fn(),
 			emit: jest.fn(),
 		} ) );
-		const options: ExporterOption[] = [ { validator: mockValidator, exporter: MockExporter } ];
 
-		await exportBackup( mockExportOptions, jest.fn(), options );
+		const exporters: NewExporter[] = [ MockExporter1, MockExporter2 ];
+		await exportBackup( mockExportOptions, jest.fn(), exporters );
 
-		expect( mockValidator.canHandle ).toHaveBeenCalledWith( expectedFiles );
-		expect( mockValidator.filterFiles ).not.toHaveBeenCalled();
-		expect( MockExporter ).not.toHaveBeenCalled();
+		expect( MockExporter1 ).toHaveBeenCalledWith( mockExportOptions );
+		expect( mockCanHandle ).toHaveBeenCalled();
+		expect( mockExport ).toHaveBeenCalled();
+		expect( MockExporter2 ).not.toHaveBeenCalled();
 	} );
 
-	it( 'should throw an error if fs.readdir fails', async () => {
-		const fsError = new Error( 'File system error' );
-		( fs.readdir as jest.Mock ).mockRejectedValue( fsError );
+	it( 'should call the second exporter if first exporter can not handle export', async () => {
+		const ExportMethod1 = jest.fn();
+		const MockExporter1 = jest.fn().mockImplementation( () => ( {
+			canHandle: jest.fn().mockResolvedValue( false ),
+			export: ExportMethod1,
+			on: jest.fn(),
+			emit: jest.fn(),
+		} ) );
 
-		await expect( exportBackup( mockExportOptions, jest.fn() ) ).rejects.toThrow( fsError );
+		const ExportMethod2 = jest.fn();
+		const MockExporter2 = jest.fn().mockImplementation( () => ( {
+			canHandle: jest.fn().mockResolvedValue( true ),
+			export: ExportMethod2,
+			on: jest.fn(),
+			emit: jest.fn(),
+		} ) );
+
+		const exporters: NewExporter[] = [ MockExporter1, MockExporter2 ];
+		await exportBackup( mockExportOptions, jest.fn(), exporters );
+
+		expect( MockExporter1 ).toHaveBeenCalledWith( mockExportOptions );
+		expect( ExportMethod1 ).not.toHaveBeenCalled();
+		expect( MockExporter2 ).toHaveBeenCalledWith( mockExportOptions );
+		expect( ExportMethod2 ).toHaveBeenCalled();
 	} );
 } );

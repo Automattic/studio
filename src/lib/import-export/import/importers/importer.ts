5597bc462b26a34166290e7d0542913399d88bc2
Wojtek Naruniec <wojtek@naruniec.me>
2024-08-07 17:01:23 +0200

    Refactor to account for another backup file importer

--- src/lib/import-export/import/importers/importer.ts
+++ src/lib/import-export/import/importers/importer.ts
@@ -23,12 +23,80 @@ abstract class BaseImporter extends EventEmitter implements Importer {
 	}
 
 	abstract import( rootPath: string, siteId: string ): Promise< ImporterResult >;
+}
 
-	protected async importDatabase(
-		rootPath: string,
-		siteId: string,
-		sqlFiles: string[]
-	): Promise< void > {
+export abstract class BaseBackupImporter extends BaseImporter implements Importer {
+	async import( rootPath: string, siteId: string ): Promise< ImporterResult > {
+		this.emit( ImportEvents.IMPORT_START );
+
+		try {
+			const databaseDir = path.join( rootPath, 'wp-content', 'database' );
+			const dbPath = path.join( databaseDir, '.ht.sqlite' );
+			const dbImporter = new DatabaseImporter();
+
+			await this.moveExistingDatabaseToTrash( dbPath );
+			await this.createEmptyDatabase( dbPath );
+			await dbImporter.importDatabase( rootPath, siteId, this.backup.sqlFiles );
+			await this.importWpContent( rootPath );
+			let meta: MetaFileData | undefined;
+			if ( this.backup.metaFile ) {
+				meta = await this.parseMetaFile();
+			}
+
+			this.emit( ImportEvents.IMPORT_COMPLETE );
+			return {
+				extractionDirectory: this.backup.extractionDirectory,
+				sqlFiles: this.backup.sqlFiles,
+				wpContent: this.backup.wpContent,
+				meta,
+			};
+		} catch ( error ) {
+			this.emit( ImportEvents.IMPORT_ERROR, error );
+			throw error;
+		}
+	}
+
+	protected abstract parseMetaFile(): Promise< MetaFileData | undefined >;
+
+	protected async createEmptyDatabase( dbPath: string ): Promise< void > {
+		await fsPromises.writeFile( dbPath, '' );
+	}
+
+	protected async moveExistingDatabaseToTrash( dbPath: string ): Promise< void > {
+		if ( ! fs.existsSync( dbPath ) ) {
+			return;
+		}
+		await shell.trashItem( dbPath );
+	}
+
+	protected async importWpContent( rootPath: string ): Promise< void > {
+		this.emit( ImportEvents.IMPORT_WP_CONTENT_START );
+		const extractionDirectory = this.backup.extractionDirectory;
+		const wpContent = this.backup.wpContent;
+		const wpContentDir = path.join( rootPath, 'wp-content' );
+		for ( const files of Object.values( wpContent ) ) {
+			for ( const file of files ) {
+				const stats = await lstat( file );
+				// Skip if it's a directory
+				if ( stats.isDirectory() ) {
+					continue;
+				}
+				const relativePath = path.relative( path.join( extractionDirectory, 'wp-content' ), file );
+				const destPath = path.join( wpContentDir, relativePath );
+				await fsPromises.mkdir( path.dirname( destPath ), { recursive: true } );
+				await fsPromises.copyFile( file, destPath );
+			}
+		}
+		this.emit( ImportEvents.IMPORT_WP_CONTENT_COMPLETE );
+	}
+}
+
+export class DatabaseImporter extends EventEmitter {
+	constructor() {
+		super();
+	}
+
+	async importDatabase( rootPath: string, siteId: string, sqlFiles: string[] ): Promise< void > {
 		if ( ! sqlFiles.length ) {
 			return;
 		}
@@ -78,68 +146,25 @@ abstract class BaseImporter extends EventEmitter implements Importer {
 	}
 }
 
-export class JetpackImporter extends BaseImporter {
-	async import( rootPath: string, siteId: string ): Promise< ImporterResult > {
-		this.emit( ImportEvents.IMPORT_START );
-
-		try {
-			const databaseDir = path.join( rootPath, 'wp-content', 'database' );
-			const dbPath = path.join( databaseDir, '.ht.sqlite' );
-
-			await this.moveExistingDatabaseToTrash( dbPath );
-			await this.createEmptyDatabase( dbPath );
-			await this.importDatabase( rootPath, siteId, this.backup.sqlFiles );
-			await this.importWpContent( rootPath );
-			let meta: MetaFileData | undefined;
-			if ( this.backup.metaFile ) {
-				meta = await this.parseMetaFile();
-			}
-
-			this.emit( ImportEvents.IMPORT_COMPLETE );
-			return {
-				extractionDirectory: this.backup.extractionDirectory,
-				sqlFiles: this.backup.sqlFiles,
-				wpContent: this.backup.wpContent,
-				meta,
-			};
-		} catch ( error ) {
-			this.emit( ImportEvents.IMPORT_ERROR, error );
-			throw error;
-		}
-	}
-
-	protected async createEmptyDatabase( dbPath: string ): Promise< void > {
-		await fsPromises.writeFile( dbPath, '' );
-	}
-
-	protected async moveExistingDatabaseToTrash( dbPath: string ): Promise< void > {
-		if ( ! fs.existsSync( dbPath ) ) {
+export class JetpackImporter extends BaseBackupImporter {
+	protected async parseMetaFile(): Promise< MetaFileData | undefined > {
+		const metaFilePath = this.backup.metaFile;
+		if ( ! metaFilePath ) {
 			return;
 		}
-		await shell.trashItem( dbPath );
-	}
-
-	protected async importWpContent( rootPath: string ): Promise< void > {
-		this.emit( ImportEvents.IMPORT_WP_CONTENT_START );
-		const extractionDirectory = this.backup.extractionDirectory;
-		const wpContent = this.backup.wpContent;
-		const wpContentDir = path.join( rootPath, 'wp-content' );
-		for ( const files of Object.values( wpContent ) ) {
-			for ( const file of files ) {
-				const stats = await lstat( file );
-				// Skip if it's a directory
-				if ( stats.isDirectory() ) {
-					continue;
-				}
-				const relativePath = path.relative( path.join( extractionDirectory, 'wp-content' ), file );
-				const destPath = path.join( wpContentDir, relativePath );
-				await fsPromises.mkdir( path.dirname( destPath ), { recursive: true } );
-				await fsPromises.copyFile( file, destPath );
-			}
+		this.emit( ImportEvents.IMPORT_META_START );
+		try {
+			const metaContent = await fsPromises.readFile( metaFilePath, 'utf-8' );
+			return JSON.parse( metaContent );
+		} catch ( e ) {
+			return;
+		} finally {
+			this.emit( ImportEvents.IMPORT_META_COMPLETE );
 		}
-		this.emit( ImportEvents.IMPORT_WP_CONTENT_COMPLETE );
 	}
+}
 
+export class LocalImporter extends BaseBackupImporter {
 	protected async parseMetaFile(): Promise< MetaFileData | undefined > {
 		const metaFilePath = this.backup.metaFile;
 		if ( ! metaFilePath ) {
@@ -158,11 +183,16 @@ export class JetpackImporter extends BaseImporter {
 }
 
 export class SQLImporter extends BaseImporter {
+	constructor( protected backup: BackupContents ) {
+		super( backup );
+	}
+
 	async import( rootPath: string, siteId: string ): Promise< ImporterResult > {
+		const dbImporter = new DatabaseImporter();
 		this.emit( ImportEvents.IMPORT_START );
 
 		try {
-			await this.importDatabase( rootPath, siteId, this.backup.sqlFiles );
+			await dbImporter.importDatabase( rootPath, siteId, this.backup.sqlFiles );
 
 			this.emit( ImportEvents.IMPORT_COMPLETE );
 			return {

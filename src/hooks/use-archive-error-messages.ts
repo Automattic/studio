2b8f33792c1541886611a8aa4bd3a906f5ee3ddf
Wojtek Naruniec <wojtek@naruniec.me>
2024-08-08 14:55:27 +0200

    Clean up space limit constants and increase it to 250 MB

--- src/hooks/use-archive-error-messages.ts
+++ src/hooks/use-archive-error-messages.ts
@@ -1,8 +1,7 @@
 import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import { useMemo } from 'react';
-
-const SIZE_LIMIT_MB = 100;
+import { SIZE_LIMIT_MB} from "../constants";
 
 export function useArchiveErrorMessages() {
 	const { __ } = useI18n();

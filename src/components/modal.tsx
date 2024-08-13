c2f2163141e2a327921048d5fc2aa16cc456dae9
Antonio Sejas <antonio.sejas@automattic.com>
2024-08-13 09:29:28 +0100

    Translate the tooltip for the close button on the modal #454
    * Translates the tooltip of the close button in all the modals

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,10 +1,13 @@
 import { Modal as WPModal } from '@wordpress/components';
+import { useI18n } from '@wordpress/react-i18n';
 import { cx } from '../lib/cx';
 import type { ComponentProps } from 'react';
 
 export default function Modal( { className, ...rest }: ComponentProps< typeof WPModal > ) {
+	const { __ } = useI18n();
 	return (
 		<WPModal
+			closeButtonLabel={ __( 'Close' ) }
 			className={ cx( 'select-none [&_h1]:!text-xl [&_h1]:!font-normal outline-0', className ) }
 			{ ...rest }
 		/>

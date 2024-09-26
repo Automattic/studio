2f19efe14e385970388935d1508982373cb2e9c2
Antonio Sejas <antonio@sejas.es>
2024-09-26 16:44:38 +0100

    Remove pasting the command to avoid many permission alerts

--- src/ipc-handlers.ts
+++ src/ipc-handlers.ts
@@ -672,16 +672,7 @@ export function openTerminalAtPath(
 				if not application "Terminal" is running then launch
 				do script "${ wpCliEnabled ? loadWpCliCommand : '' } cd ${ targetPath } && clear"
 				activate
-			end tell
-
-			delay 0.5 -- Small delay to ensure the terminal is ready
-
-			tell application "System Events"
-				tell process "Terminal"
-					keystroke "v" using command down -- Simulate Cmd+V to paste the clipboard
-				end tell
-			end tell
-			`;
+			end tell`;
 			command = `osascript -e '${ script }'`;
 		} else if ( platform === 'linux' ) {
 			// Linux

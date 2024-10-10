55ce7bb2af7cd150929e6d4d01f3bddf47c81f57
Wojtek Naruniec <wojtek@naruniec.me>
2024-10-10 10:20:02 +0200

    Move constant to fix translations

--- src/lib/generate-site-name.ts
+++ src/lib/generate-site-name.ts
@@ -1,29 +1,28 @@
 import { __ } from '@wordpress/i18n';
 
-const siteNames = [
-	__( 'My Bold Website' ),
-	__( 'My Bright Website' ),
-	__( 'My Blissful Website' ),
-	__( 'My Calm Website' ),
-	__( 'My Cool Website' ),
-	__( 'My Dreamy Website' ),
-	__( 'My Elite Website' ),
-	__( 'My Fresh Website' ),
-	__( 'My Glowing Website' ),
-	__( 'My Happy Website' ),
-	__( 'My Joyful Website' ),
-	__( 'My Noble Website' ),
-	__( 'My Pure Website' ),
-	__( 'My Peak Website' ),
-	__( 'My Prime Website' ),
-	__( 'My Serene Website' ),
-	__( 'My Shiny Website' ),
-	__( 'My Sparkly Website' ),
-	__( 'My Swift Website' ),
-	__( 'My True Website' ),
-];
-
 export function generateSiteName( usedSiteNames: string[] ): string {
+	const siteNames = [
+		__( 'My Bold Website' ),
+		__( 'My Bright Website' ),
+		__( 'My Blissful Website' ),
+		__( 'My Calm Website' ),
+		__( 'My Cool Website' ),
+		__( 'My Dreamy Website' ),
+		__( 'My Elite Website' ),
+		__( 'My Fresh Website' ),
+		__( 'My Glowing Website' ),
+		__( 'My Happy Website' ),
+		__( 'My Joyful Website' ),
+		__( 'My Noble Website' ),
+		__( 'My Pure Website' ),
+		__( 'My Peak Website' ),
+		__( 'My Prime Website' ),
+		__( 'My Serene Website' ),
+		__( 'My Shiny Website' ),
+		__( 'My Sparkly Website' ),
+		__( 'My Swift Website' ),
+		__( 'My True Website' ),
+	];
 	let proposedName = __( 'My WordPress Website' );
 	let tryCount = 0;
 

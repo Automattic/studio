afdcae6304f85dbb250c0e12d57d008133338054
Kateryna Kodonenko <kateryna@automattic.com>
2024-10-11 10:42:15 +0200

    Add logo for wpcom and improve svg positions

--- src/components/content-tab-sync.tsx
+++ src/components/content-tab-sync.tsx
@@ -2,18 +2,19 @@ import { Icon } from '@wordpress/components';
 import { check } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import { PropsWithChildren } from 'react';
+import wpcomLogo from '../../assets/wpcom-logo.svg';
 import Button from './button';
 import { SyncTabImage } from './sync-tab-image';
 
-function SiteSyncDesription( {
-	children,
-	selectedSite,
-}: PropsWithChildren< { selectedSite: SiteDetails } > ) {
+function SiteSyncDesription( { children }: PropsWithChildren< { selectedSite: SiteDetails } > ) {
 	const { __ } = useI18n();
 	return (
-		<div className="p-8 flex justify-between max-w-3xl gap-4">
-			<div className="flex flex-col">
-				<div className="a8c-subtitle mb-1">{ __( 'Sync with' ) }</div>
+		<div className="flex justify-between max-w-3xl gap-4">
+			<div className="flex flex-col p-8">
+				<div className="flex items-center mb-1">
+					<div className="a8c-subtitle">{ __( 'Sync with' ) }</div>
+					<img src={ wpcomLogo } alt="WordPress.com Logo" className="ml-2 h-5" />
+				</div>
 				<div className="w-[40ch] text-a8c-gray-70 a8c-body pr-2">
 					{ __(
 						'Connect an existing WordPress.com site, or create a new one and share your site with the world.'
@@ -32,7 +33,7 @@ function SiteSyncDesription( {
 				</div>
 				{ children }
 			</div>
-			<div className="flex flex-col shrink-0 items-end">
+			<div className="flex flex-col shrink-0 items-end p-4">
 				<SyncTabImage />
 			</div>
 		</div>

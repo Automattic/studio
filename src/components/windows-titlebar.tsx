407317ada821d28925a2c788a68b9c8f9626867a
Philip Jackson <p-jackson@users.noreply.github.com>
2024-10-15 09:18:14 +1300

    Ensure Windows' application menu button is always visible by leaving space for window controls (#596)
    * Use window frame language to make space for windows controls
    
    * Use "logical" CSS rule padding between header controls
    
    Using padding-inline-start instead of padding-left, since it
    automatically deals with RTL cases.
    
    ---------
    
    Co-authored-by: Philip Jackson <p-jackon@live.com>

--- src/components/windows-titlebar.tsx
+++ src/components/windows-titlebar.tsx
@@ -6,6 +6,15 @@ import { cx } from '../lib/cx';
 import { getIpcApi } from '../lib/get-ipc-api';
 import Button from './button';
 
+interface HasTextInfo {
+	textInfo: { direction: 'ltr' | 'rtl' };
+}
+
+// If this breaks in a future Electron version, we might need to change `textInfo` to `getTextInfo()`
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/getTextInfo
+const isWindowFrameRtl =
+	'rtl' === ( new Intl.Locale( navigator.language ) as unknown as HasTextInfo ).textInfo.direction;
+
 export default function WindowsTitlebar( {
 	className,
 	children,
@@ -14,7 +23,20 @@ export default function WindowsTitlebar( {
 	children?: React.ReactNode;
 } ) {
 	return (
-		<HStack alignment="left" className={ cx( 'bg-chrome text-white', className ) } spacing="2">
+		<HStack
+			alignment="left"
+			className={ cx(
+				'bg-chrome text-white',
+				// Leave space for window controls depending on which side they are on
+				// Take into account the "chrome" padding, the position of which depends on the language direction
+				isWindowFrameRtl &&
+					'ltr:pl-window-controls-width-win rtl:pl-window-controls-width-excl-chrome-win',
+				! isWindowFrameRtl &&
+					'ltr:pr-window-controls-width-excl-chrome-win rtl:pr-window-controls-width-win',
+				className
+			) }
+			spacing="2"
+		>
 			<Button
 				variant="icon"
 				onClick={ () => {
@@ -29,7 +51,7 @@ export default function WindowsTitlebar( {
 				<img src={ appIcon } alt="" className="w-[16px] flex-shrink-0" />
 				<h1 className="text-xs">{ getAppGlobals().appName }</h1>
 			</div>
-			<div className="flex-1 pl-2 pr-32">{ children }</div>
+			<div className="flex-1 ps-2">{ children }</div>
 		</HStack>
 	);
 }

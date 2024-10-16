c964d26aaa38fa1d975a9066d8c86bb128025177
Antonio Sejas <antonio@sejas.es>
2024-10-16 14:03:08 +0100

    Create useSyncSites to display the unspported plans, and enable hosting features

--- src/components/sites-sync-modal-selector.tsx
+++ src/components/sites-sync-modal-selector.tsx
@@ -1,5 +1,6 @@
 import { SearchControl } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
+import { SyncSite, useSyncSites } from '../hooks/use-sync-sites';
 import { cx } from '../lib/cx';
 import { Badge } from './badge';
 import Button from './button';
@@ -8,6 +9,7 @@ import { WordPressShortLogo } from './wordpress-short-logo';
 
 export function SitesSyncModalSelector( { onRequestClose }: { onRequestClose: () => void } ) {
 	const { __ } = useI18n();
+	const { syncSites } = useSyncSites();
 	return (
 		<Modal
 			className="w-3/5 h-full max-h-[84vh] [&>div]:!p-0"
@@ -15,7 +17,7 @@ export function SitesSyncModalSelector( { onRequestClose }: { onRequestClose: ()
 			title={ __( 'Connect a WordPress.com site' ) }
 		>
 			<SearchSites />
-			<ListSites />
+			<ListSites syncSites={ syncSites } />
 			<Footer onRequestClose={ onRequestClose } />
 		</Modal>
 	);
@@ -39,59 +41,43 @@ function SearchSites() {
 	);
 }
 
-function ListSites() {
+function ListSites( { syncSites }: { syncSites: SyncSite[] } ) {
 	return (
 		<div className="flex flex-col gap-4 overflow-y-auto h-[calc(84vh-230px)]">
-			<SiteItem
-				site={ {
-					name: 'Example Site',
-					url: 'https://example.com',
-					supportedPlan: false,
-					hasStaging: true,
-				} }
-			/>
-			{ Array.from( { length: 10 } ).map( ( _, index ) => (
-				<SiteItem
-					key={ index }
-					site={ {
-						name: `Example Site ${ index }`,
-						url: `https://example.com/${ index }`,
-						supportedPlan: true,
-						hasStaging: true,
-					} }
-				/>
+			{ syncSites.map( ( site ) => (
+				<SiteItem key={ site.id } site={ site } />
 			) ) }
 		</div>
 	);
 }
 
-function SiteItem( {
-	site,
-}: {
-	site: {
-		name: string;
-		url: string;
-		supportedPlan: boolean;
-		hasStaging: boolean;
-	};
-} ) {
+function SiteItem( { site }: { site: SyncSite } ) {
 	const { __ } = useI18n();
+	if ( site.isStaging ) {
+		return null;
+	}
+	const isSyncable = site.syncSupport === 'syncable';
+	const isNeedsTransfer = site.syncSupport === 'needs-transfer';
+	const isUnsupported = site.syncSupport === 'unsupported';
 	return (
 		<div className="flex py-3 px-8 items-center border-b border-a8c-gray-0 justify-between">
 			<div className="flex flex-col gap-0.5">
-				<div className={ cx( 'a8c-body', ! site.supportedPlan && 'text-a8c-gray-30' ) }>
-					{ site.name }
-				</div>
+				<div className={ cx( 'a8c-body', ! isSyncable && 'text-a8c-gray-30' ) }>{ site.name }</div>
 				<div className="a8c-body-small text-a8c-gray-30">{ site.url }</div>
 			</div>
-			{ site.supportedPlan && (
+			{ isSyncable && (
 				<div className="flex gap-2">
 					<Badge className="bg-a8c-green-5 text-a8c-green-80"> { __( 'Production' ) }</Badge>
-					{ site.hasStaging && <Badge> { __( 'Staging' ) }</Badge> }
+					{ site.stagingSiteIds.length > 0 && <Badge>{ __( 'Staging' ) }</Badge> }
 				</div>
 			) }
-			{ ! site.supportedPlan && (
+			{ isUnsupported && (
 				<div className="a8c-body-small text-a8c-gray-30"> { __( 'Unsupported plan' ) }</div>
+			) }
+			{ isNeedsTransfer && (
+				<div className="a8c-body-small text-a8c-gray-30">
+					{ __( 'Please enable hosting features' ) }
+				</div>
 			) }
 		</div>
 	);

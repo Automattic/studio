8447dff6287011b5b631fb61c1f95115255552cc
Antonio Sejas <antonio@sejas.es>
2024-10-16 14:47:49 +0100

    add select feature

--- src/components/sites-sync-modal-selector.tsx
+++ src/components/sites-sync-modal-selector.tsx
@@ -1,6 +1,6 @@
 import { SearchControl } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
-import { isSync } from 'tar/dist/commonjs/options';
+import { useState } from 'react';
 import { SyncSite, useSyncSites } from '../hooks/use-sync-sites';
 import { cx } from '../lib/cx';
 import { Badge } from './badge';
@@ -11,6 +11,7 @@ import { WordPressShortLogo } from './wordpress-short-logo';
 export function SitesSyncModalSelector( { onRequestClose }: { onRequestClose: () => void } ) {
 	const { __ } = useI18n();
 	const { syncSites } = useSyncSites();
+	const [ selectedSiteId, setSelectedSiteId ] = useState< number | null >( null );
 	return (
 		<Modal
 			className="w-3/5 h-full max-h-[84vh] [&>div]:!p-0"
@@ -18,7 +19,11 @@ export function SitesSyncModalSelector( { onRequestClose }: { onRequestClose: ()
 			title={ __( 'Connect a WordPress.com site' ) }
 		>
 			<SearchSites />
-			<ListSites syncSites={ syncSites } />
+			<ListSites
+				syncSites={ syncSites }
+				selectedSiteId={ selectedSiteId }
+				onSelectSite={ ( id ) => setSelectedSiteId( id ) }
+			/>
 			<Footer onRequestClose={ onRequestClose } />
 		</Modal>
 	);
@@ -42,17 +47,38 @@ function SearchSites() {
 	);
 }
 
-function ListSites( { syncSites }: { syncSites: SyncSite[] } ) {
+function ListSites( {
+	syncSites,
+	selectedSiteId,
+	onSelectSite,
+}: {
+	syncSites: SyncSite[];
+	selectedSiteId: null | number;
+	onSelectSite: ( id: number ) => void;
+} ) {
 	return (
 		<div className="flex flex-col overflow-y-auto h-[calc(84vh-230px)]">
 			{ syncSites.map( ( site ) => (
-				<SiteItem key={ site.id } site={ site } />
+				<SiteItem
+					key={ site.id }
+					site={ site }
+					isSelected={ site.id === selectedSiteId }
+					onClick={ () => onSelectSite( site.id ) }
+				/>
 			) ) }
 		</div>
 	);
 }
 
-function SiteItem( { site }: { site: SyncSite } ) {
+function SiteItem( {
+	site,
+	isSelected,
+	onClick,
+}: {
+	site: SyncSite;
+	isSelected: boolean;
+	onClick: () => void;
+} ) {
 	const { __ } = useI18n();
 	if ( site.isStaging ) {
 		return null;
@@ -60,27 +86,19 @@ function SiteItem( { site }: { site: SyncSite } ) {
 	const isSyncable = site.syncSupport === 'syncable';
 	const isNeedsTransfer = site.syncSupport === 'needs-transfer';
 	const isUnsupported = site.syncSupport === 'unsupported';
-	const isSelected = false;
 	return (
 		<div
 			className={ cx(
-				'flex py-3 px-8 items-center border-b border-a8c-gray-0 justify-between group',
-				isSelected && 'hover:bg-a8c-blueberry hover:text-white',
+				'flex py-3 px-8 items-center border-b border-a8c-gray-0 justify-between',
+				isSelected && 'bg-a8c-blueberry text-white',
 				! isSelected && isSyncable && 'hover:bg-a8c-blueberry-5'
 			) }
 			role={ isSyncable ? 'button' : undefined }
-			onClick={ () => {
-				console.log( 'clicked' );
-			} }
+			onClick={ onClick }
 		>
 			<div className="flex flex-col gap-0.5">
 				<div className={ cx( 'a8c-body', ! isSyncable && 'text-a8c-gray-30' ) }>{ site.name }</div>
-				<div
-					className={ cx(
-						'a8c-body-small text-a8c-gray-30',
-						isSelected && 'group-hover:text-white'
-					) }
-				>
+				<div className={ cx( 'a8c-body-small text-a8c-gray-30', isSelected && 'text-white' ) }>
 					{ site.url }
 				</div>
 			</div>
@@ -89,17 +107,13 @@ function SiteItem( { site }: { site: SyncSite } ) {
 					<Badge
 						className={ cx(
 							'bg-a8c-green-5 text-a8c-green-80',
-							isSelected && 'group-hover:bg-white group-hover:text-a8c-blueberry'
+							isSelected && 'bg-white text-a8c-blueberry'
 						) }
 					>
 						{ __( 'Production' ) }
 					</Badge>
 					{ site.stagingSiteIds.length > 0 && (
-						<Badge
-							className={ cx(
-								isSelected && 'group-hover:bg-white group-hover:text-a8c-blueberry'
-							) }
-						>
+						<Badge className={ cx( isSelected && 'bg-white text-a8c-blueberry' ) }>
 							{ __( 'Staging' ) }
 						</Badge>
 					) }

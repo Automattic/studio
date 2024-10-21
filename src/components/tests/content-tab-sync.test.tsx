a511dc2cf060fb018bc5e319703b1f6cb699e73b
Antonio Sejas <antonio@sejas.es>
2024-10-21 10:27:19 +0100

    add tests

--- src/components/tests/content-tab-sync.test.tsx
+++ src/components/tests/content-tab-sync.test.tsx
@@ -1,11 +1,13 @@
 // To run tests, execute `npm run test -- src/components/tests/content-tab-sync.test.tsx` from the root directory
 import { render, screen, fireEvent } from '@testing-library/react';
 import { useAuth } from '../../hooks/use-auth';
+import { useSyncSites } from '../../hooks/use-sync-sites';
 import { getIpcApi } from '../../lib/get-ipc-api';
 import { ContentTabSync } from '../content-tab-sync';
 
 jest.mock( '../../hooks/use-auth' );
 jest.mock( '../../lib/get-ipc-api' );
+jest.mock( '../../hooks/use-sync-sites' );
 
 const selectedSite: SiteDetails = {
 	name: 'Test Site',
@@ -25,6 +27,11 @@ describe( 'ContentTabSync', () => {
 			openURL: jest.fn(),
 			generateProposedSitePath: jest.fn(),
 			showMessageBox: jest.fn(),
+		} );
+		( useSyncSites as jest.Mock ).mockReturnValue( {
+			syncSites: [],
+			connectedSites: [],
+			setConnectedSites: jest.fn(),
 		} );
 	} );
 
@@ -70,5 +77,123 @@ describe( 'ContentTabSync', () => {
 		const connectSiteButton = screen.getByRole( 'button', { name: 'Connect site' } );
 		fireEvent.click( connectSiteButton );
 		expect( screen.getByText( 'Connect a WordPress.com site' ) ).toBeInTheDocument();
+	} );
+
+	it( 'displays the list of connected sites', async () => {
+		const fakeSyncSite = {
+			id: 6,
+			name: 'My simple business site that needs a transfer',
+			url: 'https:/developer.wordpress.com/studio/',
+			isStaging: false,
+			stagingSiteIds: [],
+			syncSupport: 'syncable',
+		};
+		( useAuth as jest.Mock ).mockReturnValue( { isAuthenticated: true, authenticate: jest.fn() } );
+		( useSyncSites as jest.Mock ).mockReturnValue( {
+			syncSites: [ fakeSyncSite ],
+			connectedSites: [ fakeSyncSite ],
+			setConnectedSites: jest.fn(),
+		} );
+		render( <ContentTabSync selectedSite={ selectedSite } /> );
+
+		const title = screen.getByText( 'My simple business site that needs a transfer' );
+		expect( title ).toBeInTheDocument();
+
+		const disconnectButton = screen.getByRole( 'button', { name: 'Disconnect' } );
+		expect( disconnectButton ).toBeInTheDocument();
+
+		const pullButton = screen.getByRole( 'button', { name: 'Pull' } );
+		expect( pullButton ).toBeInTheDocument();
+
+		const pushButton = screen.getByRole( 'button', { name: 'Push' } );
+		expect( pushButton ).toBeInTheDocument();
+
+		const productionText = screen.getByText( 'Production' );
+		expect( productionText ).toBeInTheDocument();
+	} );
+
+	it( 'opens URL for connected sites', async () => {
+		const fakeSyncSite = {
+			id: 6,
+			name: 'My simple business site that needs a transfer',
+			url: 'https:/developer.wordpress.com/studio/',
+			isStaging: false,
+			stagingSiteIds: [],
+			syncSupport: 'syncable',
+		};
+		( useAuth as jest.Mock ).mockReturnValue( { isAuthenticated: true, authenticate: jest.fn() } );
+		( useSyncSites as jest.Mock ).mockReturnValue( {
+			syncSites: [ fakeSyncSite ],
+			connectedSites: [ fakeSyncSite ],
+			setConnectedSites: jest.fn(),
+		} );
+		render( <ContentTabSync selectedSite={ selectedSite } /> );
+
+		const urlButton = screen.getByRole( 'button', {
+			name: 'https:/developer.wordpress.com/studio/ ↗',
+		} );
+		expect( urlButton ).toBeInTheDocument();
+
+		fireEvent.click( urlButton );
+		expect( getIpcApi().openURL ).toHaveBeenCalledWith( 'https:/developer.wordpress.com/studio/' );
+	} );
+
+	it( 'displays both production and staging sites when a production site is connected', async () => {
+		const fakeProductionSite = {
+			id: 6,
+			name: 'My simple business site',
+			url: 'https://developer.wordpress.com/studio/',
+			isStaging: false,
+			stagingSiteIds: [ 7 ],
+			syncSupport: 'syncable',
+		};
+		const fakeStagingSite = {
+			id: 7,
+			name: 'Staging: My simple business site',
+			url: 'https://developer-staging.wordpress.com/studio/',
+			isStaging: true,
+			stagingSiteIds: [],
+			syncSupport: 'syncable',
+		};
+		( useAuth as jest.Mock ).mockReturnValue( { isAuthenticated: true, authenticate: jest.fn() } );
+		( useSyncSites as jest.Mock ).mockReturnValue( {
+			syncSites: [ fakeProductionSite, fakeStagingSite ],
+			connectedSites: [ fakeProductionSite ],
+			setConnectedSites: jest.fn(),
+		} );
+		render( <ContentTabSync selectedSite={ selectedSite } /> );
+
+		// Check for production site
+		const productionTitle = screen.getByText( 'My simple business site' );
+		expect( productionTitle ).toBeInTheDocument();
+		const productionText = screen.getByText( 'Production' );
+		expect( productionText ).toBeInTheDocument();
+
+		// Check for staging site where title is not displayed
+		const stagingTitle = screen.queryByText( 'Staging: My simple business site' );
+		expect( stagingTitle ).not.toBeInTheDocument();
+		const stagingText = screen.getByText( 'Staging' );
+		expect( stagingText ).toBeInTheDocument();
+
+		// Check for buttons on both sites, with only one disconnect button
+		const disconnectButtons = screen.getAllByRole( 'button', { name: 'Disconnect' } );
+		expect( disconnectButtons ).toHaveLength( 1 );
+
+		const pullButtons = screen.getAllByRole( 'button', { name: 'Pull' } );
+		expect( pullButtons ).toHaveLength( 2 );
+
+		const pushButtons = screen.getAllByRole( 'button', { name: 'Push' } );
+		expect( pushButtons ).toHaveLength( 2 );
+
+		// Check for URLs
+		const productionUrl = screen.getAllByRole( 'button', {
+			name: 'https://developer.wordpress.com/studio/ ↗',
+		} );
+		expect( productionUrl ).toHaveLength( 1 );
+
+		const stagingUrl = screen.getAllByRole( 'button', {
+			name: 'https://developer-staging.wordpress.com/studio/ ↗',
+		} );
+		expect( stagingUrl ).toHaveLength( 1 );
 	} );
 } );

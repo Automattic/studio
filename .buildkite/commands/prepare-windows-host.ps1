72bb5b4542670d7de3bdda1c1347ba3fa87ffee9
Gio Lodi <gio@mokacoding.com>
2024-10-23 14:16:46 +1100

    Add note regarding Windows 10 SDK

--- .buildkite/commands/prepare-windows-host.ps1
+++ .buildkite/commands/prepare-windows-host.ps1
@@ -60,6 +60,9 @@ Write-Host "--- :windows: Setting up Package Manager"
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
+# Building AppX with Electron Forge requires the Windows 10 SDK
+#
+# See https://github.com/electron/forge/blob/4e7517c11f46b87a1e7b2a31b7c7ca39b0ee0a97/packages/maker/appx/README.md?plain=1#L5
 & "$PSScriptRoot\install-windows-10-sdk.ps1"
 If ($LastExitCode -ne 0) { Exit $LastExitCode }
 

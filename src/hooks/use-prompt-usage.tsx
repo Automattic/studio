8aea8fdbd6ce37458f3ce0942f7906191c585e2d
Volodymyr Makukha <nei.css@gmail.com>
2024-10-30 16:33:35 +0000

    fix(assistant): fix number of monthly requests

--- src/hooks/use-prompt-usage.tsx
+++ src/hooks/use-prompt-usage.tsx
@@ -1,6 +1,5 @@
 import * as Sentry from '@sentry/electron/renderer';
 import { useState, useEffect, useCallback, useMemo, createContext, useContext } from 'react';
-import { LIMIT_OF_PROMPTS_PER_USER } from '../constants';
 import { useAuth } from './use-auth';
 
 type PromptUsage = {
@@ -13,7 +12,7 @@ type PromptUsage = {
 };
 
 const initState: PromptUsage = {
-	promptLimit: LIMIT_OF_PROMPTS_PER_USER,
+	promptLimit: 0,
 	promptCount: 0,
 	fetchPromptUsage: async () => undefined,
 	updatePromptUsage: ( _data: { maxQuota: string; remainingQuota: string } ) => undefined,
@@ -42,7 +41,7 @@ const calculateDaysRemaining = ( quotaResetDate: string ): number => {
 export function PromptUsageProvider( { children }: PromptUsageProps ) {
 	const { Provider } = promptUsageContext;
 
-	const [ promptLimit, setPromptLimit ] = useState( LIMIT_OF_PROMPTS_PER_USER );
+	const [ promptLimit, setPromptLimit ] = useState( 0 );
 	const [ promptCount, setPromptCount ] = useState( 0 );
 	const [ quotaResetDate, setQuotaResetDate ] = useState( '' );
 	const { client } = useAuth();

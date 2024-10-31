2bb83ff4e8f9d5d64ef25c07654f21979b8d624e
Gio Lodi <gio@mokacoding.com>
2024-10-31 12:30:08 +1100

    Address ESLint warning from VS Code

--- scripts/build-appx-version.mjs
+++ scripts/build-appx-version.mjs
@@ -1,8 +1,7 @@
-import { spawn } from "child_process";
+import { fileURLToPath } from `url`;
 import convertToWindowsStore from "electron2appx";
 import fs from "fs/promises";
 import path from "path";
-import { fileURLToPath } from "url";
 import xml2js from "xml2js";
 
 // FIXME: Read from location shared with what CI uses via .buildkite/commands/install-windows-10-sdk.ps1

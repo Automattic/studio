1864628a25626cb1ee4456fb7c37a707b64ecd9b
Gio Lodi <gio@mokacoding.com>
2024-11-04 13:50:32 +1100

    Also build signed version so we can test the AppX locally

--- scripts/package-appx.mjs
+++ scripts/package-appx.mjs
@@ -5,6 +5,12 @@ import convertToWindowsStore from 'electron2appx';
 
 console.log( '--- :electron: Packaging AppX' );
 
+console.log( 'Verifying WINDOWS_CODE_SIGNING_CERT_PASSWORD env var...' );
+if ( ! process.env.WINDOWS_CODE_SIGNING_CERT_PASSWORD ) {
+	console.error( 'Required env var WINDOWS_CODE_SIGNING_CERT_PASSWORD is not set!' );
+	process.exit( 1 );
+}
+
 const __dirname = path.dirname( fileURLToPath( import.meta.url ) );
 
 const windows10SDKVersionPath = path.resolve( __dirname, '..', '.windows-10-sdk-version' );
@@ -44,13 +50,8 @@ const normalizeWindowsVersion = ( version ) => {
 const appStoreVersion = normalizeWindowsVersion( packageJson.version );
 
 const appxName = packageJson.productName + '-appx';
-const appxOutputPath = path.resolve( outPath, appxName );
 
-console.log(
-	`Creating unsigned .appx for Microsoft Store submission upload at ${ appxOutputPath }...`
-);
-
-await convertToWindowsStore( {
+const sharedOptions = {
 	containerVirtualization: false,
 	inputDirectory: path.resolve( outPath, 'Studio-win32-x64' ),
 	packageVersion: appStoreVersion,
@@ -64,10 +65,30 @@ await convertToWindowsStore( {
 	assets: assetsPath,
 	makePri: true,
 	packageDisplayName: 'Studio by WordPress.com',
-	// See details at https://partner.microsoft.com/en-us/dashboard/products/<id>/identity
-	publisher: 'CN=E2E5A157-746D-4B04-9116-ABE5CB928306',
 	publisherDisplayName: 'Automattic, Inc.',
 	identityName: '22490Automattic.StudiobyWordPress.com',
+};
+
+const appxOutputPathUnsigned = path.resolve( outPath, `${ appxName }-unsigned` );
+console.log(
+	`~~~ Creating unsigned .appx for Microsoft Store submission upload at ${ appxOutputPathUnsigned }...`
+);
+
+await convertToWindowsStore( {
+	...sharedOptions,
+	// See details at https://partner.microsoft.com/en-us/dashboard/products/<id>/identity
+	publisher: 'CN=E2E5A157-746D-4B04-9116-ABE5CB928306',
 	devCert: 'nil', // skip code signing for Store upload
-	outputDirectory: appxOutputPath,
+	outputDirectory: appxOutputPathUnsigned,
+} );
+
+const appxOutputPathSigned = path.resolve( outPath, `${ appxName }-signed` );
+console.log( `~~~ Creating signed .appx for local testing at ${ appxOutputPathSigned }...` );
+
+await convertToWindowsStore( {
+	...sharedOptions,
+	publisher: 'CN=&quot;Automattic, Inc.&quot;, O=&quot;Automattic, Inc.&quot;, S=California, C=US',
+	devCert: 'certificate.pfx',
+	certPass: process.env.WINDOWS_CODE_SIGNING_CERT_PASSWORD,
+	outputDirectory: appxOutputPathSigned,
 } );

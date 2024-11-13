1fcd845ae408c8b99f6f9618231c233cd5849da3
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2024-11-13 10:11:39 +0100

    Handle header tooltip texts for stopped sites

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,6 +9,20 @@ import { SiteManagementActions } from './site-management-actions';
 export default function Header() {
 	const { __ } = useI18n();
 	const { selectedSite: site, startServer, stopServer, loadingServer } = useSiteDetails();
+	const wpAdminTooltipText = site?.url
+		? sprintf(
+				/* translators: wpAdminUrl is the site's WP admin URL */
+				__( 'Open %(wpAdminUrl)s' ),
+				{ wpAdminUrl: `${ site.url }/wp-admin` }
+		  )
+		: __( 'Open WP admin' );
+	const siteTooltipText = site?.url
+		? sprintf(
+				/* translators: siteUrl is the site URL */
+				__( 'Open %(siteUrl)s' ),
+				{ siteUrl: site.url }
+		  )
+		: __( 'Open site' );
 	return (
 		<div
 			data-testid="site-content-header"
@@ -24,11 +38,7 @@ export default function Header() {
 							disabled={ ! site.running }
 							className="[&.is-link]:text-a8c-gray-70 [&.is-link]:hover:text-a8c-blueberry !px-0 h-0 leading-4"
 							onClick={ () => getIpcApi().openSiteURL( site.id, '/wp-admin' ) }
-							tooltipText={ sprintf(
-								/* translators: siteUrl is the site's WP Admin URL */
-								__( 'Open %(siteUrl)s' ),
-								{ siteUrl: `${ site.url }/wp-admin` }
-							) }
+							tooltipText={ wpAdminTooltipText }
 							variant="link"
 						>
 							{ __( 'WP admin' ) }
@@ -42,11 +52,7 @@ export default function Header() {
 							disabled={ ! site.running }
 							className="[&.is-link]:text-a8c-gray-70 [&.is-link]:hover:text-a8c-blueberry !px-0 h-0 leading-4"
 							onClick={ () => getIpcApi().openSiteURL( site.id, '', { autoLogin: false } ) }
-							tooltipText={ sprintf(
-								/* translators: siteUrl is the site URL */
-								__( 'Open %(siteUrl)s' ),
-								{ siteUrl: site.url }
-							) }
+							tooltipText={ siteTooltipText }
 							variant="link"
 						>
 							{

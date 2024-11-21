cd93a5f45daa766f27102d2bba005bf036522ee9
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2024-11-21 16:16:24 +0100

    Test whether `startServer` was called after site import

--- src/hooks/tests/use-import-export.test.tsx
+++ src/hooks/tests/use-import-export.test.tsx
@@ -220,6 +220,8 @@ describe( 'useImportExport hook', () => {
 				body: 'Import completed',
 			} )
 		);
+		expect( useSiteDetails().startServer ).toHaveBeenCalledTimes( 1 );
+		expect( useSiteDetails().startServer ).toHaveBeenCalledWith( SITE_ID );
 	} );
 
 	it( 'shows error message when import fails', async () => {

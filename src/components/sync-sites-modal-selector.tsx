f36408059bc4ac177d7e5b49d9eb891e87720fa5
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2024-11-27 10:28:08 +0100

    Apply bottom margin to `SearchControl`

--- src/components/sync-sites-modal-selector.tsx
+++ src/components/sync-sites-modal-selector.tsx
@@ -110,7 +110,7 @@ function SearchSites( {
 	return (
 		<div className="flex flex-col px-8 pb-6 border-b border-a8c-gray-5">
 			<SearchControl
-				className="w-full mt-0.5"
+				className="w-full mt-0.5 mb-2"
 				placeholder={ __( 'Search sites' ) }
 				onChange={ ( value ) => {
 					setSearchQuery( value );

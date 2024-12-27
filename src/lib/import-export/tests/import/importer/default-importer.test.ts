eb2420dc2533cc0acacf7dca1ae11ba91c84a0b9
Kateryna Kodonenko <kateryna@automattic.com>
2024-12-27 16:02:52 -0500

    Fix tests on default importer

--- src/lib/import-export/tests/import/importer/default-importer.test.ts
+++ src/lib/import-export/tests/import/importer/default-importer.test.ts
@@ -107,7 +107,7 @@ describe( 'JetpackImporter', () => {
 
 			expect( fs.mkdir ).toHaveBeenCalled();
 			expect( fs.copyFile ).toHaveBeenCalledTimes( 4 );
-			expect( fs.readFile ).not.toHaveBeenCalled();
+			expect( fs.readFile ).toHaveBeenCalledTimes( 1 );
 		} );
 
 		it( 'should handle JSON parse error in meta file', async () => {

a331eb2d418dcdf3ca8eb419d8946d08134ef0f3
bcotrim <bernardo.cotrim@a8c.com>
2025-01-20 11:17:23 +0000

    cleanup

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -3,9 +3,6 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import type { WebpackPluginInstance } from 'webpack';
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' ); // eslint-disable-line @typescript-eslint/no-var-requires
 
-// @ts-expect-error - This is injected by webpack.DefinePlugin
-const COMMIT_HASH: string = process.env.COMMIT_HASH;
-
 export const plugins: WebpackPluginInstance[] = [
 	new ForkTsCheckerWebpackPlugin( {
 		logger: 'webpack-infrastructure',

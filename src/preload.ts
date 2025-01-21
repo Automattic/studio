eb2328ba1e41d90da5f860c70da2d87cb1f55316
Kateryna Kodonenko <kateryna@automattic.com>
2025-01-21 10:57:03 +0100

    Fix ipc hander

--- src/preload.ts
+++ src/preload.ts
@@ -102,7 +102,7 @@ const api: IpcApi = {
 		ipcRenderer.invoke( 'getConnectedWpcomSites', localSiteId ),
 	addSyncOperation: ( id: string ) => ipcRenderer.invoke( 'addSyncOperation', id ),
 	clearSyncOperation: ( id: string ) => ipcRenderer.invoke( 'clearSyncOperation', id ),
-	getWpContentSize: ( id: string ) => ipcRenderer.invoke( 'getSiteSize', id ),
+	getWpContentSize: ( id: string ) => ipcRenderer.invoke( 'getWpContentSize', id ),
 	getPathForFile: webUtils.getPathForFile,
 };
 

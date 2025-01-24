689dc60cd5360deb385069dd4d493246e4a43b2b
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-01-24 19:37:59 +0100

    Revert feature flag use

--- src/hooks/use-snapshots.tsx
+++ src/hooks/use-snapshots.tsx
@@ -13,7 +13,6 @@ import React, {
 import { LIMIT_OF_ZIP_SITES_PER_USER } from '../constants';
 import { getIpcApi } from '../lib/get-ipc-api';
 import { useAuth } from './use-auth';
-import { useFeatureFlags } from './use-feature-flags';
 import { useOffline } from './use-offline';
 
 interface SnapshotContextType {
@@ -93,10 +92,7 @@ export const SnapshotProvider: React.FC< { children: ReactNode } > = ( { childre
 	const [ allSnapshots, setAllSnapshots ] = useState< Pick< Snapshot, 'atomicSiteId' >[] | null >(
 		null
 	);
-	const quickDeploysEnabled = useFeatureFlags();
-	const [ snapshotQuota, setSnapshotQuota ] = useState(
-		quickDeploysEnabled ? 100 : LIMIT_OF_ZIP_SITES_PER_USER
-	);
+	const [ snapshotQuota, setSnapshotQuota ] = useState( LIMIT_OF_ZIP_SITES_PER_USER );
 	const [ isLoadingSnapshotUsage, setIsLoadingSnapshotUsage ] = useState( false );
 	const [ snapshotCreationBlocked, setSnapshotCreationBlocked ] = useState( false );
 

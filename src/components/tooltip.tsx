8ac72b40882abadc4c6df4ce4b00a1933cf3e414
Kateryna Kodonenko <kateryna@automattic.com>
2025-01-24 14:55:35 +0100

    Fix text width

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -58,7 +58,7 @@ const Tooltip = ( {
 				>
 					<div className="inline-flex items-center gap-2 max-w-80 rounded py-2 px-2.5 bg-[#101517] text-white animate-[fade_0.5s_ease-in-out_1]">
 						{ icon && <Icon className="fill-white shrink-0  m-[2px]" size={ 16 } icon={ icon } /> }
-						<span className="text-left text-x break-words overflow-hidden">{ text }</span>
+						<span className="text-left text-xs break-words overflow-hidden">{ text }</span>
 					</div>
 				</Popover>
 			) }

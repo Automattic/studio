13bfd784edae44f103bdaa3c879be5faf8218397
Wojtek Naruniec <wojtek@naruniec.me>
2025-01-27 13:34:43 +0100

    Remove unnecessary import

--- src/site-server.ts
+++ src/site-server.ts
@@ -11,7 +11,6 @@ import { pathExists, recursiveCopyDirectory, isEmptyDir } from './lib/fs-utils';
 import { decodePassword } from './lib/passwords';
 import { phpGetThemeDetails } from './lib/php-get-theme-details';
 import { portFinder } from './lib/port-finder';
-import { sanitizeForLogging } from './lib/sanitize-for-logging';
 import { getPreferredSiteLanguage } from './lib/site-language';
 import SiteServerProcess from './lib/site-server-process';
 import WpCliProcess, { MessageCanceled, WpCliResult } from './lib/wp-cli-process';

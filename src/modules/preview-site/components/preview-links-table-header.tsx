724588bc4be0db99ae48c8999d03a0238aaf5cbe
Kateryna Kodonenko <kateryna@automattic.com>
2025-01-29 15:33:49 +0100

    Remove icon

--- src/modules/preview-site/components/preview-links-table-header.tsx
+++ src/modules/preview-site/components/preview-links-table-header.tsx
@@ -1,5 +1,4 @@
 import { __ } from '@wordpress/i18n';
-import { arrowDown, Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 
 export function PreviewLinksTableHeader() {
@@ -9,10 +8,7 @@ export function PreviewLinksTableHeader() {
 			<div className="flex items-center h-12 px-8 text-gray-900 text-xs uppercase">
 				<div className="w-[51%]">{ __( 'Preview link' ) }</div>
 				<div className="flex ml-auto">
-					<div className="w-[110px] flex items-center pl-4">
-						{ __( 'Updated' ) }
-						<Icon icon={ arrowDown } height={ 13 } width={ 16 } />
-					</div>
+					<div className="w-[110px] flex items-center pl-4">{ __( 'Updated' ) }</div>
 					<div className="w-[100px] pl-4">{ __( 'Expires' ) }</div>
 					<div className="w-[60px] text-right">{ __( 'Actions' ) }</div>
 				</div>

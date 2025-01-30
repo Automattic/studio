1f49927817c41b194114f060e51a42022e683f22
Kateryna Kodonenko <kateryna@automattic.com>
2025-01-30 11:06:52 +0100

    Fix notifications

--- src/hooks/use-update-demo-site.tsx
+++ src/hooks/use-update-demo-site.tsx
@@ -5,6 +5,7 @@ import { useCallback, useState, createContext, useContext, useMemo, ReactNode } 
 import { DEMO_SITE_SIZE_LIMIT_BYTES, DEMO_SITE_SIZE_LIMIT_GB } from '../constants';
 import { getIpcApi } from '../lib/get-ipc-api';
 import { useAuth } from './use-auth';
+import { useFeatureFlags } from './use-feature-flags';
 import { useSnapshots } from './use-snapshots';
 
 interface DemoSiteUpdateContextType {
@@ -26,6 +27,7 @@ export const DemoSiteUpdateProvider: React.FC< DemoSiteUpdateProviderProps > = (
 	const { __ } = useI18n();
 	const [ updatingSites, setUpdatingSites ] = useState< Set< number > >( new Set() );
 	const { updateSnapshot } = useSnapshots();
+	const { quickDeploysEnabled } = useFeatureFlags();
 
 	const updateDemoSite = useCallback(
 		async ( snapshot: Snapshot, localSite: SiteDetails ) => {
@@ -45,7 +47,9 @@ export const DemoSiteUpdateProvider: React.FC< DemoSiteUpdateProviderProps > = (
 
 				if ( archiveSizeInBytes > DEMO_SITE_SIZE_LIMIT_BYTES ) {
 					getIpcApi().showErrorMessageBox( {
-						title: __( 'Updating demo site failed' ),
+						title: quickDeploysEnabled
+							? __( 'Updating preview site failed' )
+							: __( 'Updating demo site failed' ),
 						message: sprintf(
 							__(
 								'The site exceeds the maximum size of %dGB. Please remove some files and try again.'
@@ -92,16 +96,23 @@ export const DemoSiteUpdateProvider: React.FC< DemoSiteUpdateProviderProps > = (
 				} );
 				await getIpcApi().showNotification( {
 					title: __( 'Update Successful' ),
-					body: sprintf( __( "Demo site for '%s' has been updated." ), localSite.name ),
+					body: quickDeploysEnabled
+						? sprintf( __( "Preview site for '%s' has been updated." ), localSite.name )
+						: sprintf( __( "Demo site for '%s' has been updated." ), localSite.name ),
 				} );
 				return response;
 			} catch ( error ) {
 				getIpcApi().showErrorMessageBox( {
 					title: __( 'Update failed' ),
-					message: sprintf(
-						__( "We couldn't update the %s demo site. Please try again." ),
-						localSite.name
-					),
+					message: quickDeploysEnabled
+						? sprintf(
+								__( "We couldn't update the %s preview site. Please try again." ),
+								localSite.name
+						  )
+						: sprintf(
+								__( "We couldn't update the %s demo site. Please try again." ),
+								localSite.name
+						  ),
 					error,
 				} );
 				Sentry.captureException( error );
@@ -116,7 +127,7 @@ export const DemoSiteUpdateProvider: React.FC< DemoSiteUpdateProviderProps > = (
 				}
 			}
 		},
-		[ __, client, updateSnapshot ]
+		[ __, client, updateSnapshot, quickDeploysEnabled ]
 	);
 
 	const isDemoSiteUpdating = useCallback(

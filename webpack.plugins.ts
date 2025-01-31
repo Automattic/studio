5c3aae4ec3513aa4f1ae3ebb5eb81fdf6cba0b81
bcotrim <bernardo.cotrim@a8c.com>
2025-01-31 14:06:47 +0000

    adjust development builds

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -5,7 +5,8 @@ const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require( 
 
 const version = process.env.npm_package_version || '';
 const [ baseVersionWithBeta ] = version.split( '-dev.' );
-const isDevBuild = version.includes( '-dev.' ) || process.env.IS_DEV_BUILD;
+const isDevBuild =
+	version.includes( '-dev.' ) || process.env.IS_DEV_BUILD || process.env.NODE_ENV === 'development';
 const sentryRelease = `studio@${ baseVersionWithBeta }`;
 console.log( 'Sentry release version would be:', sentryRelease );
 console.log( 'Sentry environment would be:', isDevBuild ? 'development' : 'production' );

a9b9c962c5cc6fcde6892ddb9d9e34886d8188a2
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-02-04 12:22:40 +0100

    Add back `0-dns-functions.php` mu-plugin

--- vendor/wp-now/src/wp-now.ts
+++ vendor/wp-now/src/wp-now.ts
@@ -544,7 +544,24 @@ set_error_handler(function($severity, $message, $file, $line) {
 			}
 	}
 	add_action('after_setup_theme', 'check_current_theme_availability');
-`
+	`
+	);
+
+	php.writeFile(
+		path.join( PLAYGROUND_INTERNAL_MU_PLUGINS_FOLDER, '0-dns-functions.php' ),
+		`<?php
+		// Polyfill for DNS functions/features which are not currently supported by @php-wasm/node.
+		// See https://github.com/WordPress/wordpress-playground/issues/1042
+		// These specific features are polyfilled so the Jetpack plugin loads correctly, but others should be added as needed.
+		if ( ! function_exists( 'dns_get_record' ) ) {
+			function dns_get_record() {
+				return array();
+			}
+		}
+		if ( ! defined( 'DNS_NS' ) ) {
+			define( 'DNS_NS', 2 );
+		}
+	`
 	);
 
 	php.writeFile(

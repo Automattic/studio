a3a7c75cc2518c9a6cbc667d57e13601fde0b787
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-02-04 12:31:00 +0100

    Remove redundant `dns_get_record` polyfill

--- vendor/wp-now/src/wp-now.ts
+++ vendor/wp-now/src/wp-now.ts
@@ -550,14 +550,9 @@ set_error_handler(function($severity, $message, $file, $line) {
 	php.writeFile(
 		path.join( PLAYGROUND_INTERNAL_MU_PLUGINS_FOLDER, '0-dns-functions.php' ),
 		`<?php
-		// Polyfill for DNS functions/features which are not currently supported by @php-wasm/node.
+		// Polyfill for DNS features which are not currently supported by @php-wasm/node.
 		// See https://github.com/WordPress/wordpress-playground/issues/1042
 		// These specific features are polyfilled so the Jetpack plugin loads correctly, but others should be added as needed.
-		if ( ! function_exists( 'dns_get_record' ) ) {
-			function dns_get_record() {
-				return array();
-			}
-		}
 		if ( ! defined( 'DNS_NS' ) ) {
 			define( 'DNS_NS', 2 );
 		}

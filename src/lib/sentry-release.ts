290c6474f8fdf5cfcbd440483aefed19c5b4f553
bcotrim <bernardo.cotrim@a8c.com>
2025-02-07 11:27:37 +0000

    revert back to the devXXX verion format

--- src/lib/sentry-release.ts
+++ src/lib/sentry-release.ts
@@ -6,7 +6,7 @@ export function getSentryReleaseInfo( version: string ) {
 	// Handle both -dev.HASH and -devN formats for backward compatibility
 	const baseVersionWithBeta = version.replace( /(-dev\..*|-dev\d+)$/, '' );
 	const isDevEnvironment =
-		version.includes( '-dev.' ) ||
+		/-dev\..*|-dev\d+/.test( version ) ||
 		!! process.env.IS_DEV_BUILD ||
 		process.env.NODE_ENV === 'development';
 	const sentryRelease = `studio@${ baseVersionWithBeta }`;

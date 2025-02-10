48780baafcb1292e507cea6ae1301f4e13620f2f
bcotrim <bernardo.cotrim@a8c.com>
2025-02-10 14:32:39 +0000

    fix modal close button position

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -11,6 +11,10 @@ export default function Modal( { className, ...rest }: ComponentProps< typeof WP
 			className={ cx(
 				'select-none [&_h1]:!text-xl [&_h1]:!font-normal outline-0',
 				'[&_h1]:flex-1 [&_h1]:text-start',
+				'[&_[role="document"]>div:first-child_button]:rtl:left-0',
+				'[&_[role="document"]>div:first-child_button]:rtl:right-2',
+				'[&_[role="document"]>div:first-child_button]:ltr:left-2',
+				'[&_[role="document"]>div:first-child_button]:ltr:right-0',
 				className
 			) }
 			{ ...rest }

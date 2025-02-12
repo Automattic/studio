dd42707d05b6e7a580b8cd9c32f3fc7e195f3ed1
Ivan Ottinger <ivan.ottinger@automattic.com>
2025-02-12 15:59:41 +0100

    Remove seconds from the date format
    Co-authored-by: Antonio Sejas <antonio.sejas@automattic.com>

--- src/hooks/use-expiration-date.ts
+++ src/hooks/use-expiration-date.ts
@@ -23,7 +23,6 @@ function formatStringDate(
 					year: 'numeric',
 					hour: '2-digit',
 					minute: '2-digit',
-					second: '2-digit',
 					hour12: false,
 			  };
 	const formatter = new Intl.DateTimeFormat( locale, options );

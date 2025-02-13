1b175d04faa4f0c10c5e445ddfbe8b86c36ee336
Wojtek Naruniec <wojtek@naruniec.me>
2025-02-13 15:23:25 +0100

    Move fix from div containerto tabpanel

--- src/components/site-content-tabs.tsx
+++ src/components/site-content-tabs.tsx
@@ -45,12 +45,7 @@ export function SiteContentTabs() {
 					key={ selectedTab }
 				>
 					{ ( { name } ) => (
-						<div
-							className="h-full overflow-y-auto"
-							style={ {
-								scrollbarGutter: 'stable',
-							} }
-						>
+						<div className="h-full">
 							{ name === 'overview' && <ContentTabOverview selectedSite={ selectedSite } /> }
 							{ name === 'share' && <ContentTabSnapshots selectedSite={ selectedSite } /> }
 							{ name === 'previews' && <ContentTabPreviews selectedSite={ selectedSite } /> }

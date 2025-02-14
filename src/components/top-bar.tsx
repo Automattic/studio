30f7563025c69b74cea20c70a32612b3235393b3
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-02-14 13:49:12 +0100

    Add `Get help` tooltip

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -113,9 +113,11 @@ export default function TopBar( { onToggleSidebar }: TopBarProps ) {
 
 			<div className="app-no-drag-region flex items-center space-x-1.5 rtl:space-x-reverse">
 				<Authentication />
-				<Button onClick={ openDocs } aria-label={ __( 'Get help' ) } variant="icon">
-					<Icon className="text-white" size={ 24 } icon={ help } />
-				</Button>
+				<Tooltip text={ __( 'Get help' ) } placement="bottom-start">
+					<Button onClick={ openDocs } aria-label={ __( 'Get help' ) } variant="icon">
+						<Icon className="text-white" size={ 24 } icon={ help } />
+					</Button>
+				</Tooltip>
 			</div>
 		</div>
 	);

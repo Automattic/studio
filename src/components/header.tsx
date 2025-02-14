e8760b0e3af7156be17430b90251255dd2834b5d
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-02-14 13:50:30 +0100

    Add tooltips to the header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
+import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import { ArrowIcon } from 'src/components/arrow-icon';
 import Button from 'src/components/button';
 import { SiteManagementActions } from 'src/components/site-management-actions';
+import { Tooltip } from 'src/components/tooltip';
 import { useSiteDetails } from 'src/hooks/use-site-details';
 import { getIpcApi } from 'src/lib/get-ipc-api';
 
@@ -19,27 +21,55 @@ export default function Header() {
 						{ site ? site.name : null }
 					</h1>
 					<div className="flex mt-1 gap-x-4">
-						<Button
-							disabled={ ! site.running }
-							className="[&.is-link]:text-a8c-gray-70 [&.is-link]:hover:text-a8c-blueberry !px-0 h-0 leading-4"
-							onClick={ () => getIpcApi().openSiteURL( site.id, '/wp-admin' ) }
-							variant="link"
-						>
-							{ __( 'WP admin' ) }
-							<ArrowIcon />
-						</Button>
-						<Button
-							disabled={ ! site.running }
-							className="[&.is-link]:text-a8c-gray-70 [&.is-link]:hover:text-a8c-blueberry !px-0 h-0 leading-4"
-							onClick={ () => getIpcApi().openSiteURL( site.id, '', { autoLogin: false } ) }
-							variant="link"
-						>
-							{
-								// translators: "Open site" refers to the action, like "to open site"
-								__( 'Open site' )
+						<Tooltip
+							text={
+								site.running
+									? sprintf(
+											/* translators: wpAdminUrl is the site's WP admin URL */
+											__( 'Open %(wpAdminUrl)s' ),
+											{ wpAdminUrl: `${ site.url }/wp-admin` }
+									  )
+									: undefined
 							}
-							<ArrowIcon />
-						</Button>
+							disabled={ ! site.running }
+							placement="top-start"
+						>
+							<Button
+								disabled={ ! site.running }
+								className="[&.is-link]:text-a8c-gray-70 [&.is-link]:hover:text-a8c-blueberry !px-0 h-0 leading-4"
+								onClick={ () => getIpcApi().openSiteURL( site.id, '/wp-admin' ) }
+								variant="link"
+							>
+								{ __( 'WP admin' ) }
+								<ArrowIcon />
+							</Button>
+						</Tooltip>
+						<Tooltip
+							text={
+								site.running
+									? sprintf(
+											/* translators: siteUrl is the site URL */
+											__( 'Open %(siteUrl)s' ),
+											{ siteUrl: site.url }
+									  )
+									: undefined
+							}
+							disabled={ ! site.running }
+							placement="top-start"
+						>
+							<Button
+								disabled={ ! site.running }
+								className="[&.is-link]:text-a8c-gray-70 [&.is-link]:hover:text-a8c-blueberry !px-0 h-0 leading-4"
+								onClick={ () => getIpcApi().openSiteURL( site.id, '', { autoLogin: false } ) }
+								variant="link"
+							>
+								{
+									// translators: "Open site" refers to the action, like "to open site"
+									__( 'Open site' )
+								}
+								<ArrowIcon />
+							</Button>
+						</Tooltip>
 					</div>
 				</div>
 			) }

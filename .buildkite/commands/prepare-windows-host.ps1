7e4a9c29eebb550a6c2b90f25a3924843bf9ff6f
Gio Lodi <gio@mokacoding.com>
2025-02-17 15:40:16 +1100

    Remove call to setup Windows 10 SDK because script does it automatically

--- .buildkite/commands/prepare-windows-host.ps1
+++ .buildkite/commands/prepare-windows-host.ps1
@@ -4,12 +4,6 @@ $ErrorActionPreference = "Stop"
 & "prepare_windows_host_for_node.ps1"
 If ($LastExitCode -ne 0) { Exit $LastExitCode }
 
-# Building AppX requires the Windows 10 SDK
-#
-# See https://github.com/hermit99/electron-windows-store/tree/v2.1.2?tab=readme-ov-file#usage
-& "install_windows_10_sdk.ps1"
-If ($LastExitCode -ne 0) { Exit $LastExitCode }
-
 Write-Host "--- :npm: Installing Node dependencies"
 npm ci
 If ($LastExitCode -ne 0) { Exit $LastExitCode }

f6ee07c4202c0449af8b6f559baa3393022dea9e
Volodymyr Makukha <nei.css@gmail.com>
2025-02-18 12:31:34 +0000

    change default port

--- src/hooks/use-site-details.tsx
+++ src/hooks/use-site-details.tsx
@@ -218,7 +218,7 @@ export function SiteDetailsProvider( { children }: SiteDetailsProviderProps ) {
 						id: tempSiteId,
 						name: siteName || path,
 						path,
-						port: 9999, // Set a temporary port
+						port: -1, // Set a temporary port
 						running: false,
 						isAddingSite: true,
 						phpVersion: '',

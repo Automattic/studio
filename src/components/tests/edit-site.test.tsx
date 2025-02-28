1021642fd47bdd402ac8b8a4b99bf1f2d60e109e
bcotrim <bernardo.cotrim@a8c.com>
2025-02-28 10:37:16 +0000

    fix lint

--- src/components/tests/edit-site.test.tsx
+++ src/components/tests/edit-site.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import EditSite from 'src/components/edit-site';
 

a08278d88c519ba5e6bcd0a48e8388cb86dde504
Riad Benguella <benguella@gmail.com>
2025-03-05 10:32:50 +0100

    Reuse sanitize function

--- src/lib/generate-custom-domain.ts
+++ src/lib/generate-custom-domain.ts
@@ -1,13 +1,10 @@
+import { sanitizeFolderName } from './generate-site-name';
+
 /**
  * Generates a suitable domain name from site name
  */
 export const generateCustomDomainFromSiteName = ( siteName: string ): string => {
-	// Convert the site name to lowercase and replace spaces with hyphens
-	const domainBase = siteName
-		.toLowerCase()
-		.replace( /[^a-z0-9-]/g, '-' ) // Replace non-alphanumeric chars with hyphens
-		.replace( /-+/g, '-' ) // Replace multiple hyphens with single hyphen
-		.replace( /^-|-$/g, '' ); // Remove hyphens from start and end
+	const domainBase = sanitizeFolderName( siteName );
 
 	return `${ domainBase }.wp.local`;
 };

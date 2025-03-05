25604d51200fd6bc426a3f8a8f16f2f779554fb4
bcotrim <bernardo.cotrim@a8c.com>
2025-03-05 16:42:15 +0000

    bring back comment about use effect deps

--- src/components/onboarding.tsx
+++ src/components/onboarding.tsx
@@ -86,6 +86,7 @@ export default function Onboarding() {
 			setDoesPathContainWordPress( isWordPress );
 		};
 		run();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
 
 	const handleSubmit = useCallback(

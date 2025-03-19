6db227ba844ec7ea141259c1aef559a18845db58
bcotrim <bmmcotrim@gmail.com>
2025-03-19 17:32:57 +0000

    Add EADDRINUSE error on Windows when port 80 is already in use (#1090)
    * attempt to fix EADDRINUSE error on Windows
    
    * make test connection to detect when address is in use
    
    * add timeout and platform specific check
    
    * address pr feedback
    
    * simplify condition

--- src/lib/proxy-server.ts
+++ src/lib/proxy-server.ts
@@ -1,9 +1,11 @@
 import { dialog } from 'electron';
 import http from 'http';
+import { createConnection } from 'node:net';
 import { domainToASCII } from 'node:url';
 import * as Sentry from '@sentry/electron/main';
 import { __ } from '@wordpress/i18n';
 import httpProxy from 'http-proxy';
+import { isErrnoException } from 'src/lib/is-errno-exception';
 import { getMainWindow } from 'src/main-window';
 import { SiteServer } from 'src/site-server';
 import { loadUserData } from 'src/storage/user-data';
@@ -81,6 +83,32 @@ export async function startProxyServer(): Promise< boolean > {
 			} );
 		} );
 
+		// On Windows, node doesn't throw an error if port 80 is busy, so we use the net module to explicitly check
+		// if it's possible to establish a TCP connection to that port (meaning it's busy)
+		if ( process.platform === 'win32' ) {
+			await new Promise< void >( ( resolve, reject ) => {
+				const tester = createConnection( { port: 80 }, () => {
+					// If we can connect, port is in use
+					tester.end();
+					reject( new Error( 'EADDRINUSE' ) );
+				} );
+
+				tester.setTimeout( 1000, () => {
+					tester.destroy();
+					reject( new Error( 'EADDRINUSE' ) );
+				} );
+
+				tester.on( 'error', ( err ) => {
+					if ( isErrnoException( err ) && err.code === 'ECONNREFUSED' ) {
+						// Port is available
+						resolve();
+					} else {
+						reject( err );
+					}
+				} );
+			} );
+		}
+
 		await new Promise< void >( ( resolve, reject ) => {
 			proxyServer!
 				.listen( 80, () => {
@@ -97,7 +125,10 @@ export async function startProxyServer(): Promise< boolean > {
 
 		return true;
 	} catch ( error ) {
-		if ( error instanceof Error && 'code' in error && error.code === 'EADDRINUSE' ) {
+		if (
+			( isErrnoException( error ) && error.code === 'EADDRINUSE' ) ||
+			( error instanceof Error && error.message === 'EADDRINUSE' )
+		) {
 			const mainWindow = await getMainWindow();
 			dialog.showMessageBox( mainWindow, {
 				type: 'error',

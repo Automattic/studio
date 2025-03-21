025805f3fb79cb38b43e9f2d704f3600bb57a824
bcotrim <bernardo.cotrim@a8c.com>
2025-03-21 18:47:35 +0000

    studio cli boostrap files

--- bin/studio-cli.sh
+++ bin/studio-cli.sh
@@ -5,7 +5,7 @@ function realpath() {
   /usr/bin/perl -e "use Cwd;print Cwd::abs_path(@ARGV[0])" "$0";
 }
 
-CONTENTS="$(command dirname "$(command dirname "$(command dirname "$(command dirname "$(realpath "$0")")")")")"
+CONTENTS="$(command dirname "$(command dirname "$(command dirname "$(realpath "$0")")")")"
 BINARY_NAME="$(TERM=dumb command ls "$CONTENTS/MacOS/")"
 ELECTRON="$CONTENTS/MacOS/$BINARY_NAME"
 CLI="$CONTENTS/Resources/app/cli/index.js"

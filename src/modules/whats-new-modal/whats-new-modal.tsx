fc0dda3e295e1ef2ba9a8d60737d766a1846df23
Kateryna Kodonenko <kateryna@automattic.com>
2025-03-25 16:39:55 +0100

    Improve styling

--- src/modules/whats-new-modal/whats-new-modal.tsx
+++ src/modules/whats-new-modal/whats-new-modal.tsx
@@ -1,5 +1,7 @@
 import { Guide } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
+import versionSwitchIllustration from './assets/version-switch-illustration.svg';
 
 export function WhatsNewModal() {
 	const [ isOpen, setIsOpen ] = useState( true );
@@ -12,28 +14,41 @@ export function WhatsNewModal() {
 		<Guide
 			onFinish={ () => setIsOpen( false ) }
 			contentLabel="What's New in Studio"
+			className="whats-new-modal !w-[312px] !h-[453px] overflow-hidden"
 			pages={ [
 				{
+					image: (
+						<img
+							src={ versionSwitchIllustration }
+							alt=""
+							className="h-[195px] w-full object-cover"
+						/>
+					),
 					content: (
-						<div>
-							<h2>Welcome to the latest version of Studio!</h2>
-							<p>Let's take a quick tour of what's new.</p>
+						<div className="p-8">
+							<h2 className="text-2xl font-semibold mb-4">
+								{ __( 'Select WordPress and PHP versions in Studio' ) }
+							</h2>
+							<p className="text-gray-600 text-lg">
+								{ __(
+									'Select your preferred WordPress and PHP versions for existing sites or when creating a new one.'
+								) }
+							</p>
+							<div className="mt-4">
+								<a href="#" className="text-[#4741F4] hover:text-[#3732C5]">
+									{ __( 'Learn more' ) }
+								</a>
+							</div>
 						</div>
 					),
 				},
 				{
 					content: (
-						<div>
-							<h2>New Feature 1</h2>
-							<p>Description of the first new feature...</p>
-						</div>
-					),
-				},
-				{
-					content: (
-						<div>
-							<h2>New Feature 2</h2>
-							<p>Description of the second new feature...</p>
+						<div className="p-8">
+							<h2 className="text-2xl font-semibold mb-4">{ __( 'New Feature 2' ) }</h2>
+							<p className="text-gray-600 text-lg">
+								{ __( 'Description of the second feature...' ) }
+							</p>
 						</div>
 					),
 				},

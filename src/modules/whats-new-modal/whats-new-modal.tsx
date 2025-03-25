4180ae352ec9b47f5a6e9bc8b4bc131e06ea7069
Kateryna Kodonenko <kateryna@automattic.com>
2025-03-25 20:40:15 +0100

    Update correct content

--- src/modules/whats-new-modal/whats-new-modal.tsx
+++ src/modules/whats-new-modal/whats-new-modal.tsx
@@ -2,6 +2,7 @@ import { Guide } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
 import { getIpcApi } from 'src/lib/get-ipc-api';
+import previewSitesIllustration from './assets/preview-sites-illustration.svg';
 import versionSwitchIllustration from './assets/version-switch-illustration.svg';
 
 export function WhatsNewModal() {
@@ -55,12 +56,65 @@ export function WhatsNewModal() {
 					),
 				},
 				{
+					image: (
+						<img
+							src={ previewSitesIllustration }
+							alt=""
+							className="h-[173px] w-full object-cover mb-4"
+						/>
+					),
 					content: (
-						<div className="p-8">
-							<h2 className="text-2xl font-semibold mb-4">{ __( 'New Feature 2' ) }</h2>
-							<p className="text-gray-600 text-lg">
-								{ __( 'Description of the second feature...' ) }
+						<div className="px-8 py-4">
+							<h2 className="text-xl mb-4 text-gray-900">
+								{ __( 'Share your work easily with Preview sites' ) }
+							</h2>
+							<p className="text-gray-900 text-m leading-s">
+								{ __(
+									'Quickly generate a publicly accessible URL that you can share with clients and colleagues.'
+								) }
 							</p>
+							<div className="mt-4">
+								<button
+									onClick={ () =>
+										handleClick( 'https://wordpress.com/blog/2025/02/24/studio-preview-sites/' )
+									}
+									className="text-a8c-blueberry text-m leading-s cursor-pointer"
+								>
+									{ __( 'Learn more' ) }
+								</button>
+							</div>
+						</div>
+					),
+				},
+				{
+					image: (
+						<img
+							src={ previewSitesIllustration } //TODO: Add the correct illustration
+							alt=""
+							className="h-[173px] w-full object-cover mb-4"
+						/>
+					),
+					content: (
+						<div className="px-8 py-4">
+							<h2 className="text-xl mb-4 text-gray-900">
+								{ __( 'Edit domain names for exisiting sites' ) }
+							</h2>
+							<p className="text-gray-900 text-m leading-s">
+								{ __(
+									'Torem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.'
+								) }
+							</p>
+							<div className="mt-4">
+								<button
+									onClick={
+										() =>
+											handleClick( 'https://wordpress.com/blog/2025/02/24/studio-preview-sites/' ) //TODO: Add correct link
+									}
+									className="text-a8c-blueberry text-m leading-s cursor-pointer"
+								>
+									{ __( 'Learn more' ) }
+								</button>
+							</div>
 						</div>
 					),
 				},

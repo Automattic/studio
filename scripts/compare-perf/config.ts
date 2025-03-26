87c69896916afb0f0a26772cf2539a21a3354261
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2025-03-26 14:26:00 +0100

    Revert "remove chromium install since we're on electron"
    This reverts commit 3cfde4ee6a06f535a6f4bf9de3b935fb8f78b342.

--- scripts/compare-perf/config.ts
+++ scripts/compare-perf/config.ts
@@ -1,6 +1,6 @@
 const config = {
 	gitRepositoryURL: 'https://github.com/Automattic/studio.git',
-	setupTestRunner: 'npm ci',
+	setupTestRunner: 'npm ci && npx playwright install chromium',
 	setupCommand: 'npm ci && IS_DEV_BUILD=true npm run package',
 	testsPath: '/metrics/tests',
 	testCommand: 'npm run test:metrics',

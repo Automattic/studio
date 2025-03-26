4822f3529a0c44155f3fe158f63a6f3479c39e78
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2025-03-26 13:22:27 +0100

    use clean npm ci

--- scripts/compare-perf/config.ts
+++ scripts/compare-perf/config.ts
@@ -1,8 +1,7 @@
 const config = {
 	gitRepositoryURL: 'https://github.com/Automattic/studio.git',
-	setupTestRunner: 'npm ci --unsafe-perm --no-audit --no-progress',
-	setupCommand:
-		'npm ci --unsafe-perm --no-audit --no-progress && IS_DEV_BUILD=true npm run package',
+	setupTestRunner: 'npm ci',
+	setupCommand: 'npm ci && IS_DEV_BUILD=true npm run package',
 	testsPath: '/metrics/tests',
 	testCommand: 'npm run test:metrics',
 };

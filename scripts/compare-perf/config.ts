3cfde4ee6a06f535a6f4bf9de3b935fb8f78b342
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2025-03-26 14:17:43 +0100

    remove chromium install since we're on electron

--- scripts/compare-perf/config.ts
+++ scripts/compare-perf/config.ts
@@ -1,6 +1,6 @@
 const config = {
 	gitRepositoryURL: 'https://github.com/Automattic/studio.git',
-	setupTestRunner: 'npm ci && npx playwright install chromium',
+	setupTestRunner: 'npm ci',
 	setupCommand: 'npm ci && IS_DEV_BUILD=true npm run package',
 	testsPath: '/metrics/tests',
 	testCommand: 'npm run test:metrics',

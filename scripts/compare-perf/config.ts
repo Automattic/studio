378ecf595cc07f510a7ccc8a0193566ddbf814d8
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2025-03-26 12:27:42 +0100

    make the app before running tests

--- scripts/compare-perf/config.ts
+++ scripts/compare-perf/config.ts
@@ -1,7 +1,7 @@
 const config = {
 	gitRepositoryURL: 'https://github.com/Automattic/studio.git',
 	setupTestRunner: 'npm install',
-	setupCommand: 'npm install && IS_DEV_BUILD=true npm run package',
+	setupCommand: 'npm install && IS_DEV_BUILD=true npm run package && npm run make',
 	testsPath: '/metrics/tests',
 	testCommand: 'npm run test:metrics',
 };

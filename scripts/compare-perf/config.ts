ed5df767090d96d56d2b163de270950eb6912f25
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2025-03-27 11:49:34 +0100

    update runner config

--- scripts/compare-perf/config.ts
+++ scripts/compare-perf/config.ts
@@ -1,9 +1,17 @@
+import path from 'path';
+
+const metricsPath = path.resolve( __dirname, '../../metrics' );
+
 const config = {
 	gitRepositoryURL: 'https://github.com/Automattic/studio.git',
 	setupTestRunner: 'npm ci && npx playwright install chromium',
-	setupCommand: 'npm ci && IS_DEV_BUILD=true npm run package',
-	testsPath: '/metrics/tests',
 	testCommand: 'npm run test:metrics',
+	setupCommand: 'npm ci && IS_DEV_BUILD=true npm run package',
+	testsPath: 'metrics/tests',
+	testFileSuffix: '.test.ts',
+	artifactsPath: path.join( metricsPath, 'artifacts' ),
+	resultsFileSuffix: '.results.json',
+	summaryFileSuffix: '.summary.json',
 };
 
 export default config;

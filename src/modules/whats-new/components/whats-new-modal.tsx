345f6c0548dc57819b2d51d6b9191a8499992d60
Kateryna Kodonenko <kateryna@automattic.com>
2025-03-27 11:21:35 +0100

    Remove focus outline

--- src/modules/whats-new/components/whats-new-modal.tsx
+++ src/modules/whats-new/components/whats-new-modal.tsx
@@ -80,7 +80,8 @@ export default function WhatsNewModal( { showModal, onClose }: WhatsNewModalProp
 			contentLabel={ __( "What's New in Studio" ) }
 			className={ cx(
 				"whats-new-modal !w-[312px] !h-[470px] overflow-hidden [&_button[aria-label='Close']_svg]:fill-white [&_.components-button.is-tertiary]:!outline-1 [&_.components-button.is-tertiary]:!outline-solid [&_.components-button.is-tertiary]:!outline-a8c-blueberry",
-				'[&_*]:select-none'
+				'[&_*]:select-none',
+				'focus:outline-none'
 			) }
 			pages={ WHATS_NEW_PAGES.map( ( { image, title, ...pageContent } ) => ( {
 				image: (

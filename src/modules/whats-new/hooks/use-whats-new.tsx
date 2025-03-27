6e43bc96034898322fac7c96d8801941c2d6fc5f
Kateryna Kodonenko <kateryna@automattic.com>
2025-03-27 11:37:56 +0100

    Use feature flag for constant

--- src/modules/whats-new/hooks/use-whats-new.tsx
+++ src/modules/whats-new/hooks/use-whats-new.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useFeatureFlags } from 'src/hooks/use-feature-flags';
 import { useIpcListener } from 'src/hooks/use-ipc-listener';
 import { useOnboarding } from 'src/hooks/use-onboarding';
 import { useLastSeenVersion } from 'src/modules/whats-new/hooks/use-last-seen-version';
@@ -11,8 +12,11 @@ interface UseWhatsNew {
 export function useWhatsNew(): UseWhatsNew {
 	const [ showWhatsNew, setShowWhatsNew ] = useState( true );
 	const [ manuallyTriggered, setManuallyTriggered ] = useState( false );
+	const [ featureFlagModalSeen, setFeatureFlagModalSeen ] = useState( false );
+
 	const { needsOnboarding } = useOnboarding();
 	const { isNewVersion, updateLastSeenVersion } = useLastSeenVersion();
+	const { whatsNewSectionEnabled } = useFeatureFlags();
 
 	useIpcListener( 'show-whats-new', () => {
 		setManuallyTriggered( true );
@@ -22,11 +26,22 @@ export function useWhatsNew(): UseWhatsNew {
 	const closeWhatsNew = async () => {
 		setShowWhatsNew( false );
 		setManuallyTriggered( false );
+
+		// If the modal was shown because of the feature flag, mark it as seen
+		if ( whatsNewSectionEnabled && ! manuallyTriggered && ! isNewVersion ) {
+			setFeatureFlagModalSeen( true );
+		}
+
 		await updateLastSeenVersion();
 	};
 
+	// Determine if we should show the modal based on the feature flag
+	const shouldForceWhatsNew = whatsNewSectionEnabled && ! featureFlagModalSeen;
+
 	return {
-		showWhatsNew: ( manuallyTriggered || ( showWhatsNew && isNewVersion ) ) && ! needsOnboarding,
+		showWhatsNew:
+			( manuallyTriggered || shouldForceWhatsNew || ( showWhatsNew && isNewVersion ) ) &&
+			! needsOnboarding,
 		closeWhatsNew,
 	};
 }

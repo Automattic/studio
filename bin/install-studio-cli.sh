142fa099cd553e33a433edb30edb23251621defb
Fredrik Rombach Ekelund <fredrik@f26d.dev>
2025-04-09 14:18:45 +0200

    macOS CLI script tweaks (#1148)
    * First pass at improved terminology
    
    * More terminology tweaks
    
    * Don't overwrite existing non-symlink at installation path
    
    * Tweak macOS bootstrap script per @mcsf recommendations
    
    * Simplification
    
    * More nuanced error messages
    
    * Filter for ENOENT exceptions
    
    * Constants over enum

--- bin/install-studio-cli.sh
+++ bin/install-studio-cli.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# This script is used to install the Studio CLI on macOS. It creates a symlink at CLI_SYMLINK_SOURCE
-# (e.g. /usr/local/bin/studio) pointing to the packaged Studio CLI JS file at CLI_SYMLINK_TARGET.
+# This script is used to install the Studio CLI on macOS. It creates a symlink at CLI_SYMLINK_PATH
+# (e.g. /usr/local/bin/studio) pointing to the packaged Studio CLI JS file at CLI_PACKAGED_PATH.
 
 # Exit if any command fails
 set -e
 
-if [ -z "$CLI_SYMLINK_SOURCE" ] || [ -z "$CLI_SYMLINK_TARGET" ]; then
-	echo "Error: CLI_SYMLINK_SOURCE and CLI_SYMLINK_TARGET environment variables must be set"
+if [ -z "$CLI_SYMLINK_PATH" ] || [ -z "$CLI_PACKAGED_PATH" ]; then
+	echo >&2 "Error: CLI_SYMLINK_PATH and CLI_PACKAGED_PATH environment variables must be set"
 	exit 1
 fi
 
-DIRECTORY_PATH=$(dirname "$CLI_SYMLINK_SOURCE")
+CLI_DIRECTORY_PATH=$(dirname "$CLI_SYMLINK_PATH")
 
-rm -f "$CLI_SYMLINK_SOURCE"
-mkdir -p "$DIRECTORY_PATH"
-ln -s "$CLI_SYMLINK_TARGET" "$CLI_SYMLINK_SOURCE"
+[ -h "$CLI_SYMLINK_PATH" ] && rm "$CLI_SYMLINK_PATH"
+mkdir -p "$CLI_DIRECTORY_PATH"
+ln -s "$CLI_PACKAGED_PATH" "$CLI_SYMLINK_PATH"

82907e8d453e4d79e16c0a47859242649e62a3ac
Ivan Ottinger <ivan.ottinger@automattic.com>
2025-04-10 11:55:23 +0200

    What's New Section: Fix translations (#1184)
    * Fix translations
    
    * Revert unintentional change in `PageContent`
    
    * Add translations for external library strings used in the Guide component
    
    * Simplify string addition
    
    Co-authored-by: Wojtek Naruniec <wojtek.naruniec@automattic.com>
    
    ---------
    
    Co-authored-by: Wojtek Naruniec <wojtek.naruniec@automattic.com>

---
+++ src/additional-phrases.ts
@@ -0,0 +1,11 @@
+/**
+ * This file contains translations for strings used in external libraries
+ * that we don't control directly. By including these strings here,
+ * they will be picked up by the translation extraction process.
+ */
+
+import { __ } from '@wordpress/i18n';
+
+// Navigation strings used in external components like Guide
+__( 'Next' );
+__( 'Previous' );

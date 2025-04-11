cefee1c06d0ce5879239950816acc6aa353212ea
Roberto Aranda <roberto.aranda@automattic.com>
2025-04-11 13:46:57 +0200

    Dynamically determine sidebar breakpoint based on tablist width

--- src/hooks/use-sidebar-visibility.ts
+++ src/hooks/use-sidebar-visibility.ts
@@ -1,22 +1,33 @@
 import { useState, useEffect, useCallback } from 'react';
-import { DEFAULT_WIDTH } from 'src/constants';
+import { DEFAULT_WIDTH, SIDEBAR_WIDTH } from 'src/constants';
 import { getIpcApi } from 'src/lib/get-ipc-api';
 
 const SIDEBAR_BREAKPOINT = DEFAULT_WIDTH;
 
 export function useSidebarVisibility() {
+	const [ dynamicBreakPoint, setDynamicBreakPoint ] = useState( SIDEBAR_BREAKPOINT );
 	const [ isSidebarVisible, setIsSidebarVisible ] = useState( true );
 	const [ isLowerThanBreakpoint, setIsLowerThanBreakpoint ] = useState( false );
 
 	useEffect( () => {
 		const handleResize = () => {
-			setIsLowerThanBreakpoint( window.innerWidth < SIDEBAR_BREAKPOINT );
+			const el = document.querySelector( 'div[role="tablist"]' );
+			if (
+				! isLowerThanBreakpoint && // Only recalculate the breakpoint if bigger than the default
+				el?.clientWidth &&
+				el?.scrollWidth &&
+				el?.clientWidth < el?.scrollWidth
+			) {
+				setDynamicBreakPoint( el?.clientWidth + SIDEBAR_WIDTH + 10 );
+			}
+
+			setIsLowerThanBreakpoint( window.innerWidth < dynamicBreakPoint );
 		};
 		window.addEventListener( 'resize', handleResize );
 		return () => {
 			window.removeEventListener( 'resize', handleResize );
 		};
-	}, [] );
+	}, [ dynamicBreakPoint, isLowerThanBreakpoint ] );
 
 	useEffect( () => {
 		if ( isLowerThanBreakpoint ) {

5ca75619c1bcf666877fe4f50817560bfd9af200
Kateryna Kodonenko <kateryna@automattic.com>
2025-04-11 09:31:00 +0200

    Widen the modal

--- src/modules/whats-new/components/whats-new-modal.tsx
+++ src/modules/whats-new/components/whats-new-modal.tsx
@@ -92,7 +92,7 @@ export default function WhatsNewModal( { showModal, onClose }: WhatsNewModalProp
 			onFinish={ onClose }
 			contentLabel={ __( "What's New in Studio" ) }
 			className={ cx(
-				"whats-new-modal !w-[312px] !h-[470px] overflow-hidden [&_button[aria-label='Close']_svg]:fill-white [&_.components-button.is-tertiary]:!outline-1 [&_.components-button.is-tertiary]:!outline-solid [&_.components-button.is-tertiary]:!outline-a8c-blueberry",
+				"whats-new-modal !w-[360px] !h-[470px] overflow-hidden [&_button[aria-label='Close']_svg]:fill-white [&_.components-button.is-tertiary]:!outline-1 [&_.components-button.is-tertiary]:!outline-solid [&_.components-button.is-tertiary]:!outline-a8c-blueberry",
 				'[&_*]:select-none',
 				'focus:outline-none'
 			) }

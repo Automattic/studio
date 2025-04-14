4b76ef4c1830f71ad775ce30c9a248e684e86f11
Kateryna Kodonenko <kateryna@automattic.com>
2025-04-14 21:06:02 +0200

    Fix more tests

--- src/components/tests/add-site.test.tsx
+++ src/components/tests/add-site.test.tsx
@@ -229,18 +229,24 @@ describe( 'AddSite', () => {
 		render( <AddSite /> );
 
 		await user.click( screen.getByRole( 'button', { name: 'Add site' } ) );
-		await user.click( screen.getByDisplayValue( 'My WordPress Website' ) );
-		await user.type( screen.getByDisplayValue( 'My WordPress Website' ), ' mutated' );
+		expect( screen.getByRole( 'dialog' ) ).toBeVisible();
 
-		expect( screen.getByDisplayValue( 'My WordPress Website mutated' ) ).toBeVisible();
+		const siteNameInput = screen.getByDisplayValue( 'My WordPress Website' );
+		await user.click( siteNameInput );
+		await user.type( siteNameInput, ' changed' );
+
+		expect( screen.getByDisplayValue( 'My WordPress Website changed' ) ).toBeVisible();
 		await user.click( screen.getByRole( 'button', { name: 'Advanced settings' } ) );
 		expect(
-			screen.getByDisplayValue( '/default_path/my-wordpress-website-mutated' )
+			screen.getByDisplayValue( '/default_path/my-wordpress-website-changed' )
 		).toBeVisible();
 
-		await userEvent.keyboard( '{Escape}' );
+		await user.keyboard( '{Escape}' );
+		await waitFor( () => {
+			expect( screen.queryByRole( 'dialog' ) ).not.toBeInTheDocument();
+		} );
 		await user.click( screen.getByRole( 'button', { name: 'Add site' } ) );
-
+		expect( screen.getByRole( 'dialog' ) ).toBeVisible();
 		expect( screen.getByDisplayValue( 'My WordPress Website' ) ).toBeVisible();
 		await user.click( screen.getByRole( 'button', { name: 'Advanced settings' } ) );
 		expect( screen.getByDisplayValue( '/default_path/my-wordpress-website' ) ).toBeVisible();

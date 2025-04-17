a8098d74d21e0fbf61b9b28b3bcbf665fb69de5c
Gergely Csécsey <gcsecsey@gmail.com>
2025-04-17 14:08:45 +0100

    Remove components index file

--- src/modules/user-settings/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as UserSettings } from './user-settings';
-export { AccountTab } from './account-tab';
-export { NonAuthenticatedAccountTab } from './non-authenticated-account-tab';
-export { PreferencesTab } from './preferences-tab';
-export { UsageTab } from './usage-tab';
-export { SnapshotInfo } from './snapshot-info';
-export { PromptInfo } from './prompt-info';
--- src/modules/user-settings/index.ts
+++ src/modules/user-settings/index.ts
@@ -1,1 +1,1 @@
-export * from './components';
+export { default as UserSettings } from './components/user-settings';

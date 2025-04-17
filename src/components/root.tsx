5e45c01d139b0f9aae641d7e205ff907058b8a2e
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-04-17 10:00:13 +0200

    Clean up

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -14,7 +14,6 @@ import { PromptUsageProvider } from 'src/hooks/use-prompt-usage';
 import { SiteDetailsProvider } from 'src/hooks/use-site-details';
 import { SnapshotProvider } from 'src/hooks/use-snapshots';
 import { ThemeDetailsProvider } from 'src/hooks/use-theme-details';
-import { DemoSiteUpdateProvider } from 'src/hooks/use-update-demo-site';
 import { store } from 'src/stores';
 
 const Root = () => {
@@ -27,23 +26,21 @@ const Root = () => {
 						<SnapshotProvider>
 							<SiteDetailsProvider>
 								<FeatureFlagsProvider>
-									<DemoSiteUpdateProvider>
-										<ThemeDetailsProvider>
-											<InstalledAppsProvider>
-												<OnboardingProvider>
-													<PromptUsageProvider>
-														<ImportExportProvider>
-															<ContentTabsProvider>
-																<SyncSitesProvider>
-																	<App />
-																</SyncSitesProvider>
-															</ContentTabsProvider>
-														</ImportExportProvider>
-													</PromptUsageProvider>
-												</OnboardingProvider>
-											</InstalledAppsProvider>
-										</ThemeDetailsProvider>
-									</DemoSiteUpdateProvider>
+									<ThemeDetailsProvider>
+										<InstalledAppsProvider>
+											<OnboardingProvider>
+												<PromptUsageProvider>
+													<ImportExportProvider>
+														<ContentTabsProvider>
+															<SyncSitesProvider>
+																<App />
+															</SyncSitesProvider>
+														</ContentTabsProvider>
+													</ImportExportProvider>
+												</PromptUsageProvider>
+											</OnboardingProvider>
+										</InstalledAppsProvider>
+									</ThemeDetailsProvider>
 								</FeatureFlagsProvider>
 							</SiteDetailsProvider>
 						</SnapshotProvider>

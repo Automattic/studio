c554a7dac118ad3dda573600d36139172400ce11
bcotrim <bernardo.cotrim@a8c.com>
2025-04-17 13:41:29 +0100

    remove unnecessary constant from common

--- common/constants.ts
+++ common/constants.ts
@@ -2,7 +2,3 @@ export const DEMO_SITE_SIZE_LIMIT_GB = 2;
 export const DEMO_SITE_SIZE_LIMIT_BYTES = DEMO_SITE_SIZE_LIMIT_GB * 1024 * 1024 * 1024; // 2GB
 export const HOUR_MS = 1000 * 60 * 60;
 export const DAY_MS = HOUR_MS * 24;
-// AI Assistant constants
-// IMPORTANT: When updating this value, we need to update the string located in `AIClearHistoryReminder` component.
-// Reference: https://github.com/Automattic/studio/blob/3dd5c58cdb7998e458d191e508e8e859177225a9/src/components/ai-clear-history-reminder.tsx#L78
-export const CLEAR_HISTORY_REMINDER_TIME = 2 * HOUR_MS; // In milliseconds

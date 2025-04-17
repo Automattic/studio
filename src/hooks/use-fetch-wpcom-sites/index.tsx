6171561e71514b98d77399bd6ba6dc6a3cb57f63
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-04-17 15:25:00 +0200

    Fix site plan validation to handle Pressable sites separately

--- src/hooks/use-fetch-wpcom-sites/index.tsx
+++ src/hooks/use-fetch-wpcom-sites/index.tsx
@@ -61,11 +61,7 @@ function isAtomicSite( site: SitesEndpointSite ): boolean {
 }
 
 function hasSupportedPlan( site: SitesEndpointSite ): boolean {
-	return (
-		( isPressableSite( site ) ||
-			site.plan?.features.active.includes( STUDIO_SYNC_FEATURE_NAME ) ) ??
-		false
-	);
+	return site.plan?.features.active.includes( STUDIO_SYNC_FEATURE_NAME ) ?? false;
 }
 
 function isJetpackSite( site: SitesEndpointSite ): boolean {
@@ -91,7 +87,7 @@ function getSyncSupport( site: SitesEndpointSite, connectedSiteIds: number[] ): 
 	if ( isJetpackSite( site ) ) {
 		return 'jetpack-site';
 	}
-	if ( ! hasSupportedPlan( site ) ) {
+	if ( ! hasSupportedPlan( site ) && ! isPressableSite( site ) ) {
 		return 'unsupported';
 	}
 	if ( needsTransfer( site ) ) {

20536c23d6451503cc2a042e3f0c18286cd040fa
Rahul Gavande <rahul.gavande@automattic.com>
2025-04-23 18:59:57 +0530

    Handle additional editors in overview tab

--- src/lib/editor.ts
+++ src/lib/editor.ts
@@ -8,6 +8,11 @@ export type SupportedEditor =
 	| 'sublime'
 	| 'atom';
 
+export type SupportedEditorConfig = {
+	label: string;
+	url: ( path: string ) => string;
+};
+
 export const supportedEditorNames: Record< SupportedEditor, string > = {
 	vscode: 'Visual Studio Code',
 	phpstorm: 'PhpStorm',
@@ -17,4 +22,27 @@ export const supportedEditorNames: Record< SupportedEditor, string > = {
 	webstorm: 'WebStorm',
 	sublime: 'Sublime',
 	atom: 'Atom',
+};
+
+export const supportedEditorConfig: Record< string, SupportedEditorConfig > = {
+	vscode: {
+		label: 'VS Code',
+		url: ( path: string ) => `vscode://file/${ path }?windowId=_blank`,
+	},
+	phpstorm: {
+		label: 'PhpStorm',
+		url: ( path: string ) => `phpstorm://open?file=${ path }`,
+	},
+	webstorm: {
+		label: 'WebStorm',
+		url: ( path: string ) => `webstorm://open?file=${ path }`,
+	},
+	windsurf: {
+		label: 'WindSurf',
+		url: ( path: string ) => `windsurf://file/${ path }?windowId=_blank`,
+	},
+	cursor: {
+		label: 'Cursor',
+		url: ( path: string ) => `cursor://file/${ path }?windowId=_blank`,
+	},
 };

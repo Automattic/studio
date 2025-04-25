b4a4f74cfb3bce5884a8cb46315f187699ea9580
Gergely Csécsey <gcsecsey@gmail.com>
2025-04-25 15:14:22 +0100

    Set default terminal name conditionally

--- src/modules/user-settings/lib/terminal.ts
+++ src/modules/user-settings/lib/terminal.ts
@@ -1,9 +1,10 @@
 import { __ } from '@wordpress/i18n';
+import { isWindows } from 'src/lib/app-globals';
 
 export type SupportedTerminal = 'terminal' | 'iterm' | 'warp' | 'ghostty';
 
 export const supportedTerminalNames: Record< SupportedTerminal, string > = {
-	terminal: process.platform === 'win32' ? __( 'Command Prompt' ) : __( 'Terminal' ),
+	terminal: isWindows() ? __( 'Command Prompt' ) : __( 'Terminal' ),
 	// translators: "iTerm" is the brand name for a terminal app and does not need to be translated
 	iterm: __( 'iTerm' ),
 	// translators: "Warp" is the brand name for a terminal app and does not need to be translated

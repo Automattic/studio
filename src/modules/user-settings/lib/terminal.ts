90a035297bb3e3d66605b4171847bf55e55edfcb
Rahul Gavande <rahul.gavande@automattic.com>
2025-04-27 17:36:45 +0530

    Fix undefined var issue while running node and renderer

--- src/modules/user-settings/lib/terminal.ts
+++ src/modules/user-settings/lib/terminal.ts
@@ -2,8 +2,20 @@ import { __ } from '@wordpress/i18n';
 
 export type SupportedTerminal = 'terminal' | 'iterm' | 'warp' | 'ghostty';
 
+const getPlatform = (): NodeJS.Platform | null => {
+	if ( typeof window !== 'undefined' && window?.studio?.platform ) {
+		// Renderer process
+		return window.studio.platform;
+	} else if ( typeof process !== 'undefined' ) {
+		// Main process
+		return process.platform;
+	}
+
+	return null;
+};
+
 export const supportedTerminalNames: Record< SupportedTerminal, string > = {
-	terminal: window?.studio?.platform === 'win32' ? __( 'Command Prompt' ) : __( 'Terminal' ),
+	terminal: getPlatform() === 'win32' ? __( 'Command Prompt' ) : __( 'Terminal' ),
 	// translators: "iTerm" is the brand name for a terminal app and does not need to be translated
 	iterm: __( 'iTerm' ),
 	// translators: "Warp" is the brand name for a terminal app and does not need to be translated

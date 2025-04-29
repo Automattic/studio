f486aead5f7b1882fcdc92c76ef9bebb7426062a
bcotrim <bmmcotrim@gmail.com>
2025-04-29 13:07:55 +0100

    cli prevent update of an expired site (#1290)

--- common/constants.ts
+++ common/constants.ts
@@ -1,5 +1,6 @@
 export const DEMO_SITE_SIZE_LIMIT_GB = 2;
 export const DEMO_SITE_SIZE_LIMIT_BYTES = DEMO_SITE_SIZE_LIMIT_GB * 1024 * 1024 * 1024; // 2GB
+export const DEMO_SITE_EXPIRATION_DAYS = 7;
 export const HOUR_MS = 1000 * 60 * 60;
 export const DAY_MS = HOUR_MS * 24;
 

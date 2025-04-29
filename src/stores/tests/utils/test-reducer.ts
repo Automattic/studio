f057f43dba26a609218a59bc98074be91f6534b2
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-04-29 09:53:11 +0200

    Fix tests

--- src/stores/tests/utils/test-reducer.ts
+++ src/stores/tests/utils/test-reducer.ts
@@ -2,9 +2,12 @@ import { Action } from '@reduxjs/toolkit';
 import { RootState, rootReducer } from 'src/stores';
 
 const initialState = rootReducer( undefined, { type: '@@INIT' } );
+let dispatchedActions: Action[] = [];
 
 // This reducer adds an action to reset the state to its initial value. This is useful for testing.
 export function testReducer( state: RootState | undefined, action: Action ) {
+	dispatchedActions.push( action );
+
 	if ( action.type === 'test/resetState' ) {
 		return initialState;
 	}
@@ -17,3 +20,11 @@ export const testActions = {
 		return { type: 'test/resetState' };
 	},
 };
+
+export function getDispatchedActions() {
+	return dispatchedActions;
+}
+
+export function resetDispatchedActions() {
+	dispatchedActions = [];
+}

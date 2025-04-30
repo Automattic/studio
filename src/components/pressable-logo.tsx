3bf3e466c32e6061fa75c74f6e944b7819c80a14
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-04-30 13:06:31 +0200

    Fix Pressable logo component

--- src/components/pressable-logo.tsx
+++ src/components/pressable-logo.tsx
@@ -7,9 +7,9 @@ export function PressableLogo( {
 } ) {
 	return (
 		<svg
-			width="16"
-			height="16"
-			viewBox={ `0 0 ${ size } ${ size }` }
+			width={ `${ size }` }
+			height={ `${ size }` }
+			viewBox="0 0 16 16"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>

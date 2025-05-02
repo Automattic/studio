f7ebc74554cbbf8d4b5f42c6f13794da2a904cd0
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-05-02 13:16:49 +0200

    CLI initial release

--- src/modules/cli/lib/is-cli-feature-enabled.ts
+++
@@ -1,7 +0,0 @@
-import { app } from 'electron';
-import { prerelease } from 'semver';
-
-// The Studio CLI is available in development, and in prerelease builds (dev and beta builds).
-export function isCLIFeatureEnabled() {
-	return process.env.NODE_ENV === 'development' || prerelease( app.getVersion() );
-}

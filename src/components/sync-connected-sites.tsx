effc1256cde01b69e9443096120a20df959b2ffc
Antonio Sejas <antonio@sejas.es>
2025-05-02 17:14:44 +0100

    Restore the space between the logo title and the list of sites

--- src/components/sync-connected-sites.tsx
+++ src/components/sync-connected-sites.tsx
@@ -379,7 +379,7 @@ const SyncConnectedSiteSection = ( {
 
 	return (
 		<div key={ section.id } className="flex flex-col gap-2 mb-6">
-			<div className="flex items-center gap-2 border-b border-a8c-gray-0 px-8">
+			<div className="flex items-center gap-2 border-b border-a8c-gray-0 px-8 pb-2.5">
 				{ logo }
 				<div className={ cx( 'a8c-label-semibold', hasConnectionErrors && 'error-message' ) }>
 					{ section.name }

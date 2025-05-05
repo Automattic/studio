c0850e2477f3d47e3fccf483457c5ed87b5117e0
Rahul Gavande <rahul.gavande@automattic.com>
2025-05-05 17:12:51 +0530

    Remove useRef

--- src/hooks/use-certificate-trust.ts
+++ src/hooks/use-certificate-trust.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useWindowListener } from 'src/hooks/use-window-listener';
 import { getIpcApi } from 'src/lib/get-ipc-api';
 
@@ -7,7 +7,6 @@ import { getIpcApi } from 'src/lib/get-ipc-api';
  * @returns A boolean indicating if the certificate is trusted
  */
 export function useCertificateTrust(): boolean {
-	const isMounted = useRef( true );
 	const [ isTrusted, setIsTrusted ] = useState< boolean >( false );
 
 	const checkCertificateTrust = useCallback( () => {
@@ -19,23 +18,17 @@ export function useCertificateTrust(): boolean {
 		getIpcApi()
 			.isCATrusted()
 			.then( ( trusted ) => {
-				if ( isMounted.current ) {
-					setIsTrusted( trusted );
-				}
+				setIsTrusted( trusted );
 			} )
 			.catch( ( error ) => {
 				console.error( 'Failed to check certificate trust:', error );
 			} );
-	}, [ setIsTrusted, isTrusted ] );
+	}, [ isTrusted ] );
 
 	useWindowListener( 'focus', checkCertificateTrust );
 
 	useEffect( () => {
-		isMounted.current = true;
 		checkCertificateTrust();
-		return () => {
-			isMounted.current = false;
-		};
 	}, [ checkCertificateTrust ] );
 
 	return isTrusted;

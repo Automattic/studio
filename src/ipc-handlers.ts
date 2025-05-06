6f2a51fc92f67d0c8d666d61d262075b4cccd666
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-05-06 13:50:26 +0200

    One more tweak

--- src/ipc-handlers.ts
+++ src/ipc-handlers.ts
@@ -804,7 +804,7 @@ export async function openSiteURL(
 	if ( ! site?.server?.url ) {
 		await showMessageBox( event, {
 			type: 'error',
-			message: __( 'Could not open link' ),
+			message: __( 'Failed to open link' ),
 			detail: __( 'Please ensure your site files have not been moved or deleted.' ),
 		} );
 		return;

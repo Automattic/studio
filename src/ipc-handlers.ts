ed0b9c059b9fc70072a6b6faa53634bd44b9b310
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-05-07 16:24:21 +0200

    Small type fixes and cleanup

--- src/ipc-handlers.ts
+++ src/ipc-handlers.ts
@@ -1027,7 +1027,7 @@ export async function openTerminalAtPath(
 		initScriptSteps.push( `cd \\"${ escapedPath }\\"`, 'clear' );
 
 		const userData = await loadUserData();
-		const preferredTerminal = ( userData.preferredTerminal || 'terminal' ) as SupportedTerminal;
+		const preferredTerminal = userData.preferredTerminal || DEFAULT_TERMINAL;
 
 		if ( preferredTerminal === 'warp' ) {
 			return promiseExec( `open -a Warp "${ targetPath }"` );
@@ -1324,7 +1324,7 @@ export async function saveUserTerminal(
 
 export async function getUserTerminal( _event: IpcMainInvokeEvent ): Promise< SupportedTerminal > {
 	const userData = await loadUserData();
-	return ( userData.preferredTerminal || DEFAULT_TERMINAL ) as SupportedTerminal;
+	return userData.preferredTerminal || DEFAULT_TERMINAL;
 }
 
 export async function isFullscreen( _event: IpcMainInvokeEvent ): Promise< boolean > {
@@ -1338,10 +1338,6 @@ export async function getAllCustomDomains(): Promise< string[] > {
 	return userData.sites
 		.map( ( site ) => site.customDomain )
 		.filter( ( domain ): domain is string => domain !== undefined );
-}
-
-export function getRandomUUID(): crypto.UUID {
-	return crypto.randomUUID();
 }
 
 export async function createSnapshot(

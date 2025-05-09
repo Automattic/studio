e0213af2408a671b56b6f91b122f5c021858e81d
Kateryna Kodonenko <kateryna@automattic.com>
2025-05-09 11:27:23 +0200

    Solve warnings for content-tab-settings.test

--- src/components/tests/content-tab-settings.test.tsx
+++ src/components/tests/content-tab-settings.test.tsx
@@ -1,6 +1,6 @@
 // To run tests, execute `npm run test -- src/components/tests/content-tab-settings.test.tsx` from the root directory
 import { UnknownAction } from '@reduxjs/toolkit';
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { act, fireEvent, render, screen, waitFor, within } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { produce } from 'immer';
 import { Provider } from 'react-redux';
@@ -244,15 +244,22 @@ describe( 'ContentTabSettings', () => {
 					name: 'Save',
 				} )
 			);
-			expect( updateSite ).toHaveBeenCalledWith( expect.objectContaining( { phpVersion: '8.2' } ) );
-			expect( stopServer ).not.toHaveBeenCalled();
-			expect( startServer ).not.toHaveBeenCalled();
+
+			await waitFor( () => {
+				expect( updateSite ).toHaveBeenCalledWith(
+					expect.objectContaining( { phpVersion: '8.2' } )
+				);
+				expect( stopServer ).not.toHaveBeenCalled();
+				expect( startServer ).not.toHaveBeenCalled();
+			} );
 
 			rerenderWithProvider(
 				rerender,
 				<ContentTabSettings selectedSite={ { ...selectedSite, phpVersion: '8.2' } } />
 			);
-			expect( screen.getByText( '8.2' ) ).toBeVisible();
+			await waitFor( () => {
+				expect( screen.getByText( '8.2' ) ).toBeVisible();
+			} );
 		} );
 
 		it( 'changes PHP version and restarts site when site is running', async () => {
@@ -288,15 +295,22 @@ describe( 'ContentTabSettings', () => {
 					name: 'Save',
 				} )
 			);
-			expect( updateSite ).toHaveBeenCalledWith( expect.objectContaining( { phpVersion: '8.2' } ) );
-			expect( stopServer ).toHaveBeenCalled();
-			expect( startServer ).toHaveBeenCalled();
+
+			await waitFor( () => {
+				expect( updateSite ).toHaveBeenCalledWith(
+					expect.objectContaining( { phpVersion: '8.2' } )
+				);
+				expect( stopServer ).toHaveBeenCalled();
+				expect( startServer ).toHaveBeenCalled();
+			} );
 
 			rerenderWithProvider(
 				rerender,
 				<ContentTabSettings selectedSite={ { ...selectedSite, phpVersion: '8.2' } } />
 			);
-			expect( screen.getByText( '8.2' ) ).toBeVisible();
+			await waitFor( () => {
+				expect( screen.getByText( '8.2' ) ).toBeVisible();
+			} );
 		} );
 	} );
 } );

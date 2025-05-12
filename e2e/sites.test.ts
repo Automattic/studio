6d29edd5ed64ea799d4b80b670972055a8c5fe3e
bcotrim <bernardo.cotrim@a8c.com>
2025-05-12 19:45:22 +0100

    revert timeout increase for whats new modal

--- e2e/sites.test.ts
+++ e2e/sites.test.ts
@@ -25,7 +25,7 @@ test.describe( 'Servers', () => {
 		await onboarding.continueButton.click();
 
 		const whatsNewModal = new WhatsNewModal( session.mainWindow );
-		if ( await whatsNewModal.locator.isVisible( { timeout: 20000 } ) ) {
+		if ( await whatsNewModal.locator.isVisible( { timeout: 5000 } ) ) {
 			await whatsNewModal.closeButton.click();
 		}
 

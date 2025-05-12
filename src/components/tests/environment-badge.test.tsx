c79adf2235a1d273b707bcdb0572b780cb4d1553
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-05-12 14:59:38 +0200

    Add tests for `EnvironmentBadge` component

---
+++ src/components/tests/environment-badge.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { EnvironmentBadge } from 'src/components/environment-badge';
+
+describe( 'EnvironmentBadge', () => {
+	it( 'renders the Production badge with correct label', () => {
+		render( <EnvironmentBadge type="production" /> );
+		const badge = screen.getByText( 'Production' );
+		expect( badge ).toBeInTheDocument();
+	} );
+
+	it( 'renders the Staging badge with correct label', () => {
+		render( <EnvironmentBadge type="staging" /> );
+		const badge = screen.getByText( 'Staging' );
+		expect( badge ).toBeInTheDocument();
+	} );
+
+	it( 'applies specific classes for the Production badge', () => {
+		const { container } = render( <EnvironmentBadge type="production" /> );
+
+		const badgeElement = container.firstChild as HTMLElement;
+		expect( badgeElement ).not.toBeNull();
+
+		expect( badgeElement.className ).toContain( 'bg-a8c-green-5' );
+		expect( badgeElement.className ).toContain( 'text-a8c-green-80' );
+	} );
+
+	it( 'applies specific classes for the Staging badge', () => {
+		const { container } = render( <EnvironmentBadge type="staging" /> );
+
+		const badgeElement = container.firstChild as HTMLElement;
+		expect( badgeElement ).not.toBeNull();
+
+		expect( badgeElement.className ).toContain( 'text-a8c-yellow-80' );
+		expect( badgeElement.className ).toContain( 'bg-a8c-yellow-10' );
+	} );
+
+	it( 'applies "selected" styling when selected prop is true', () => {
+		const { container } = render( <EnvironmentBadge type="production" selected={ true } /> );
+
+		const badgeElement = container.firstChild as HTMLElement;
+		expect( badgeElement ).not.toBeNull();
+
+		expect( badgeElement.className ).toContain( 'bg-white' );
+		expect( badgeElement.className ).toContain( 'text-a8c-blueberry' );
+	} );
+} );

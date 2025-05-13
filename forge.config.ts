1819d79be7aabd211283e6e5457bf42101d53944
bcotrim <bernardo.cotrim@a8c.com>
2025-05-13 10:58:32 +0100

    remove existing devtools references

--- forge.config.ts
+++ forge.config.ts
@@ -112,11 +112,8 @@ const config: ForgeConfig = {
 		generateAssets: async () => {
 			console.log( 'Building the HTML entry file ...' );
 
-			const REACT_DEV_TOOLS =
-				process.env.REACT_DEV_TOOLS === 'true' || process.env.REACT_DEV_TOOLS === '1';
-
 			const ejsTemplate = fs.readFileSync( './src/index.ejs', 'utf8' );
-			const data = { REACT_DEV_TOOLS };
+			const data = {};
 			const renderedHtml = ejs.render( ejsTemplate, data );
 			fs.mkdirSync( './dist', { recursive: true } );
 			fs.writeFileSync( './dist/index.html', renderedHtml );

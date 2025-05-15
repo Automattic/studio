c55e39b4322a132fd24def53236341140703bf68
Ivan Ottinger <ivan.ottinger@automattic.com>
2025-05-15 16:39:07 +0200

    Remove `export` from the `EnvironmentType`
    Co-authored-by: Antonio Sejas <antonio.sejas@automattic.com>

--- src/components/environment-badge.tsx
+++ src/components/environment-badge.tsx
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 import { Badge } from 'src/components/badge';
 import { cx } from 'src/lib/cx';
 
-export type EnvironmentType = 'production' | 'staging' | 'sandbox';
+type EnvironmentType = 'production' | 'staging' | 'sandbox';
 
 interface EnvironmentBadgeProps {
 	type: EnvironmentType;

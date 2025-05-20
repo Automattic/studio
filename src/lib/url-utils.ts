69d158ba886ce13fe41d3c4b770adc93b1a91c9f
Roberto Aranda <roberto.aranda@automattic.com>
2025-05-20 16:21:16 +0200

    Return original URL param when it cannot be parsed

--- src/lib/url-utils.ts
+++ src/lib/url-utils.ts
@@ -1,13 +1,13 @@
 /**
  * Extracts the hostname from a URL string.
  * @param url The URL string to extract the hostname from
- * @returns The hostname (domain and port if specified), or an empty string if the URL is invalid
+ * @returns The hostname (domain and port if specified), or the passed url param if invalid
  */
 export function getHostnameFromUrl( url: string ): string {
 	try {
 		const parsedUrl = new URL( url );
 		return parsedUrl.hostname;
 	} catch ( error ) {
-		return '';
+		return url;
 	}
 }

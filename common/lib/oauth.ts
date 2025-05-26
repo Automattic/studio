1d305d6c06edd2f25000fa0c66cae6af0cd6d90f
Antonio Sejas <antonio@sejas.es>
2025-05-26 12:31:00 +0100

    Add locale param on getAuthenticationUrl

--- common/lib/oauth.ts
+++ common/lib/oauth.ts
@@ -1,13 +1,18 @@
 import { PROTOCOL_PREFIX, CLIENT_ID } from 'common/constants';
+import { getUserLocaleWithFallback } from 'src/lib/locale-node';
 
 const SCOPES = 'global';
 const REDIRECT_URI = `${ PROTOCOL_PREFIX }://auth`;
 
-export function getAuthenticationUrl(): string {
+export async function getAuthenticationUrl(): Promise< string > {
 	const url = new URL( 'https://public-api.wordpress.com/oauth2/authorize' );
 	url.searchParams.set( 'response_type', 'token' );
 	url.searchParams.set( 'client_id', CLIENT_ID );
 	url.searchParams.set( 'redirect_uri', REDIRECT_URI );
 	url.searchParams.set( 'scope', SCOPES );
+
+	const locale = await getUserLocaleWithFallback();
+	url.searchParams.set( 'locale', locale );
+
 	return url.toString();
 }

5290c74d16a32bc89f6996acb81dc5ff0aea99fc
Antonio Sejas <antonio@sejas.es>
2025-05-26 12:35:31 +0100

    Receive locale from function params

--- common/lib/oauth.ts
+++ common/lib/oauth.ts
@@ -1,17 +1,15 @@
 import { PROTOCOL_PREFIX, CLIENT_ID } from 'common/constants';
-import { getUserLocaleWithFallback } from 'src/lib/locale-node';
+import { SupportedLocale } from 'common/lib/locale';
 
 const SCOPES = 'global';
 const REDIRECT_URI = `${ PROTOCOL_PREFIX }://auth`;
 
-export async function getAuthenticationUrl(): Promise< string > {
+export function getAuthenticationUrl( locale: SupportedLocale ): string {
 	const url = new URL( 'https://public-api.wordpress.com/oauth2/authorize' );
 	url.searchParams.set( 'response_type', 'token' );
 	url.searchParams.set( 'client_id', CLIENT_ID );
 	url.searchParams.set( 'redirect_uri', REDIRECT_URI );
 	url.searchParams.set( 'scope', SCOPES );
-
-	const locale = await getUserLocaleWithFallback();
 	url.searchParams.set( 'locale', locale );
 
 	return url.toString();

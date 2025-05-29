dcc71fefb041bd163c0a0525182a57721aba5117
Gergely Csécsey <gergely.csecsey@automattic.com>
2025-05-29 13:00:32 +0200

    Remove `downloadWordPress` call in `startWPNow` (#1417)
    * Remove `downloadWordPress`call
    
    * Remove unused import

--- vendor/wp-now/src/wp-now.ts
+++ vendor/wp-now/src/wp-now.ts
@@ -32,7 +32,7 @@ import {
 	SQLITE_FILENAME,
 	SQLITE_PLUGIN_FOLDER,
 } from './constants';
-import { downloadWordPress, removeDownloadedMuPlugins } from './download';
+import { removeDownloadedMuPlugins } from './download';
 import getSqlitePath from './get-sqlite-path';
 import getWordpressVersionsPath from './get-wordpress-versions-path';
 import { output } from './output';
@@ -89,7 +89,6 @@ export default async function startWPNow(
 		return { php, options };
 	}
 	output?.log( `wp: ${ options.wordPressVersion }` );
-	await downloadWordPress( options.wordPressVersion );
 
 	if ( options.reset ) {
 		fs.removeSync( options.wpContentPath );

3d8e067df087dfc77133c24666baa0e3eda865cb
Rahul Gavande <rahul.gavande@automattic.com>
2025-06-13 18:25:17 +0530

    Fix linting errors

--- src/hooks/use-network-connection.ts
+++ src/hooks/use-network-connection.ts
@@ -3,21 +3,21 @@ import { useSiteDetails } from './use-site-details';
 import { useOffline } from './use-offline';
 
 export function useNetworkConnection() {
-    const { data: sites, stopServer, startServer } = useSiteDetails();
-    const isOffline = useOffline();
+	const { data: sites, stopServer, startServer } = useSiteDetails();
+	const isOffline = useOffline();
 
-    const restartRunningSites = async () => {
-        const runningSites = sites.filter(site => site.running);
-        await Promise.all(runningSites.map(site => stopServer(site.id)));
-        await Promise.all(runningSites.map(site => startServer(site.id)));
-    };
+	const restartRunningSites = async () => {
+		const runningSites = sites.filter( ( site ) => site.running );
+		await Promise.all( runningSites.map( ( site ) => stopServer( site.id ) ) );
+		await Promise.all( runningSites.map( ( site ) => startServer( site.id ) ) );
+	};
 
-    useEffect(() => {
-        window.addEventListener('online', restartRunningSites);
-        window.addEventListener('offline', restartRunningSites);
-        return () => {
-            window.removeEventListener('online', restartRunningSites);
-            window.removeEventListener('offline', restartRunningSites);
-        };
-    }, [sites, stopServer, startServer, isOffline]);
-} 
+	useEffect( () => {
+		window.addEventListener( 'online', restartRunningSites );
+		window.addEventListener( 'offline', restartRunningSites );
+		return () => {
+			window.removeEventListener( 'online', restartRunningSites );
+			window.removeEventListener( 'offline', restartRunningSites );
+		};
+	}, [ sites, stopServer, startServer, isOffline ] );
+}

a90a586a5a92ff44a505faddef0d87dc730f8fa0
Volodymyr Makukha <nei.css@gmail.com>
2025-07-03 15:53:27 +0200

    Fix extended sync tree alignment and copy (#1531)

--- src/modules/sync/hooks/use-sync-dialog-texts.tsx
+++ src/modules/sync/hooks/use-sync-dialog-texts.tsx
@@ -36,19 +36,19 @@ export const useSyncDialogTexts = ( type: 'pull' | 'push' ) => {
 				staging: {
 					title: __( 'Push to Staging' ),
 					description: __(
-						'Pushing will replace the existing files and database with a copy from your local site.\n\n The staging site will be backed-up before any changes are applied.'
+						'Pushing will replace the existing files and database with a copy from your local site.\n\n The staging site will be backed up before any changes are applied.'
 					),
 				},
 				sandbox: {
 					title: __( 'Push to Sandbox' ),
 					description: __(
-						'Pushing will replace the existing files and database with a copy from your local site.\n\n The sandbox site will be backed-up before any changes are applied.'
+						'Pushing will replace the existing files and database with a copy from your local site.\n\n The sandbox site will be backed up before any changes are applied.'
 					),
 				},
 				production: {
 					title: __( 'Push to Production' ),
 					description: __(
-						'Pushing will replace the existing files and database with a copy from your local site.\n\n The production site will be backed-up before any changes are applied.'
+						'Pushing will replace the existing files and database with a copy from your local site.\n\n The production site will be backed up before any changes are applied.'
 					),
 				},
 				fromLabel: __( 'Push' ),

7c405257602dc0007611d881c0fccef4689524ad
Antonio Sejas <antonio@sejas.es>
2025-07-08 14:57:00 +0100

    update copy

--- src/modules/whats-new/components/whats-new-modal.tsx
+++ src/modules/whats-new/components/whats-new-modal.tsx
@@ -61,7 +61,7 @@ export default function WhatsNewModal( { showModal, onClose }: WhatsNewModalProp
 			image: selectiveSyncIllustration,
 			title: __( 'Synchronize with precision' ),
 			description: __(
-				'Choose the parts of your site to synchronize, speeding up the process and reducing unnecessary changes.'
+				"Gain more control and speed up updates by selecting the plugins, themes, and database you want to synchronize to WordPress.com or Pressable."
 			),
 			learnMoreUrl: `${ getLocalizedLink( locale, 'docsSync' ) }#pull`,
 		},

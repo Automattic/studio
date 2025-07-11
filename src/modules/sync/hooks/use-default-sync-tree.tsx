a3f7924d64c8bfd95b22d05343f36f59caa31e91
Antonio Sejas <antonio@sejas.es>
2025-07-11 10:57:25 +0100

    Undo push contents option

--- src/modules/sync/hooks/use-default-sync-tree.tsx
+++ src/modules/sync/hooks/use-default-sync-tree.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { TreeNode } from 'src/components/tree-view';
 import { SYNC_OPTIONS } from 'src/constants';
 
-export const useDefaultSyncTree = ( type: 'push' | 'pull' ): TreeNode[] => {
+export const useDefaultSyncTree = (): TreeNode[] => {
 	const { __ } = useI18n();
 
 	return useMemo( () => {
@@ -49,17 +49,13 @@ export const useDefaultSyncTree = ( type: 'push' | 'pull' ): TreeNode[] => {
 								type: 'folder',
 								expanded: false,
 							},
-							...( type === 'pull'
-								? [
-										{
-											id: SYNC_OPTIONS.contents,
-											name: SYNC_OPTIONS.contents,
-											label: __( 'Other files and directories' ),
-											checked: true,
-											type: 'more' as const,
-										},
-								  ]
-								: [] ),
+							{
+								id: SYNC_OPTIONS.contents,
+								name: SYNC_OPTIONS.contents,
+								label: __( 'Other files and directories' ),
+								checked: true,
+								type: 'more',
+							},
 						],
 					},
 				],
@@ -71,5 +67,5 @@ export const useDefaultSyncTree = ( type: 'push' | 'pull' ): TreeNode[] => {
 				checked: true,
 			},
 		];
-	}, [ __, type ] );
+	}, [ __ ] );
 };

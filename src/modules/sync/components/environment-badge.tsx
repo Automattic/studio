79bfdc54dd977a97f555125d12bdea0521c7eed9
Wojtek Naruniec <wojtek@naruniec.me>
2025-07-29 12:18:19 +0200

    Remove unused SyncSite import

--- src/modules/sync/components/environment-badge.tsx
+++ src/modules/sync/components/environment-badge.tsx
@@ -2,7 +2,6 @@ import { __ } from '@wordpress/i18n';
 import { Badge } from 'src/components/badge';
 import { cx } from 'src/lib/cx';
 import { getEnvironmentLabel } from 'src/modules/sync/lib/environment-utils';
-import type { SyncSite } from 'src/hooks/use-fetch-wpcom-sites/types';
 
 export type EnvironmentType = string;
 

42b91c0dabe215f02f708c54ae2d0081e177218d
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-08-08 13:49:12 +0200

    Remove local environment type and related badge and copy

--- src/modules/sync/lib/environment-utils.ts
+++ src/modules/sync/lib/environment-utils.ts
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 import { z } from 'zod';
 import { SyncSite } from 'src/hooks/use-fetch-wpcom-sites/types';
 
-const EnvironmentSchema = z.enum( [ 'production', 'staging', 'development', 'local' ] );
+const EnvironmentSchema = z.enum( [ 'production', 'staging', 'development' ] );
 export type EnvironmentType = z.infer< typeof EnvironmentSchema >;
 
 export const getSiteEnvironment = ( site: SyncSite ): EnvironmentType => {
@@ -18,7 +18,6 @@ export const getEnvironmentLabel = ( type: EnvironmentType ): string => {
 		production: __( 'Production' ),
 		staging: __( 'Staging' ),
 		development: __( 'Development' ),
-		local: __( 'Local' ),
 	};
 	return labels[ type ] || type.charAt( 0 ).toUpperCase() + type.slice( 1 );
 };

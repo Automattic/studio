fa01a875a7ff46ecd9b6ec3ce75f2223aad92a59
Antonio Sejas <antonio@sejas.es>
2025-08-13 14:22:50 +0200

    Unify exports on sync index

--- src/stores/sync/index.ts
+++ src/stores/sync/index.ts
@@ -1,5 +1,4 @@
 export { syncReducer, syncActions, syncSelectors } from './sync-slice';
 export { useLatestRewindId, useRemoteFileTree } from './sync-slice';
-export { useGetLatestRewindIdQuery } from './sync-api';
-export { fetchRemoteFileTree } from './sync-api';
+export { useGetLatestRewindIdQuery, fetchRemoteFileTree } from './sync-api';
 export * from './sync-types';

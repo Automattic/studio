7defcb389b69a20fb096ed16f1f984de88031fec
Antonio Sejas <antonio@sejas.es>
2025-08-13 11:27:31 +0200

    Separate options based on PULL and PUSH

--- src/constants.ts
+++ src/constants.ts
@@ -47,13 +47,16 @@ export const ARCHIVER_OPTIONS = {
 };
 
 export const SYNC_OPTIONS = {
+	// Options sent for pull and push
 	all: 'all',
+	sqls: 'sqls',
+	// Option sent for pull
+	paths: 'paths',
+	// Options sent for push
 	themes: 'themes',
 	plugins: 'plugins',
 	uploads: 'uploads',
-	sqls: 'sqls',
 	contents: 'contents',
-	paths: 'paths',
 } as const;
 
 // AI Assistant constants

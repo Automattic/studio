b52b963a920cd288f5010bdb058a657323b7ef72
Ian Maia <ian.maia@automattic.com>
2025-08-18 14:22:28 +0200

    Revert "Add debug console log before tests' GC"
    This reverts commit af1fc32eb14b6abb790cadd69b9fe75e4a95cc31.

--- jest-setup.ts
+++ jest-setup.ts
@@ -42,16 +42,12 @@ if (typeof window !== 'undefined') {
 
 // Silence console.log for all tests
 beforeEach( () => {
-	// console.log = jest.fn();
+	console.log = jest.fn();
 } );
 
 // Force garbage collection after each test
 afterAll(() => {
-	console.log('afterAll')
-	console.log(global.gc)
 	if (global.gc) {
-		console.log('---')
-		console.log('Running GC')
 		global.gc();
 	}
 });

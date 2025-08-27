9e99a2484b09c02b228a400dd6d5c3f6a2872381
Volodymyr Makukha <nei.css@gmail.com>
2025-08-27 12:22:09 +0100

    Fix using custom domains with Playground-cli

--- src/lib/wordpress-provider/playground-cli/playground-server-process-child.ts
+++ src/lib/wordpress-provider/playground-cli/playground-server-process-child.ts
@@ -142,6 +142,7 @@ async function startServer(
 			port: options.port,
 			login: true,
 			'mount-before-install': mounts,
+			'site-url': serverOptions.absoluteUrl,
 		};
 
 		if ( ! options.isSetupMode ) {

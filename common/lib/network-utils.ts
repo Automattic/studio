a06a814822aba9da69391d5d8faff8c4f727ac04
Riad Benguella <benguella@gmail.com>
2025-08-31 01:40:25 +0200

    Add electron agnostic isOnline utility

---
+++ common/lib/network-utils.ts
@@ -0,0 +1,20 @@
+import dns from 'dns/promises';
+
+/**
+ * Check if the system has internet connectivity by testing DNS resolution.
+ *
+ * @returns Promise that resolves to true if online, false if offline
+ */
+export async function isOnline(): Promise< boolean > {
+	try {
+		const timeoutPromise = new Promise( ( _, reject ) =>
+			setTimeout( () => reject( new Error( 'Timeout' ) ), 5000 )
+		);
+
+		await Promise.race( [ dns.resolve( 'google.com' ), timeoutPromise ] );
+
+		return true;
+	} catch {
+		return false;
+	}
+}

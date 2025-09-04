96f26529f027ee9f0e454ad5c94f242a1efae43c
katinthehatsite <katerynakodonenko@gmail.com>
2025-09-04 11:22:38 +0200

    Remove unused links for the what's new modal (#1733)
    * Remove unused links
    
    * Replace links in tests for existing ones
    
    * Fix remaining tests
    
    ---------
    
    Co-authored-by: Kateryna Kodonenko <kateryna@automattic.com>

--- src/lib/get-localized-link.ts
+++ src/lib/get-localized-link.ts
@@ -37,23 +37,12 @@ const BLOG_LINKS = {
 	blogPreferredApps: {
 		en: 'https://wordpress.com/blog/2025/05/12/preferences-studio/',
 	},
-	blogPhpVersions: {
-		en: 'https://wordpress.com/blog/2025/03/17/studio-wordpress-php-versions/',
-		es: 'https://wordpress.com/es/blog/2025/04/02/modifica-las-versiones-de-wordpress-y-php-de-tu-sitio-local-con-studio/',
-		fr: 'https://wordpress.com/fr/blog/2025/03/28/studio-wordpress-php-versions/',
-		'pt-br': 'https://wordpress.com/pt-br/blog/2025/04/02/estudio-wordpress-php-versoes/',
-	},
 	blogPreviewSites: {
 		en: 'https://wordpress.com/blog/2025/02/24/studio-preview-sites/',
 		es: 'https://wordpress.com/es/blog/2025/03/05/colabora-mas-y-mejor-en-studio-con-los-sitios-de-vista-previa/',
 		fr: 'https://wordpress.com/fr/blog/2025/03/04/sites-de-previsualisation-studio/',
 		ja: 'https://wordpress.com/ja/blog/2025/03/28/studio-preview-sites/',
 		'pt-br': 'https://wordpress.com/pt-br/blog/2025/03/06/estudio-visualizar-sites/',
-	},
-	blogCustomDomainsHttps: {
-		en: 'https://wordpress.com/blog/2025/03/31/studio-custom-domains-https/',
-		es: 'https://wordpress.com/es/blog/2025/03/31/studio-custom-domains-https/',
-		'pt-br': 'https://wordpress.com/pt-br/blog/2025/04/03/estudio-dominios-personalizados-https/',
 	},
 } satisfies Record< `blog${ string }`, TranslatedLink >;
 

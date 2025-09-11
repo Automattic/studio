2472ff25092edf9b6faafcaf0588a133ab324ef4
Riad Benguella <benguella@gmail.com>
2025-09-11 17:33:15 +0100

    Lib: Move port-finder to common folder to be used later by both CLI and App

--- src/lib/proxy-server.ts
+++ src/lib/proxy-server.ts
@@ -4,7 +4,7 @@ import { createSecureContext } from 'node:tls';
 import { domainToASCII } from 'node:url';
 import * as Sentry from '@sentry/electron/main';
 import httpProxy from 'http-proxy';
-import { portFinder } from 'src/lib/port-finder';
+import { portFinder } from 'common/lib/port-finder';
 import { SiteServer } from 'src/site-server';
 import { loadUserData } from 'src/storage/user-data';
 

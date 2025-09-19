1adff2546a4db36087d2f765c8d17fab2184efd4
bcotrim <bernardo.cotrim@a8c.com>
2025-09-19 15:25:24 +0100

    fix lint

--- cli/polyfills/browser-globals.js
+++ cli/polyfills/browser-globals.js
@@ -1,22 +1,22 @@
 // Polyfills for browser globals in Node.js environment
-if (typeof window === 'undefined') {
-  global.window = {
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    location: { href: '' },
-    document: {},
-  };
+if ( typeof window === 'undefined' ) {
+	global.window = {
+		addEventListener: () => {},
+		removeEventListener: () => {},
+		location: { href: '' },
+		document: {},
+	};
 }
 
-if (typeof document === 'undefined') {
-  global.document = {
-    createElement: () => ({}),
-    addEventListener: () => {},
-  };
+if ( typeof document === 'undefined' ) {
+	global.document = {
+		createElement: () => ( {} ),
+		addEventListener: () => {},
+	};
 }
 
-if (typeof navigator === 'undefined') {
-  global.navigator = {
-    userAgent: 'Node.js',
-  };
+if ( typeof navigator === 'undefined' ) {
+	global.navigator = {
+		userAgent: 'Node.js',
+	};
 }

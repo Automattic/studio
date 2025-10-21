ef610213a42e4ed6e15da7dafcf3ab290a66c203
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-10-21 10:24:22 +0200

    Remove sync-dialog-wrapper class and its border styling

--- src/modules/sync/components/sync-dialog.tsx
+++ src/modules/sync/components/sync-dialog.tsx
@@ -208,7 +208,7 @@ export function SyncDialog( {
 
 	return (
 		<Modal
-			className="sync-dialog-wrapper w-3/5 min-w-[550px] h-full max-h-[84vh] [&>div]:!p-0"
+			className="w-3/5 min-w-[550px] h-full max-h-[84vh] [&>div]:!p-0"
 			onRequestClose={ onRequestClose }
 			title={ syncTexts.title }
 		>

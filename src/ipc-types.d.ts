1bd589508b61c7b6c64fc1893fbb1c1a2e9c7df2
katinthehatsite <katerynakodonenko@gmail.com>
2025-10-23 13:15:52 +0200

    Studio: Add streamline onboarding feature flag (#1920)
    * Add streamline onboarding feature flag
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Kateryna Kodonenko <kateryna@automattic.com>

--- src/ipc-types.d.ts
+++ src/ipc-types.d.ts
@@ -82,6 +82,7 @@ type IpcApi = {
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface FeatureFlags {
 	enableBlueprints: boolean;
+	streamlineOnboarding: boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type

e542b7022f833d3b8994476ca3ace25077c59f6e
Kateryna Kodonenko <kateryna@automattic.com>
2025-10-28 16:02:17 +0100

    Remove duplication

--- src/modules/sync/lib/tree-utils.ts
+++ src/modules/sync/lib/tree-utils.ts
@@ -1,15 +1,11 @@
 import { SYNC_EXCLUSIONS } from '../constants';
 
-export const shouldExcludeFromSync = ( itemName: string, relativePath: string ): boolean => {
+export const shouldExcludeFromSync = ( itemName: string ): boolean => {
 	if ( itemName.startsWith( '.' ) ) {
 		return true;
 	}
 
 	if ( SYNC_EXCLUSIONS.includes( itemName ) ) {
-		return true;
-	}
-
-	if ( relativePath.includes( 'mu-plugins' ) && itemName === 'sqlite-database-integration' ) {
 		return true;
 	}
 

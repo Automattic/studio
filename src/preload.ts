42a8c1c5ce72afaa0ef734a5b1133e0c284e660c
Kateryna Kodonenko <kateryna@automattic.com>
2025-11-03 20:44:51 +0100

    Fix cancellation operation

--- src/preload.ts
+++ src/preload.ts
@@ -108,7 +108,7 @@ const api: IpcApi = {
 		ipcRendererInvoke( 'getConnectedWpcomSites', localSiteId ),
 	addSyncOperation: ( id, status ) => ipcRendererSend( 'addSyncOperation', id, status ),
 	clearSyncOperation: ( id ) => ipcRendererSend( 'clearSyncOperation', id ),
-	cancelSyncOperation: ( id ) => ipcRendererInvoke( 'cancelSyncOperation', id ),
+	cancelSyncOperation: ( id ) => ipcRendererSend( 'cancelSyncOperation', id ),
 	getDirectorySize: ( id, subdir ) => ipcRendererInvoke( 'getDirectorySize', id, subdir ),
 	getFileSize: ( id, filePath ) => ipcRendererInvoke( 'getFileSize', id, filePath ),
 	getPathForFile: ( file ) => webUtils.getPathForFile( file ),

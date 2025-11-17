88669d496b40b2f1847ca0d472c247d423eeab87
Antonio Sejas <antonio@sejas.es>
2025-11-17 20:18:54 +0000

    Add createSiteFromRemote to beta feature flag

--- src/lib/beta-features.ts
+++ src/lib/beta-features.ts
@@ -14,6 +14,12 @@ const BETA_FEATURES_DEFINITION: Record< keyof BetaFeatures, BetaFeatureDefinitio
 		default: false,
 		description: '"studio site" command to manage local sites from terminal',
 	},
+	createSiteFromRemote: {
+		label: 'Create Site from Remote',
+		key: 'createSiteFromRemote',
+		default: false,
+		description: 'Enable creating a site from a remote site',
+	},
 } as const;
 
 export const BETA_FEATURES: Record< keyof BetaFeatures, BetaFeatureDefinition > =

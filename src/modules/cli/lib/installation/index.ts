485a1256826847edf8f6d9513d6e860befd8a4ef
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-11-18 11:53:30 +0100

    Dev version warnings

--- src/modules/cli/lib/installation/index.ts
+++ src/modules/cli/lib/installation/index.ts
@@ -1,3 +1,6 @@
+import { dialog } from 'electron';
+import { __ } from '@wordpress/i18n';
+import { getMainWindow } from 'src/main-window';
 import {
 	installCliWithConfirmation as installCliMacOS,
 	isCliInstalled as isCliInstalledMacOS,
@@ -21,6 +24,21 @@ export async function isStudioCliInstalled(): Promise< boolean > {
 }
 
 export async function installStudioCli(): Promise< void > {
+	if ( process.env.NODE_ENV !== 'production' ) {
+		const mainWindow = await getMainWindow();
+		const { response } = await dialog.showMessageBox( mainWindow, {
+			type: 'warning',
+			buttons: [ __( 'Proceed' ), __( 'Cancel' ) ],
+			title: 'You are running a development version of Studio',
+			message:
+				'If you proceed with the CLI installation, the CLI will use the system-level `node` runtime to execute commands instead of the Electron node runtime (which is what is used in production).',
+		} );
+
+		if ( response === 1 ) {
+			return;
+		}
+	}
+
 	if ( process.platform === 'darwin' ) {
 		await installCliMacOS();
 	} else if ( process.platform === 'win32' ) {
@@ -29,6 +47,21 @@ export async function installStudioCli(): Promise< void > {
 }
 
 export async function uninstallStudioCli(): Promise< void > {
+	if ( process.env.NODE_ENV !== 'production' ) {
+		const mainWindow = await getMainWindow();
+		const { response } = await dialog.showMessageBox( mainWindow, {
+			type: 'warning',
+			buttons: [ __( 'Proceed' ), __( 'Cancel' ) ],
+			title: 'You are running a development version of Studio',
+			message:
+				'By uninstalling the CLI, you may be removing a version that uses the Electron runtime to execute commands. If you install the CLI again using this version of the app, a different node runtime will be used to execute commands.',
+		} );
+
+		if ( response === 1 ) {
+			return;
+		}
+	}
+
 	if ( process.platform === 'darwin' ) {
 		await uninstallCliOnMacOS();
 	} else if ( process.platform === 'win32' ) {

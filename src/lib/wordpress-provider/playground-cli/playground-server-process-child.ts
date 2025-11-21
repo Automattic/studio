bff5ceab8effbbe0d88a3a318f1768516b53bce8
Roberto Aranda <roberto.aranda@automattic.com>
2025-11-21 17:36:30 +0100

    Add translations for steps when adding a site

--- src/lib/wordpress-provider/playground-cli/playground-server-process-child.ts
+++ src/lib/wordpress-provider/playground-cli/playground-server-process-child.ts
@@ -1,4 +1,5 @@
 import { SupportedPHPVersion, PHPRunOptions } from '@php-wasm/universal';
+import { __ } from '@wordpress/i18n';
 import { runCLI, RunCLIArgs, RunCLIServer } from '@wp-playground/cli';
 import { sanitizeRunCLIArgs } from 'src/lib/sentry-sanitizer';
 import { isWordPressDevVersion } from 'src/lib/wordpress-version-utils';
@@ -50,11 +51,29 @@ const originalConsoleError = console.error;
 const originalConsoleWarn = console.warn;
 
 function formatMessageForUI( message: string ): string {
-	if ( message.includes( 'WordPress is running on' ) ) {
-		return 'WordPress is running';
+	if ( message.includes( 'WordPress is running' ) ) {
+		return __( 'WordPress is running' );
 	}
 	if ( message.includes( 'Resolved WordPress release URL' ) ) {
-		return 'Downloading WordPress…';
+		return __( 'Downloading WordPress…' );
+	}
+	if ( message.includes( 'Downloading WordPress' ) ) {
+		return __( 'Downloading WordPress…' );
+	}
+	if ( message.includes( 'Starting up workers' ) ) {
+		return __( 'Starting up workers…' );
+	}
+	if ( message.includes( 'Booting WordPress' ) ) {
+		return __( 'Booting WordPress…' );
+	}
+	if ( message.includes( 'Running the Blueprint' ) ) {
+		return __( 'Running the Blueprint…' );
+	}
+	if ( message.includes( 'Finished running the blueprint' ) ) {
+		return __( 'Finished running the Blueprint…' );
+	}
+	if ( message.includes( 'Preparing workers' ) ) {
+		return __( 'Preparing workers…' );
 	}
 	return message;
 }

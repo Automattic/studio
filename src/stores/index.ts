c4efc6c031c6db8c23218a5c7d26d95d359e88e5
Roberto Aranda <roberto.aranda@automattic.com>
2025-11-25 20:27:08 +0100

    Implement RTK Query approach for fetching wpcom sites

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -26,6 +26,7 @@ import {
 } from 'src/stores/snapshot-slice';
 import { syncReducer } from 'src/stores/sync';
 import { connectedSitesApi, connectedSitesReducer } from 'src/stores/sync/connected-sites';
+import { wpcomSitesApi } from 'src/stores/sync/wpcom-sites';
 import { wpcomApi, wpcomPublicApi } from 'src/stores/wpcom-api';
 import { wordpressVersionsApi } from './wordpress-versions-api';
 import type { SupportedLocale } from 'common/lib/locale';
@@ -42,6 +43,7 @@ export type RootState = {
 	sync: ReturnType< typeof syncReducer >;
 	connectedSitesApi: ReturnType< typeof connectedSitesApi.reducer >;
 	connectedSites: ReturnType< typeof connectedSitesReducer >;
+	wpcomSitesApi: ReturnType< typeof wpcomSitesApi.reducer >;
 	wordpressVersionsApi: ReturnType< typeof wordpressVersionsApi.reducer >;
 	wpcomApi: ReturnType< typeof wpcomApi.reducer >;
 	wpcomPublicApi: ReturnType< typeof wpcomPublicApi.reducer >;
@@ -96,6 +98,7 @@ export const rootReducer = combineReducers( {
 	installedAppsApi: installedAppsApi.reducer,
 	connectedSitesApi: connectedSitesApi.reducer,
 	connectedSites: connectedSitesReducer,
+	wpcomSitesApi: wpcomSitesApi.reducer,
 	onboarding: onboardingReducer,
 	providerConstants: providerConstantsReducer,
 	snapshot: snapshotReducer,
@@ -115,6 +118,7 @@ export const store = configureStore( {
 			.concat( appVersionApi.middleware )
 			.concat( installedAppsApi.middleware )
 			.concat( connectedSitesApi.middleware )
+			.concat( wpcomSitesApi.middleware )
 			.concat( wordpressVersionsApi.middleware )
 			.concat( wpcomApi.middleware )
 			.concat( wpcomPublicApi.middleware )

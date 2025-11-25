54adcc28cd12400ac130ba1c98bee6ef6737944a
bcotrim <bernardo.cotrim@a8c.com>
2025-11-25 18:16:58 +0000

    studio site create

---
+++ common/lib/domains.ts
@@ -0,0 +1,46 @@
+import { __ } from '@wordpress/i18n';
+import { sanitizeFolderName } from './sanitize-folder-name';
+
+const DOMAIN_PATTERN =
+	/^(?!-)[\p{L}\p{N}][\p{L}\p{N}-]{0,61}[\p{L}\p{N}](?<!-)(?:\.(?!-)[\p{L}\p{N}-]{1,61}[\p{L}\p{N}](?<!-))+$/u;
+
+/**
+ * Generates a suitable domain name from site name
+ */
+export const generateCustomDomainFromSiteName = ( siteName: string ): string => {
+	const domainBase = sanitizeFolderName( siteName );
+
+	return `${ domainBase }.wp.local`;
+};
+
+export const getDomainNameValidationError = (
+	useCustomDomain: boolean,
+	domainName: string | null,
+	existingDomainNames: string[]
+): string => {
+	if ( ! useCustomDomain ) {
+		return '';
+	}
+
+	if ( ! domainName ) {
+		return __( 'The domain name is required' );
+	}
+
+	if ( existingDomainNames.includes( domainName ) ) {
+		return __( 'The domain name is already in use' );
+	}
+
+	if ( ! DOMAIN_PATTERN.test( domainName ) ) {
+		return __( 'Please enter a valid domain name' );
+	}
+
+	if ( domainName.length > 253 ) {
+		return __( 'The domain name is too long' );
+	}
+
+	if ( ! domainName.endsWith( '.local' ) ) {
+		return __( 'The domain name must end with .local' );
+	}
+
+	return '';
+};

d5849bf93beb5987d5d25ca4967420e227e36d6e
bcotrim <bernardo.cotrim@a8c.com>
2025-11-25 10:22:10 +0000

    remove studio start from app bundle

--- vite.cli.config.ts
+++ vite.cli.config.ts
@@ -4,7 +4,6 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { existsSync } from 'fs';
 
 const yargsLocalesPath = resolve( __dirname, 'node_modules/yargs/locales' );
-const cliNodeModulesPath = resolve( __dirname, 'cli/node_modules' );
 
 export default defineConfig( {
 	plugins: [
@@ -20,25 +19,11 @@ export default defineConfig( {
 					} ),
 			  ]
 			: [] ),
-		...( existsSync( cliNodeModulesPath )
-			? [
-					viteStaticCopy( {
-						targets: [
-							{
-								src: 'cli/node_modules',
-								dest: '.',
-							},
-						],
-					} ),
-			  ]
-			: [] ),
 	],
 	build: {
 		lib: {
 			entry: {
 				main: resolve( __dirname, 'cli/index.ts' ),
-				'proxy-daemon': resolve( __dirname, 'cli/proxy-daemon.ts' ),
-				'wordpress-server-child': resolve( __dirname, 'cli/wordpress-server-child.ts' ),
 			},
 			name: 'StudioCLI',
 			formats: [ 'cjs' ],
@@ -51,14 +36,6 @@ export default defineConfig( {
 				/^(path|fs|os|child_process|crypto|http|https|http2|url|querystring|stream|util|events|buffer|assert|net|tty|readline|zlib|constants|tls|domain)$/,
 				'fs/promises',
 				'pm2',
-				'@php-wasm/node',
-				'@php-wasm/web',
-				'@php-wasm/logger',
-				'@php-wasm/universal',
-				'@php-wasm/scopes',
-				'@wp-playground/cli',
-				'@wp-playground/blueprints',
-				'@wp-playground/wordpress',
 			],
 			output: {
 				format: 'cjs',

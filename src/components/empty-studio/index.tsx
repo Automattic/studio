2a353b1aae052dc825205939bfe95f25737ec8ba
Antonio Sejas <antonio@sejas.es>
2025-12-01 12:36:40 +0000

    Remove unused translate function

--- src/components/empty-studio/index.tsx
+++ src/components/empty-studio/index.tsx
@@ -1,9 +1,6 @@
-import { useI18n } from '@wordpress/react-i18n';
 import { AddSiteModalContent } from 'src/modules/add-site';
 
 export function EmptyStudio() {
-	const { __ } = useI18n();
-
 	return (
 		<main className="bg-white h-full flex items-center justify-center overflow-hidden z-10">
 			<div className="h-full w-full pt-14 pb-4 max-w-[786px]">

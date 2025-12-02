7d1cd9af594e87b3638f86c54d2e6aacec80f58a
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-12-02 13:35:32 +0100

    No point in using jest.fn in ora mock

--- cli/__mocks__/ora.ts
+++ cli/__mocks__/ora.ts
@@ -1,9 +1,11 @@
-const mockSpinner = {
-	start: jest.fn().mockReturnThis(),
-	stop: jest.fn().mockReturnThis(),
-	succeed: jest.fn().mockReturnThis(),
-	fail: jest.fn().mockReturnThis(),
-};
+function mockOra() {
+	return {
+		start: jest.fn().mockReturnThis(),
+		stop: jest.fn().mockReturnThis(),
+		succeed: jest.fn().mockReturnThis(),
+		fail: jest.fn().mockReturnThis(),
+	};
+}
 
-module.exports = jest.fn().mockReturnValue( mockSpinner );
-module.exports.default = jest.fn().mockReturnValue( mockSpinner );
+module.exports = mockOra;
+module.exports.default = mockOra;

58a2f3fc871f3faddf5b3706ebdedd3b748756bd
Antonio Sejas <antonio@sejas.es>
2025-12-03 18:14:35 +0000

    Update title of dialog to Sync will continue

--- src/index.ts
+++ src/index.ts
@@ -371,7 +371,7 @@ async function appBoot() {
 						type: 'warning',
 				  }
 				: {
-						message: __( 'Sync is in progress' ),
+						message: __( 'Sync will continue' ),
 						detail: __(
 							'The sync process will continue on the server side even after you quit Studio. We will send you an email when it is complete.'
 						),

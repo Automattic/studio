920dcee3da097d17a6412b9184638a074070ea3a
Gergely Csécsey <gergely.csecsey@automattic.com>
2025-12-08 12:56:40 +0000

    Add Site modal: make sure deeplink hook is always mounted (#2201)
    * Make sure deeplink hook is always mounted
    
    * simplify prop defaults
    
    * pass selectedBlueprint from AddSiteModal
    
    * reset deeplink state when the modal is closed
    
    * pass `addSiteProps` to modal content
    
    * move deeplink handler to wrapper component

--- src/components/no-studio-sites/index.tsx
+++ src/components/no-studio-sites/index.tsx
@@ -1,10 +1,10 @@
-import { AddSiteModalContent } from 'src/modules/add-site';
+import { AddSiteContentWithDeeplinkSupport } from 'src/modules/add-site';
 
 export function NoStudioSites() {
 	return (
 		<main className="bg-white h-full flex items-center justify-center overflow-hidden z-10">
 			<div className="h-full w-full pt-14 pb-4 max-w-[786px]">
-				<AddSiteModalContent />
+				<AddSiteContentWithDeeplinkSupport />
 			</div>
 		</main>
 	);

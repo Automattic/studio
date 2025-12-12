4b47ef94c07f7569c0dd54f946b8571f960f9675
Volodymyr Makukha <nei.css@gmail.com>
2025-12-12 15:46:00 +0000

    Inline

--- src/about-menu/open-about-menu.ts
+++ src/about-menu/open-about-menu.ts
@@ -1,7 +1,7 @@
 import { BrowserWindow, app } from 'electron';
 import path from 'path';
 import * as Sentry from '@sentry/electron/renderer';
-import { sprintf, __ } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { ABOUT_WINDOW_HEIGHT, ABOUT_WINDOW_WIDTH } from 'src/constants';
 import { shellOpenExternalWrapper } from 'src/lib/shell-open-external-wrapper';
 
@@ -16,10 +16,10 @@ function getPlatformLabel(): string {
 	const arch = process.arch;
 
 	if ( platform === 'darwin' ) {
-		return arch === 'arm64' ? 'macOS Apple Silicon' : 'macOS Intel';
+		return arch === 'arm64' ? __( 'Mac with Apple Silicon Chip' ) : __( 'Mac with Intel Chip' );
 	}
 	if ( platform === 'win32' ) {
-		return arch === 'arm64' ? 'Windows ARM64' : 'Windows x64';
+		return arch === 'arm64' ? __( 'Windows on ARM' ) : __( 'Windows on Intel/AMD' );
 	}
 	return `${ platform } ${ arch }`;
 }
@@ -57,20 +57,18 @@ export function openAboutWindow() {
 	aboutWindow.webContents.on( 'dom-ready', () => {
 		if ( aboutWindow ) {
 			//When updating these strings, make sure to update the corresponding strings in the about-menu.html file
-			const versionText = sprintf( __( 'Version %s' ), packageJson );
+			const versionText = escapeSingleQuotes( `${ packageJson } (${ getPlatformLabel() })` );
 			const studioByWpcomText = escapeSingleQuotes( __( 'WordPress Studio' ) );
 			const aboutStudioText = escapeSingleQuotes( __( 'About WordPress Studio' ) );
 			const shareFeedbackText = escapeSingleQuotes( __( 'Share Feedback' ) );
 			const releasesText = escapeSingleQuotes( __( 'Release Notes' ) );
 			const demoSitesText = escapeSingleQuotes( __( 'Preview sites powered by' ) );
 			const localSitesText = escapeSingleQuotes( __( 'Local sites powered by' ) );
-			const systemInfoTooltip = escapeSingleQuotes( getPlatformLabel() );
 
 			const script = `
 				document.title = '${ aboutStudioText }';
 				document.getElementById('studio-by-wpcom').innerText = '${ studioByWpcomText }';
 				document.getElementById('version-text').innerText = '${ versionText }';
-				document.getElementById('version-text').title = '${ systemInfoTooltip }';
 				document.getElementById('share-feedback').innerText = '${ shareFeedbackText }';
 				document.getElementById('release-notes').innerText = '${ releasesText }';
 				document.getElementById('demo-sites').innerText = '${ demoSitesText }';

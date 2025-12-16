b0605c880bd9474be9da67b22bf31e0b22b0d849
Kateryna Kodonenko <kateryna@automattic.com>
2025-12-16 13:41:28 +0100

    Bring back isSyncing check

--- src/components/publish-site-button.tsx
+++ src/components/publish-site-button.tsx
@@ -1,8 +1,8 @@
 import { cloudUpload } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import { useCallback } from 'react';
+import { useSyncSites } from 'src/hooks/sync-sites';
 import { useAuth } from 'src/hooks/use-auth';
-import { useOffline } from 'src/hooks/use-offline';
 import { useSiteDetails } from 'src/hooks/use-site-details';
 import { generateCheckoutUrl } from 'src/lib/generate-checkout-url';
 import { getIpcApi } from 'src/lib/get-ipc-api';
@@ -11,13 +11,14 @@ import { useGetConnectedSitesForLocalSiteQuery } from 'src/stores/sync/connected
 
 export const PublishSiteButton = () => {
 	const { __ } = useI18n();
-	const isOffline = useOffline();
 	const { user } = useAuth();
 	const { selectedSite } = useSiteDetails();
 	const { data: connectedSites = [] } = useGetConnectedSitesForLocalSiteQuery( {
 		localSiteId: selectedSite?.id,
 		userId: user?.id,
 	} );
+	const { isAnySitePulling, isAnySitePushing } = useSyncSites();
+	const isAnySiteSyncing = isAnySitePulling || isAnySitePushing;
 
 	const handlePublishClick = useCallback( () => {
 		getIpcApi().openURL( generateCheckoutUrl( selectedSite ?? undefined ) );
@@ -30,8 +31,14 @@ export const PublishSiteButton = () => {
 			variant="primary"
 			icon={ cloudUpload }
 			connectSite={ handlePublishClick }
-			disabled={ isOffline }
-			tooltipText={ __( 'Publishing your site requires an internet connection.' ) }
+			disabled={ isAnySiteSyncing }
+			tooltipText={
+				isAnySiteSyncing
+					? __(
+							'Another site is syncing. Please wait for the sync to finish before you publish your site.'
+					  )
+					: __( 'Publishing your site requires an internet connection.' )
+			}
 		>
 			{ __( 'Publish site' ) }
 		</ConnectButton>

040e0ec1eeaca6349d846892e6d364b6d42bf3f7
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-01-06 12:18:54 +0100

    Tweak comment

--- src/modules/sync/components/sync-dialog.tsx
+++ src/modules/sync/components/sync-dialog.tsx
@@ -395,8 +395,8 @@ export function SyncDialog( {
 								showLabels
 								valueLabel={ formattedSize }
 								limitLabel={ formattedLimit }
-								// translators: %s is a filesize string. This label is displayed if a sync archive is larger
-								// than a given limit.
+								// translators: %s is a filesize string, e.g. "1.3GB". This label is displayed if a sync
+								// archive is larger than a given limit.
 								overLimitLabel={ sprintf( __( '%s over' ), formattedOverAmount ) }
 							/>
 						</div>

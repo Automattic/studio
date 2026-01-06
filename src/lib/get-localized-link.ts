4d7f39727b2b769230c5270a646266284f31c1ae
Volodymyr Makukha <nei.css@gmail.com>
2026-01-06 12:59:37 +0000

    Add LearnHow link (#2353)

--- src/lib/get-localized-link.ts
+++ src/lib/get-localized-link.ts
@@ -34,6 +34,9 @@ const DOCS_LINKS = {
 	docsXdebug: {
 		en: 'https://developer.wordpress.com/docs/developer-tools/studio/xdebug/',
 	},
+	docsSslInStudio: {
+		en: 'https://developer.wordpress.com/docs/developer-tools/studio/ssl-in-studio/',
+	},
 } satisfies Record< `docs${ string }`, TranslatedLink >;
 
 const BLOG_LINKS = {
--- src/modules/add-site/components/create-site-form.tsx
+++ src/modules/add-site/components/create-site-form.tsx
@@ -7,11 +7,10 @@ import { FormEvent, useState, useEffect } from 'react';
 import { generateCustomDomainFromSiteName } from 'common/lib/domains';
 import Button from 'src/components/button';
 import FolderIcon from 'src/components/folder-icon';
-import { LearnMoreLink } from 'src/components/learn-more';
+import { LearnMoreLink, LearnHowLink } from 'src/components/learn-more';
 import TextControlComponent from 'src/components/text-control';
 import { WPVersionSelector } from 'src/components/wp-version-selector';
 import { cx } from 'src/lib/cx';
-import { getIpcApi } from 'src/lib/get-ipc-api';
 import { AllowedPHPVersion } from 'src/lib/wordpress-provider/constants';
 import { useRootSelector } from 'src/stores';
 import { useCheckCertificateTrustQuery } from 'src/stores/certificate-trust-api';
@@ -380,17 +379,7 @@ export const CreateSiteForm = ( {
 										{ __(
 											'You need to manually add the Studio root certificate authority to your keychain and trust it to enable HTTPS.'
 										) }{ ' ' }
-										<Button
-											variant="link"
-											onClick={ () => {
-												getIpcApi().openURL(
-													'https://developer.wordpress.com/docs/developer-tools/studio/ssl-in-studio/'
-												);
-											} }
-										>
-											{ __( 'Learn how' ) }
-											<span aria-label={ __( '(opens in a web browser)' ) }>&#8599;</span>
-										</Button>
+										<LearnHowLink docsLinksKey="docsSslInStudio" />
 									</div>
 								) }
 							</div>

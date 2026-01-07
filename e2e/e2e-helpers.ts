07fc67930103c33975a65fd705edeec7cd3559b2
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-01-07 14:36:54 +0100

    New approach: don't remove `sessionPath` dir

--- e2e/e2e-helpers.ts
+++ e2e/e2e-helpers.ts
@@ -94,7 +94,9 @@ export class E2ESession {
 	async cleanup() {
 		await this.electronApp?.close();
 
-		// Clean up temporary folder to hold application data
-		await rimraf( this.sessionPath );
+		// Removing the `sessionPath` directory has proven to be difficult, especially on Windows. Since
+		// session paths are unique, the WordPress installations are relatively small, and the E2E tests
+		// primarily run in ephemeral CI workers, we've decided to fix this issue by simply not removing
+		// the `sessionPath` directory.
 	}
 }

54011fc363ed8db1b7f199a6cc7f0d61d6e0a999
Rahul Gavande <rahul.gavande@automattic.com>
2026-01-12 17:05:07 +0530

    Add pause and play icons for sync upload controls

---
+++ src/components/icons/play.tsx
@@ -0,0 +1,5 @@
+export const PlayIcon = () => (
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M7 4L15 10L7 16V4Z" fill="currentColor" />
+	</svg>
+);

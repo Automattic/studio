a2e06eae4f208f773551e8062704644fa789fc69
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-01-12 12:04:59 +0100

    Destroy stdio streams again

--- src/modules/cli/lib/execute-command.ts
+++ src/modules/cli/lib/execute-command.ts
@@ -106,6 +106,9 @@ export function executeCliCommand(
 
 	child.on( 'exit', ( code ) => {
 		capturedExitCode = code;
+		// Destroy streams immediately on exit to allow 'close' event to fire on Windows
+		child.stdout?.destroy();
+		child.stderr?.destroy();
 	} );
 
 	function appQuitHandler() {

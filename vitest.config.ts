bef0a556302e9e6bd1f42c96b9c886c4b65ebf36
Kateryna Kodonenko <kateryna@automattic.com>
2026-01-15 11:48:37 +0100

    Adjust vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import path from 'path';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',

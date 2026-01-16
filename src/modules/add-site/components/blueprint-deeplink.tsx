f2cc0692df54ccf800d94b4d3c202a262cb469dc
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2026-01-16 17:59:34 +0100

    Add "Blueprint loaded from URL" footer

--- src/modules/add-site/components/blueprint-deeplink.tsx
+++ src/modules/add-site/components/blueprint-deeplink.tsx
@@ -5,7 +5,7 @@ import {
 	__experimentalText as Text,
 	Icon,
 } from '@wordpress/components';
-import { check } from '@wordpress/icons';
+import { check, link } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import { BlueprintValidationWarning } from 'common/lib/blueprint-validation';
 import { Blueprint } from 'src/stores/wpcom-api';
@@ -30,35 +30,42 @@ export default function BlueprintDeeplink( {
 			<Heading className="text-center text-[32px] text-gray-900 mb-5" weight={ 500 }>
 				{ __( 'Start from a Blueprint' ) }
 			</Heading>
-
 			<BlueprintWarningNotice
 				warnings={ warnings }
 				fileName={ blueprintTitle }
 				className="w-full max-w-4xl mx-auto"
 			/>
-
-			<VStack className="max-w-[400px] min-w-[250px] max-h-[172px] mx-auto p-6 border rounded-lg border-gray-200 mt-16">
-				<HStack className="h-full" alignment="top" spacing={ 4 }>
-					<Icon className="fill-a8c-blue-50 shrink-0" icon={ check } size={ 29 } />
-					<VStack alignment="left" spacing={ 1 }>
-						<Text className="text-lg font-medium text-gray-900">
-							{ __( 'Blueprint selected' ) }
-						</Text>
-						<VStack alignment="left" spacing={ 4 }>
-							<Text className="text-[13px] text-[#50575E]" weight={ 400 }>
-								{ blueprintTitle }
+			<VStack className="max-w-[400px] min-w-[250px] mx-auto mt-16" spacing={ 0 }>
+				<VStack className="w-full max-h-[172px] p-6 border rounded-t-lg border-gray-200">
+					<HStack className="h-full" alignment="top" spacing={ 4 }>
+						<Icon className="fill-a8c-blue-50 shrink-0" icon={ check } size={ 29 } />
+						<VStack alignment="left" spacing={ 1 }>
+							<Text className="text-lg font-medium text-gray-900">
+								{ __( 'Blueprint selected' ) }
 							</Text>
-							{ blueprintDescription && (
-								<Text
-									className="text-[13px] text-a8c-gray-700 line-clamp-3"
-									weight={ 400 }
-									align="initial"
-								>
-									{ blueprintDescription }
+							<VStack alignment="left" spacing={ 4 }>
+								<Text className="text-[13px] text-[#50575E]" weight={ 400 }>
+									{ blueprintTitle }
 								</Text>
-							) }
+								{ blueprintDescription && (
+									<Text
+										className="text-[13px] text-a8c-gray-700 line-clamp-3"
+										weight={ 400 }
+										align="initial"
+									>
+										{ blueprintDescription }
+									</Text>
+								) }
+							</VStack>
 						</VStack>
-					</VStack>
+					</HStack>
+				</VStack>
+				<HStack
+					alignment="start"
+					className="w-full bg-[#FAFAFA] border border-t-0 rounded-b-lg p-[14px]"
+				>
+					<Icon icon={ link } size={ 18 } />
+					<Text className="text-[13px] text-[#2F2F2F]">{ __( 'Blueprint loaded from URL' ) }</Text>
 				</HStack>
 			</VStack>
 		</VStack>

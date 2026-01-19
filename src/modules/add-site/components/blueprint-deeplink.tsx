0b50df80c5535480ba90dca1ad84dad967c36278
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2026-01-19 10:52:39 +0100

    Update container alignment

--- src/modules/add-site/components/blueprint-deeplink.tsx
+++ src/modules/add-site/components/blueprint-deeplink.tsx
@@ -36,8 +36,8 @@ export default function BlueprintDeeplink( {
 				className="w-full max-w-4xl mx-auto"
 			/>
 			<VStack className="max-w-[400px] min-w-[350px] mx-auto mt-16 justify-start" spacing={ 0 }>
-				<VStack className="w-full max-h-[172px] p-6 border rounded-t-lg border-gray-200">
-					<HStack className="h-full" alignment="top" spacing={ 4 }>
+				<VStack className="w-full max-h-[172px] p-6 border rounded-t-lg border-gray-200 justify-start">
+					<HStack className="h-full justify-start" alignment="top" spacing={ 4 }>
 						<Icon className="fill-a8c-blue-50 shrink-0" icon={ check } size={ 29 } />
 						<VStack alignment="left" spacing={ 1 }>
 							<Text className="text-lg font-medium text-gray-900">

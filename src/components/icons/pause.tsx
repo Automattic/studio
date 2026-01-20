9892f91582d4ec099b99606c3f6b9b26d71ec27b
Rahul Gavande <rahul.gavande@automattic.com>
2026-01-20 23:24:19 +0530

    Refactor PauseIcon component to use a smaller size

--- src/components/icons/pause.tsx
+++ src/components/icons/pause.tsx
@@ -1,6 +1,6 @@
 export const PauseIcon = () => (
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect x="9.5" y="5" width="1.5" height="14" fill="currentColor" />
-		<rect x="16" y="5" width="1.5" height="14" fill="currentColor" />
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect x="6" y="4" width="2" height="12" rx="1" fill="currentColor" />
+		<rect x="12" y="4" width="2" height="12" rx="1" fill="currentColor" />
 	</svg>
 );

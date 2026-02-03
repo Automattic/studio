493370d5e8af4a505e85d9c7eff25cac65e188b6
Antonio Sejas <antonio@sejas.es>
2026-02-03 19:19:36 +0000

    start site after copying it

--- src/hooks/use-site-details.tsx
+++ src/hooks/use-site-details.tsx
@@ -466,6 +466,12 @@ export function SiteDetailsProvider( { children }: SiteDetailsProviderProps ) {
 				// Update selected site ID to the new site
 				setSelectedSiteId( newSite.id );
 
+				try {
+					await getIpcApi().startServer( newSite.id );
+				} catch ( startError ) {
+					console.error( 'Failed to start copied site:', startError );
+				}
+
 				// Show notification
 				getIpcApi().showNotification( {
 					title: newSite.name,

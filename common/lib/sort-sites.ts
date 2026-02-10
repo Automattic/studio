de143e748f5c33f3bc13b4bab50b2fee24519662
Mika Andrianarijaona <mikaoelitiana@users.noreply.github.com>
2026-02-10 16:09:26 +0100

    fix: improve sortSite implementation
    Co-authored-by: Bernardo Cotrim <bmmcotrim@gmail.com>

--- common/lib/sort-sites.ts
+++ common/lib/sort-sites.ts
@@ -1,18 +1,9 @@
 export function sortSites< T extends { name: string; sortOrder?: number } >( sites: T[] ): T[] {
+	const MAX = Number.MAX_SAFE_INTEGER;
 	return sites.sort( ( a, b ) => {
-		// If both have sortOrder, sort by sortOrder
-		if ( a.sortOrder !== undefined && b.sortOrder !== undefined ) {
-			return a.sortOrder - b.sortOrder;
-		}
-		// If only a has sortOrder, a comes first
-		if ( a.sortOrder !== undefined ) {
-			return -1;
-		}
-		// If only b has sortOrder, b comes first
-		if ( b.sortOrder !== undefined ) {
-			return 1;
-		}
-		// Neither has sortOrder, sort by name
-		return a.name.localeCompare( b.name, undefined, { numeric: true } );
+		return (
+			( a.sortOrder ?? MAX ) - ( b.sortOrder ?? MAX ) ||
+			a.name.localeCompare( b.name, undefined, { numeric: true } )
+		);
 	} );
 }

bf539ff0ce0a08b9fa7b5306d687a44538cb36e1
Wojtek Naruniec <wojtek@naruniec.me>
2026-02-17 19:04:51 +0100

    Fix prettier formatting in is-window-frame-rtl.ts

--- src/lib/is-window-frame-rtl.ts
+++ src/lib/is-window-frame-rtl.ts
@@ -14,8 +14,7 @@ export function isWindowFrameRtl(): boolean {
 		// `getTextInfo()` replaced the `textInfo` property in Chromium 130+ (Electron 32+)
 		// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/getTextInfo
 		const locale = new Intl.Locale( navigator.language ) as unknown as HasTextInfo;
-		const direction =
-			( locale.getTextInfo?.() ?? locale.textInfo )?.direction ?? 'ltr';
+		const direction = ( locale.getTextInfo?.() ?? locale.textInfo )?.direction ?? 'ltr';
 		cachedResult = 'rtl' === direction;
 	}
 

86cd9315940d4c689fe5035c4a0bb1a1303b8cba
Gergely Csécsey <gcsecsey@gmail.com>
2026-02-18 16:04:48 +0000

    fix tests

--- src/modules/sync/components/sync-connected-sites.tsx
+++ src/modules/sync/components/sync-connected-sites.tsx
@@ -111,6 +111,7 @@ const SyncConnectedSiteControls = ( {
 							) }
 							onClick={ () => setSyncDialogType( 'pull' ) }
 							disabled={ isAnySiteSyncing || isOffline }
+							data-testid="sync-list-pull-button"
 						>
 							<Icon icon={ cloudDownload } />
 							{ __( 'Pull' ) }
@@ -151,6 +152,7 @@ const SyncConnectedSiteControls = ( {
 							) }
 							onClick={ () => setSyncDialogType( 'push' ) }
 							disabled={ isAnySiteSyncing || isOffline }
+							data-testid="sync-list-push-button"
 						>
 							<Icon icon={ cloudUpload } />
 							{ __( 'Push' ) }

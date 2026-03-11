aaad8cea0d272993c506c53a7ad2d71724361d78
Volodymyr Makukha <nei.css@gmail.com>
2026-03-11 12:31:51 +0000

    Feature flag

--- apps/studio/src/modules/user-settings/components/preferences-tab.tsx
+++ apps/studio/src/modules/user-settings/components/preferences-tab.tsx
@@ -2,6 +2,7 @@ import { SupportedLocale } from '@studio/common/lib/locale';
 import { useI18n } from '@wordpress/react-i18n';
 import { useState } from 'react';
 import Button from 'src/components/button';
+import { useFeatureFlags } from 'src/hooks/use-feature-flags';
 import { isWindowsStore } from 'src/lib/app-globals';
 import { McpSettings } from 'src/modules/mcp/components/mcp-settings';
 import { EditorPicker } from 'src/modules/user-settings/components/editor-picker';
@@ -23,6 +24,7 @@ import {
 
 export const PreferencesTab = ( { onClose }: { onClose: () => void } ) => {
 	const { __ } = useI18n();
+	const { enableAgentSuite } = useFeatureFlags();
 	const savedLocale = useI18nLocale();
 	const dispatch = useAppDispatch();
 
@@ -79,7 +81,7 @@ export const PreferencesTab = ( { onClose }: { onClose: () => void } ) => {
 			{ ! isWindowsStore() && (
 				<>
 					<StudioCliToggle value={ isCliInstalledSelection } onChange={ setDirtyIsCliInstalled } />
-					<McpSettings />
+					{ enableAgentSuite && <McpSettings /> }
 				</>
 			) }
 			<div className="mt-auto pt-2 flex justify-end gap-3">

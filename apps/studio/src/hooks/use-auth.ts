81cd242effe9da534b7753f7d77cac15fc9fb10d
Kateryna Kodonenko <kateryna@automattic.com>
2026-03-12 15:44:22 +0100

    Clean up duplicate callbacks

--- apps/studio/src/hooks/use-auth.ts
+++ apps/studio/src/hooks/use-auth.ts
@@ -1,23 +1,12 @@
-import { useCallback } from 'react';
-import { useOffline } from 'src/hooks/use-offline';
-import { getIpcApi } from 'src/lib/get-ipc-api';
-import { useAppDispatch, useRootSelector } from 'src/stores';
-import { authLogout, selectIsAuthenticated, selectUser } from 'src/stores/auth-slice';
-import { getWpcomClient } from 'src/stores/wpcom-api';
-import type { AuthContextType } from 'src/components/auth-provider';
+import { useContext } from 'react';
+import { AuthContext, type AuthContextType } from 'src/components/auth-provider';
 
 export const useAuth = (): AuthContextType => {
-	const dispatch = useAppDispatch();
-	const isOffline = useOffline();
+	const context = useContext( AuthContext );
 
-	const isAuthenticated = useRootSelector( selectIsAuthenticated );
-	const user = useRootSelector( selectUser );
-	const client = getWpcomClient();
+	if ( ! context ) {
+		throw new Error( 'useAuth must be used within an AuthProvider' );
+	}
 
-	const authenticate = useCallback( () => getIpcApi().authenticate(), [] );
-	const logout = useCallback( async () => {
-		await dispatch( authLogout( { isOffline } ) );
-	}, [ dispatch, isOffline ] );
-
-	return { isAuthenticated, user, client, authenticate, logout };
+	return context;
 };

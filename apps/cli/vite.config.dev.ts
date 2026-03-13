81e1f6630b78c3411f79ee36702c4ee499901f0e
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-03-13 09:41:12 +0100

    Only copy node_modules when building CLI for prod

--- apps/cli/vite.config.dev.ts
+++ apps/cli/vite.config.dev.ts
@@ -1,48 +1,9 @@
-import { existsSync, rmSync } from 'fs';
-import { resolve } from 'path';
-import { sync as globSync } from 'glob';
 import { defineConfig, mergeConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { baseConfig, nodeBuiltinExternals } from './vite.config.base';
-
-const cliNodeModulesPath = resolve( __dirname, 'node_modules' );
-const distCliNodeModulesPath = resolve( __dirname, 'dist/cli/node_modules' );
 
 export default mergeConfig(
 	baseConfig,
 	defineConfig( {
-		plugins: [
-			...( existsSync( cliNodeModulesPath )
-				? [
-						viteStaticCopy( {
-							targets: [
-								{
-									src: 'node_modules',
-									dest: '.',
-								},
-							],
-						} ),
-						{
-							// Remove PHP-WASM asyncify binaries from dist. JSPI is newer and faster
-							// than asyncify, and there's no need to bundle both. Removing asyncify
-							// saves ~250MB. Web binaries were removed upstream in
-							// WordPress/wordpress-playground#3315.
-							name: 'prune-php-wasm',
-							apply: 'build' as const,
-							closeBundle() {
-								const asyncifyPaths = globSync( '@php-wasm/node-*/asyncify/', {
-									cwd: distCliNodeModulesPath,
-									absolute: true,
-								} );
-
-								for ( const path of asyncifyPaths ) {
-									rmSync( path, { recursive: true, force: true } );
-								}
-							},
-						},
-				  ]
-				: [] ),
-		],
 		build: {
 			rollupOptions: {
 				external: [

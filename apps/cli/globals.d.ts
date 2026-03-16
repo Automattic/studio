771a728cbbfe42b30a8960cad7f5a215fd892247
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-03-16 15:48:18 +0100

    Fix type errors

--- apps/cli/globals.d.ts
+++ apps/cli/globals.d.ts
@@ -1,3 +1,6 @@
 declare const __ENABLE_STUDIO_AI__: boolean;
 declare const __ENABLE_AGENT_SUITE__: boolean;
 declare const __STUDIO_CLI_VERSION__: string;
+
+declare module 'wpcom-xhr-request';
+declare module '@wp-playground/blueprints/blueprint-schema-validator';

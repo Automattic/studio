93cd9d6aade2ed9b4740df2c1693cf20431280b8
Wojtek Naruniec <wojtek@naruniec.me>
2026-03-20 09:54:39 +0100

    Update way of patching phpMyAdmin patch sqlite location

--- apps/cli/wordpress-server-child.ts
+++ apps/cli/wordpress-server-child.ts
@@ -181,8 +181,9 @@ async function getBaseRunCLIArgs(
 	const enableDebugLog = config.enableDebugLog ?? false;
 	const enableDebugDisplay = config.enableDebugDisplay ?? false;
 
-	const defaultConstants: Record< string, boolean > = {
+	const defaultConstants: Record< string, boolean | string > = {
 		WP_SQLITE_AST_DRIVER: true,
+		WP_SQLITE_LOCATION: '/wordpress/wp-content/mu-plugins/sqlite-database-integration',
 		WP_DEBUG: enableDebugLog || enableDebugDisplay,
 		WP_DEBUG_LOG: enableDebugLog,
 		WP_DEBUG_DISPLAY: enableDebugDisplay,

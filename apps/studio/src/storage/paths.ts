9cd3740ac6e9ecca27d8723aad8dac7eda269c22
bcotrim <bernardo.cotrim@a8c.com>
2026-03-23 11:52:26 +0000

    Add APP_CONFIG_LOCKFILE_NAME constant and fix test failures

--- apps/studio/src/storage/paths.ts
+++ apps/studio/src/storage/paths.ts
@@ -1,6 +1,5 @@
 import path from 'path';
-import { APP_CONFIG_LOCKFILE_NAME } from '@studio/common/constants';
-import { getAppConfigPath, getConfigDirectory } from '@studio/common/lib/config-paths';
+import { getAppConfigPath } from '@studio/common/lib/config-paths';
 
 function inChildProcess() {
 	return process.env.STUDIO_IN_CHILD_PROCESS === 'true';
@@ -21,10 +20,6 @@ export function getUserDataFilePath(): string {
 		return process.env.DEV_APP_DATA_PATH;
 	}
 	return getAppConfigPath();
-}
-
-export function getUserDataLockFilePath(): string {
-	return path.join( getConfigDirectory(), APP_CONFIG_LOCKFILE_NAME );
 }
 
 export function getServerFilesPath(): string {

0538ec43e2c2adeba9d128cfcc87be1e8a1b3db7
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-03-26 09:49:17 +0100

    Refactor site deletion in app

--- apps/studio/src/stores/snapshot-slice.ts
+++ apps/studio/src/stores/snapshot-slice.ts
@@ -3,7 +3,6 @@ import {
 	createAsyncThunk,
 	createSelector,
 	createSlice,
-	isAnyOf,
 	PayloadAction,
 } from '@reduxjs/toolkit';
 import { SNAPSHOT_EVENTS } from '@studio/common/lib/cli-events';
@@ -41,14 +40,7 @@ type DeleteOperation = BaseOperation & {
 	optimistic?: boolean;
 };
 
-type BulkOperation = Omit< BaseOperation, 'progress' > & {
-	operationIds: UUID[];
-	siteId?: string;
-	type: 'bulk';
-	userId?: number;
-};
-
-export type SnapshotOperation = CreateOperation | UpdateOperation | DeleteOperation | BulkOperation;
+export type SnapshotOperation = CreateOperation | UpdateOperation | DeleteOperation;
 
 type SnapshotState = {
 	operations: Record< UUID, SnapshotOperation >;
@@ -96,41 +88,6 @@ const deleteSnapshot = createTypedAsyncThunk(
 		return { operationId };
 	}
 );
-
-async function deleteMultipleSnapshots(
-	snapshots: Snapshot[]
-): Promise< [ url: string, operationId: UUID ][] > {
-	return await Promise.all(
-		snapshots.map( async ( snapshot ) => {
-			const { operationId } = await getIpcApi().deleteSnapshot( snapshot.url );
-			return [ snapshot.url, operationId ];
-		} )
-	);
-}
-
-const deleteAllSnapshotsForSite = createTypedAsyncThunk<
-	{ operations: [ url: string, operationId: UUID ][]; bulkOperationId: UUID },
-	{ siteId: string }
->( 'snapshot/deleteAllSnapshotsForSite', async ( { siteId }, thunkAPI ) => {
-	const state = thunkAPI.getState();
-	const snapshots = snapshotSelectors.selectSnapshotsBySite( state, siteId );
-	const operations = await deleteMultipleSnapshots( snapshots );
-	const bulkOperationId = window.crypto.randomUUID();
-
-	return { operations, bulkOperationId };
-} );
-
-const deleteAllSnapshotsForUser = createTypedAsyncThunk<
-	{ operations: [ url: string, operationId: UUID ][]; bulkOperationId: UUID },
-	{ userId: number }
->( 'snapshot/deleteAllSnapshotsForUser', async ( { userId }, thunkAPI ) => {
-	const state = thunkAPI.getState();
-	const snapshots = snapshotSelectors.selectSnapshotsByUser( state, userId );
-	const operations = await deleteMultipleSnapshots( snapshots );
-	const bulkOperationId = window.crypto.randomUUID();
-
-	return { operations, bulkOperationId };
-} );
 
 const updateSnapshotLocally = createAction< {
 	atomicSiteId: number;
@@ -200,47 +157,9 @@ const snapshotSlice = createSlice( {
 					type: 'delete',
 					optimistic: action.meta.arg.optimistic ?? false,
 				};
-			} )
-			.addMatcher(
-				isAnyOf( deleteAllSnapshotsForSite.fulfilled, deleteAllSnapshotsForUser.fulfilled ),
-				( state, action ) => {
-					if ( ! action.payload.operations.length ) {
-						return;
-					}
-
-					const bulkOperation: BulkOperation = {
-						error: null,
-						operationIds: action.payload.operations.map( ( [ _, operationId ] ) => operationId ),
-						status: 'pending',
-						type: 'bulk',
-					};
-
-					if ( 'siteId' in action.meta.arg ) {
-						bulkOperation.siteId = action.meta.arg.siteId;
-					} else if ( 'userId' in action.meta.arg ) {
-						bulkOperation.userId = action.meta.arg.userId;
-					}
-
-					state.operations[ action.payload.bulkOperationId ] = bulkOperation;
-
-					action.payload.operations.forEach( ( [ url, operationId ] ) => {
-						state.operations[ operationId ] = {
-							error: null,
-							progress: 0,
-							snapshotUrl: url,
-							status: 'pending',
-							type: 'delete',
-						};
-					} );
-				}
-			);
+			} );
 	},
 	selectors: {
-		selectActiveBulkOperationForUser: ( state, userId: number ): BulkOperation | undefined =>
-			Object.values( state.operations ).find(
-				( operation ): operation is BulkOperation =>
-					operation.status === 'pending' && operation.type === 'bulk' && operation.userId === userId
-			),
 		selectActiveCreateOperationForSite: ( state, siteId: string ): CreateOperation | undefined =>
 			Object.values( state.operations ).find(
 				( operation ): operation is CreateOperation =>
@@ -268,6 +187,9 @@ const snapshotSlice = createSlice( {
 	},
 } );
 
+// We use the `createSelector` API here to memoize the result. Whenever a selector returns a
+// reference type (array, object, etc), it's good to use `createSelector` to avoid unnecessary
+// React re-renders.
 const selectActiveCreateUpdateOperationsForAnySite = createSelector(
 	[ ( state: RootState ) => state.snapshot.operations ],
 	( operations ) =>
@@ -371,27 +293,6 @@ function getOperationProgress( action: PreviewCommandLoggerAction ): [ string, n
 function getOperation( operationId: UUID ) {
 	const state = store.getState();
 	return state.snapshot.operations[ operationId ];
-}
-
-function getAssociatedBulkOperation( targetOperationId: UUID ): [ UUID, BulkOperation ] | [] {
-	const state = store.getState();
-	// `Object.entries()` always returns a string type for the key, but we know the type is actually constrained to UUID
-	const entries = Object.entries( state.snapshot.operations ) as [ UUID, SnapshotOperation ][];
-
-	for ( const [ operationId, operation ] of entries ) {
-		if ( operation.type === 'bulk' && operation.operationIds.includes( targetOperationId ) ) {
-			return [ operationId, operation ];
-		}
-	}
-
-	return [];
-}
-
-function isBulkOperationSettled( bulkOperation: BulkOperation ) {
-	return bulkOperation.operationIds.every( ( operationId ) => {
-		const operation = getOperation( operationId );
-		return operation && operation.status !== 'pending';
-	} );
 }
 
 window.ipcListener.subscribe( 'snapshot-event', ( _, snapshotEvent ) => {
@@ -528,18 +429,6 @@ window.ipcListener.subscribe( 'snapshot-success', ( event, payload ) => {
 		} )
 	);
 
-	const [ bulkOperationId, bulkOperation ] = getAssociatedBulkOperation( payload.operationId );
-	const bulkOperationIsSettled = bulkOperation && isBulkOperationSettled( bulkOperation );
-
-	if ( bulkOperationId && bulkOperationIsSettled ) {
-		store.dispatch(
-			snapshotActions.updateOperation( {
-				operationId: bulkOperationId,
-				operation: { status: 'fulfilled' },
-			} )
-		);
-	}
-
 	const { snapshotUrl = '' } = operation;
 
 	if ( operation.type === 'create' ) {
@@ -553,17 +442,10 @@ window.ipcListener.subscribe( 'snapshot-success', ( event, payload ) => {
 			body: sprintf( __( "Preview site '%s' has been updated." ), snapshotUrl ),
 		} );
 	} else if ( operation.type === 'delete' ) {
-		if ( operation.optimistic ) {
-			// Do nothing
-		} else if ( ! bulkOperation ) {
+		if ( ! operation.optimistic ) {
 			getIpcApi().showNotification( {
 				title: operation.snapshotName,
 				body: sprintf( __( "Preview site '%s' has been deleted." ), snapshotUrl ),
-			} );
-		} else if ( bulkOperationIsSettled ) {
-			getIpcApi().showNotification( {
-				title: __( 'Delete Successful' ),
-				body: __( 'All preview sites have been deleted.' ),
 			} );
 		}
 	}
@@ -584,7 +466,5 @@ export const snapshotThunks = {
 	createSnapshot,
 	updateSnapshot,
 	deleteSnapshot,
-	deleteAllSnapshotsForSite,
-	deleteAllSnapshotsForUser,
 };
 export const reducer = snapshotSlice.reducer;

f3a555418fc320c1742f3ffee1dd0fc9fe9d7356
Kateryna Kodonenko <kateryna@automattic.com>
2026-04-02 09:54:37 +0200

    Solve for sitename errors

--- apps/studio/src/modules/add-site/components/create-site-form.tsx
+++ apps/studio/src/modules/add-site/components/create-site-form.tsx
@@ -293,6 +293,7 @@ export const CreateSiteForm = ( {
 		}
 
 		const hasErrors =
+			! siteName.trim() ||
 			!! pathError ||
 			( useCustomDomain && !! customDomainError ) ||
 			! adminUsername.trim() ||
@@ -306,7 +307,7 @@ export const CreateSiteForm = ( {
 			onValidityChange( isValid );
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ pathError, customDomainError, useCustomDomain, adminUsername, adminPassword, adminEmail ] );
+	}, [ siteName, pathError, customDomainError, useCustomDomain, adminUsername, adminPassword, adminEmail ] );
 
 	const handleSiteNameChange = useCallback(
 		async ( name: string ) => {
@@ -455,7 +456,7 @@ export const CreateSiteForm = ( {
 						onKeyDown={ ( event ) => {
 							if ( event.key === 'Enter' ) {
 								event.preventDefault();
-								if ( siteName.trim() && errorCount === 0 ) {
+								if ( errorCount === 0 ) {
 									handleFormSubmit( event as FormEvent );
 								}
 							}

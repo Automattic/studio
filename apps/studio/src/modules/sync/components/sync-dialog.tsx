bde82d964ae7c5b0e9a2a266240a08df77d4cfa2
Volodymyr Makukha <nei.css@gmail.com>
2026-04-07 15:48:51 +0100

    Fix copy for php and wp version mismatch

--- apps/studio/src/modules/sync/components/sync-dialog.tsx
+++ apps/studio/src/modules/sync/components/sync-dialog.tsx
@@ -451,7 +451,7 @@ export function SyncDialog( {
 						<Notice status="warning" isDismissible={ false } className="mb-4">
 							<p data-testid="push-version-mismatch-notice">
 								{ __(
-									'Your Studio site is using a different WordPress or PHP version than your WordPress.com site. The remote site will keep on using the newest supported versions.'
+									'Your Studio site is using a different WordPress or PHP version than your remote site. The remote site will keep using the newest supported versions.'
 								) }
 							</p>
 						</Notice>

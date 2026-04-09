c96e79ab6e18b3187f34cf94f8ea68d73679d7c7
Volodymyr Makukha <nei.css@gmail.com>
2026-04-09 09:45:09 +0100

    Render correct error and enable buttons when pull failed (#2964)

--- apps/studio/src/ipc-utils.ts
+++ apps/studio/src/ipc-utils.ts
@@ -6,7 +6,6 @@ import { PreviewCommandLoggerAction } from '@studio/common/logger-actions';
 import { ImportExportEventData } from 'src/lib/import-export/handle-events';
 import { getMainWindow } from 'src/main-window';
 import type { StoredAuthToken } from '@studio/common/lib/shared-config';
-import type { UserData } from 'src/storage/storage-types';
 
 type SnapshotEventData = {
 	action: PreviewCommandLoggerAction;

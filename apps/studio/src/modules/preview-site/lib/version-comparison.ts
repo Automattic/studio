e5b566418faf29928547c5efd04407ad9cabbd27
Volodymyr Makukha <nei.css@gmail.com>
2026-04-15 17:02:46 +0100

    Fix

--- apps/studio/src/modules/preview-site/lib/version-comparison.ts
+++ apps/studio/src/modules/preview-site/lib/version-comparison.ts
@@ -1,3 +1,4 @@
+import { RecommendedPHPVersion as DEFAULT_PHP_VERSION } from '@studio/common/types/php-versions';
 import semver from 'semver';
 
 export function hasVersionMismatch(
@@ -12,4 +13,32 @@ export function hasVersionMismatch(
 		!! coercedVersion2 &&
 		semver.compare( coercedVersion1, coercedVersion2 ) !== 0
 	);
+}
+
+/**
+ * Checks if the WordPress and PHP versions of the current site are supported by preview sites.
+ * Supported versions are:
+ * - WordPress version is the latest stable version
+ * - PHP version is the recommended By Studio
+ */
+export function isWpAndPhpVersionsSupportedByPreviewSites( {
+	wpVersion,
+	latestWpVersion,
+	phpVersion,
+}: {
+	wpVersion: string;
+	latestWpVersion: string | undefined;
+	phpVersion: string;
+} ): boolean {
+	const coercedWpVersion = semver.coerce( wpVersion );
+	const coercedLatestWpVersion = semver.coerce( latestWpVersion );
+	const isPhpVersionDefault = phpVersion === DEFAULT_PHP_VERSION;
+
+	// We need coerced versions to ensure they can be parsed and compared by semver
+	const isWpVersionOlderThanLatest =
+		coercedWpVersion &&
+		coercedLatestWpVersion &&
+		semver.compare( coercedWpVersion, coercedLatestWpVersion ) < 0;
+
+	return isWpVersionOlderThanLatest || ! isPhpVersionDefault;
 }

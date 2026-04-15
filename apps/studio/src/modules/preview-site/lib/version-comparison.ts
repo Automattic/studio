3d4594c3ac0766cb8b2631dc86e8daa8d1ee4859
Volodymyr Makukha <nei.css@gmail.com>
2026-04-15 15:58:05 +0100

    Lint

--- apps/studio/src/modules/preview-site/lib/version-comparison.ts
+++ apps/studio/src/modules/preview-site/lib/version-comparison.ts
@@ -1,6 +1,6 @@
 import semver from 'semver';
 
-export function hasVersionMismatch( version1: string, version2: string ): boolean {
+export function hasVersionMismatch( version1: string | null, version2: string | null ): boolean {
 	const coercedVersion1 = semver.coerce( version1 );
 	const coercedVersion2 = semver.coerce( version2 );
 

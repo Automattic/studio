f165bf059a89ecf76cc7d7a8fddad6543e5d52de
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-04-16 10:02:56 +0200

    Fix picospinner migration issues, clean up `Logger` API

--- apps/cli/logger.ts
+++ apps/cli/logger.ts
@@ -57,44 +57,38 @@ export class Logger< T extends string > {
 
 		if ( canSend() ) {
 			process.send!( { action, status: 'inprogress', message } );
-			return;
-		}
-		if ( progressCallback ) {
+		} else if ( progressCallback ) {
 			progressCallback!( message );
-			return;
+		} else {
+			this.spinner.setText( message );
+			if ( ! this.spinner.running ) {
+				this.spinner.start();
+			}
 		}
-		this.spinner.setText( message );
-		this.spinner.start();
 	}
 
 	public reportProgress( message: string ) {
 		if ( canSend() ) {
 			process.send!( { action: this.currentAction, status: 'inprogress', message } );
-			return;
-		}
-
-		if ( progressCallback ) {
+		} else if ( progressCallback ) {
 			progressCallback!( message, true );
-			return;
-		}
-
-		// Update the spinner text and force render
-		this.spinner.setText( message );
-		if ( ! this.spinner.running ) {
-			this.spinner.start();
 		} else {
-			this.spinner.refresh();
+			if ( ! this.spinner.running ) {
+				this.spinner.start();
+			}
+			this.spinner.setText( message );
 		}
 	}
 
-	public reportSuccess( message: string, shouldClearSpinner = false ) {
+	public reportSuccess( message: string ) {
 		if ( canSend() ) {
 			process.send!( { action: this.currentAction, status: 'success', message } );
 		} else if ( progressCallback ) {
 			progressCallback!( message );
-		} else if ( shouldClearSpinner ) {
-			this.spinner.stop();
 		} else {
+			if ( ! this.spinner.running ) {
+				this.spinner.start();
+			}
 			this.spinner.succeed( message );
 		}
 
@@ -104,13 +98,14 @@ export class Logger< T extends string > {
 	public reportWarning( message: string ) {
 		if ( canSend() ) {
 			process.send!( { action: this.currentAction, status: 'warning', message } );
-			return;
-		}
-		if ( progressCallback ) {
+		} else if ( progressCallback ) {
 			progressCallback!( message );
-			return;
+		} else {
+			if ( ! this.spinner.running ) {
+				this.spinner.start();
+			}
+			this.spinner.warn( message );
 		}
-		this.spinner.warn( message );
 	}
 
 	public reportError( error: LoggerError, isFatal = true ) {
@@ -123,6 +118,9 @@ export class Logger< T extends string > {
 		} else if ( progressCallback ) {
 			progressCallback!( error.message );
 		} else {
+			if ( ! this.spinner.running ) {
+				this.spinner.start();
+			}
 			this.spinner.fail( error.message );
 		}
 

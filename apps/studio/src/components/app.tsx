e39dfedab16a6a10cf70b0a3de8e407fc64caada
katinthehatsite <katerynakodonenko@gmail.com>
2026-04-21 19:55:57 +0200

    Update apps/studio/src/components/app.tsx
    Co-authored-by: Bernardo Cotrim <bmmcotrim@gmail.com>

--- apps/studio/src/components/app.tsx
+++ apps/studio/src/components/app.tsx
@@ -105,7 +105,7 @@ export default function App() {
 						>
 							<div
 								className={ cx(
-									'w-[3px] rounded-[2px] transition-opacity duration-150 bg-[#3858e9]',
+									'w-[3px] rounded-[2px] transition-opacity duration-150 bg-frame-theme',
 									isDragging ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
 								) }
 							/>

95059d0c56e29fb09d04118244b8b7130fcea694
bcotrim <bernardo.cotrim@a8c.com>
2026-04-22 17:02:01 +0100

    Update Windows launcher to detect bundled SEA binary

--- apps/studio/bin/studio-cli.bat
+++ apps/studio/bin/studio-cli.bat
@@ -8,15 +8,19 @@ set ORIGINAL_CP=!ORIGINAL_CP: =!
 rem Set code page to UTF-8
 chcp 65001 >nul
 
-set BUNDLED_NODE_EXECUTABLE=%~dp0node.exe
+set "CLI_BINARY=%~dp0studio.exe"
 set "CLI_SCRIPT=%~dp0..\cli\main.mjs"
 
 rem Prevent node from printing warnings about NODE_OPTIONS being ignored
 set NODE_OPTIONS=
 
-if exist "%BUNDLED_NODE_EXECUTABLE%" (
-	"!BUNDLED_NODE_EXECUTABLE!" --experimental-wasm-jspi "!CLI_SCRIPT!" %*
+if exist "%CLI_BINARY%" (
+	rem Production: run the bundled SEA binary directly. We can't pass
+	rem --experimental-wasm-jspi here because Node's SEA startup skips CLI flag
+	rem parsing; PHP-WASM falls back to asyncify.
+	"!CLI_BINARY!" %*
 ) else (
+	rem Development fallback: use system Node with the CLI script
 	if not exist "!CLI_SCRIPT!" (
 		set "CLI_SCRIPT=%~dp0..\dist\cli\main.mjs"
 	)

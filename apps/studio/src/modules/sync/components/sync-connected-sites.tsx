b26bba2b0419a9e7107578e2118dcffe3c04beea
Kateryna Kodonenko <kateryna@automattic.com>
2026-04-24 08:16:55 -0400

    Ensure the trunk is correct

--- apps/studio/src/modules/sync/components/sync-connected-sites.tsx
+++ apps/studio/src/modules/sync/components/sync-connected-sites.tsx
@@ -11,6 +11,7 @@ import { CircleRedCrossIcon } from 'src/components/icons/circle-red-cross';
 import { PauseIcon } from 'src/components/icons/pause';
 import { PlayIcon } from 'src/components/icons/play';
 import offlineIcon from 'src/components/offline-icon';
+import { PressableLogo } from 'src/components/pressable-logo';
 import ProgressBar from 'src/components/progress-bar';
 import { Tooltip, DynamicTooltip } from 'src/components/tooltip';
 import { WordPressLogoCircle } from 'src/components/wordpress-logo-circle';
@@ -666,6 +667,8 @@ const SyncConnectedSiteSection = ( {
 		logo = <div className="w-5 h-5 rounded-full skeleton-bg" aria-label={ __( 'Loading' ) } />;
 	} else if ( hasConnectionErrors ) {
 		logo = <CircleRedCrossIcon />;
+	} else if ( connectedSite.isPressable ) {
+		logo = <PressableLogo />;
 	} else if ( connectedSite.siteIconUrl ) {
 		logo = (
 			<img src={ connectedSite.siteIconUrl } alt="" className="w-5 h-5 rounded-full object-cover" />

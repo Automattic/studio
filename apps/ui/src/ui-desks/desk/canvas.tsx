35619896f7d8ec23d8f14fd59256a28c49a67b7a
Riad Benguella <benguella@gmail.com>
2026-05-09 15:05:01 +0100

    apps/ui: Disable the default tldraw context menu on the desk canvas (#3411)

--- apps/ui/src/ui-desks/desk/canvas.tsx
+++ apps/ui/src/ui-desks/desk/canvas.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect } from 'react';
-import { Tldraw, type Editor, type TldrawOptions } from 'tldraw';
+import { Tldraw, type Editor, type TLComponents, type TldrawOptions } from 'tldraw';
 import 'tldraw/tldraw.css';
 import { deskShapeUtils } from '@/ui-desks/shapes/registry';
 import { useDesk, useRegisterDeskEditor } from './provider';
@@ -8,6 +8,10 @@ import styles from './style.module.css';
 const deskCanvasOptions = {
 	createTextOnCanvasDoubleClick: false,
 } satisfies Partial< TldrawOptions >;
+
+const deskCanvasComponents = {
+	ContextMenu: null,
+} satisfies Partial< TLComponents >;
 
 export function DeskCanvas() {
 	const { isLoading } = useDesk();
@@ -36,6 +40,7 @@ export function DeskCanvas() {
 				hideUi
 				autoFocus
 				options={ deskCanvasOptions }
+				components={ deskCanvasComponents }
 				shapeUtils={ deskShapeUtils }
 				onMount={ handleMount }
 			/>

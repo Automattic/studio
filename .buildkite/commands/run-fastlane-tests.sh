f07e158e63a9c29caa03eaac265c2e1dcef28efb
Gio Lodi <gio@mokacoding.com>
2026-05-27 11:36:36 +1000

    s/Fastlane/fastlane/
    Co-authored-by: Gio Lodi <giovanni.lodi42@gmail.com>

--- .buildkite/commands/run-fastlane-tests.sh
+++ .buildkite/commands/run-fastlane-tests.sh
@@ -6,7 +6,7 @@ if .buildkite/commands/should-skip-job.sh --job-type fastlane; then
   exit 0
 fi
 
-echo '--- :ruby: Run Fastlane Helper Tests'
+echo '--- :fastlane: Run fastlane Helper Tests'
 # Tests use stdlib minitest and don't require bundler — see fastlane/test/*.
 for test_file in fastlane/test/*_test.rb; do
   ruby "$test_file"

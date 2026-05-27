6113477567416fcc99988c1d163a4d67c19b1c49
Gio Lodi <gio@mokacoding.com>
2026-05-27 11:36:51 +1000

    Remove noisy comment

--- .buildkite/commands/run-fastlane-tests.sh
+++ .buildkite/commands/run-fastlane-tests.sh
@@ -7,7 +7,6 @@ if .buildkite/commands/should-skip-job.sh --job-type fastlane; then
 fi
 
 echo '--- :fastlane: Run fastlane Helper Tests'
-# Tests use stdlib minitest and don't require bundler — see fastlane/test/*.
 for test_file in fastlane/test/*_test.rb; do
   ruby "$test_file"
 done

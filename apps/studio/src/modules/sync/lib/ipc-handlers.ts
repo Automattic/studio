23c67a7d0fbf7685625ea1865491375a87d924b9
Rahul Gavande <rahul.gavande@automattic.com>
2026-06-26 18:28:09 +0530

    Reword 413 push error to suggest retrying later

--- apps/studio/src/modules/sync/lib/ipc-handlers.ts
+++ apps/studio/src/modules/sync/lib/ipc-handlers.ts
@@ -392,7 +392,7 @@ export async function pushArchive(
 			return {
 				success: false,
 				error: __(
-					'The site archive is too large to upload to WordPress.com right now. Please reduce the size of the site and try again.'
+					'The site archive is too large to upload to WordPress.com right now. Please try again later.'
 				),
 			};
 		}

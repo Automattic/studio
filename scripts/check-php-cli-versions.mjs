341fb96dee535771d30d7f00c8089a0fd4a37466
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2026-07-01 15:02:41 +0200

    Fix import path

--- scripts/check-php-cli-versions.mjs
+++ scripts/check-php-cli-versions.mjs
@@ -2,7 +2,7 @@
 
 import fs from 'node:fs/promises';
 import { pathToFileURL } from 'node:url';
-import phpBinaryCdnMetadata from '../tools/common/lib/php-binary-cdn-metadata.mjs';
+import phpBinaryCdnMetadata from '../packages/common/lib/php-binary-cdn-metadata.mjs';
 
 const PHP_UPSTREAM_REPOSITORY = 'php/php-src';
 

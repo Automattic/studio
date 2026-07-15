b1758e3a850e1df8867b819f299f49c8c14f3c73
Volodymyr Makukha <nei.css@gmail.com>
2026-07-15 18:48:33 +0100

    Fix

--- apps/cli/index.ts
+++ apps/cli/index.ts
@@ -62,12 +62,14 @@ async function main() {
 		.version( version )
 		.alias( 'v', 'version' )
 		.alias( 'h', 'help' )
+		.wrap( Math.min( 90, yargs().terminalWidth() ?? 90 ) )
 		.option( 'avoid-telemetry', {
 			type: 'boolean',
 			hidden: true,
 		} )
 		.option( 'path', {
 			type: 'string',
+			alias: 'p',
 			normalize: true,
 			default: process.cwd(),
 			defaultDescription: __( 'Current directory' ),

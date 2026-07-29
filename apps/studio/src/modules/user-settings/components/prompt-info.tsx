c153d41e05d9d0adcfc0b699e55044571eb280f5
Volodymyr Makukha <nei.css@gmail.com>
2026-07-29 13:02:21 +0100

    fix ui

--- apps/studio/src/modules/user-settings/components/prompt-info.tsx
+++ apps/studio/src/modules/user-settings/components/prompt-info.tsx
@@ -8,6 +8,7 @@ import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import ProgressBar from 'src/components/progress-bar';
 import { useOffline } from 'src/hooks/use-offline';
+import { cx } from 'src/lib/cx';
 import { useI18nLocale } from 'src/stores';
 import { useGetStudioAssistantQuota } from 'src/stores/wpcom-api';
 
@@ -34,7 +35,7 @@ export function PromptInfo() {
 			<div className="flex gap-3 flex-row items-center w-full">
 				<div className="flex w-full flex-col gap-2">
 					<div className="flex w-full flex-row justify-between gap-8 ">
-						<div className="flex flex-row items-center text-right">
+						<div className={ cx( 'flex flex-row items-center', ! isBlocked && 'text-right' ) }>
 							<span className="text-frame-text-secondary">
 								{ isOffline && __( "You're currently offline" ) }
 								{ isBlocked && formatAiBlockedNotice() }

e43967b1b0499b07e84d9561668c050c96420e74
Volodymyr Makukha <nei.css@gmail.com>
2026-08-11 13:24:09 +0100

    Use array insted of comma separated list fpr pulling

--- apps/cli/commands/pull.ts
+++ apps/cli/commands/pull.ts
@@ -257,18 +257,15 @@ export const registerCommand = ( yargs: StudioArgv ) => {
 					description: __( 'Remote site URL or ID' ),
 				} )
 				.option( 'include-path-list', {
-					type: 'string',
-					description: __(
-						'Comma-separated backup node ids to pull when using the "paths" option'
-					),
+					type: 'array',
+					description: __( 'Backup node ids to pull when using the "paths" option' ),
 					hidden: true,
-					coerce: ( val: string | undefined ) =>
-						val !== undefined
-							? val
-									.split( ',' )
-									.map( ( item ) => item.trim() )
-									.filter( Boolean )
-							: undefined,
+					coerce: ( value ) => {
+						if ( ! Array.isArray( value ) ) {
+							throw new Error( __( 'include-path-list must be an array' ) );
+						}
+						return value.map( String );
+					},
 				} );
 		},
 		handler: async ( argv ) => {
